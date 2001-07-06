153d49d7f71aa10ae87d124504a1985e66c4fc53
Janne Jalkanen <jalkanen@apache.org>
2001-07-06 19:11:12 +0000

    Javadocs.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@620894 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/Release.java
+++ src/com/ecyrd/jspwiki/Release.java
@@ -1,6 +1,11 @@
 
 package com.ecyrd.jspwiki;
 
+/**
+ *  Contains release information.
+ *
+ *  @author Janne Jalkanen
+ */
 public class Release
 {
     public static final String     APPNAME       = "JSPWiki";
@@ -8,7 +13,7 @@ public class Release
     public static final String     RELEASE       = "R1";
     public static final int        VERSION       = 1;
     public static final int        REVISION      = 0;
-    public static final int        MINORREVISION = 0;
+    public static final int        MINORREVISION = 1;
 
     public static final String     VERSTR        = 
         VERSION+"."+REVISION+"."+MINORREVISION;
