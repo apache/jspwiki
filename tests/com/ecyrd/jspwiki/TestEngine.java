d1409ef403a95ebe588a56f52635242caaa06a0a
Janne Jalkanen <jalkanen@apache.org>
2001-07-10 20:51:12 +0000

    Added TestEngine( Properties props )
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@620915 13f79535-47bb-0310-9956-ffa450edef68

--- tests/com/ecyrd/jspwiki/TestEngine.java
+++ tests/com/ecyrd/jspwiki/TestEngine.java
@@ -1,8 +1,14 @@
 
 package com.ecyrd.jspwiki;
+import java.util.Properties;
 
 public class TestEngine extends WikiEngine
 {
+    public TestEngine( Properties props )
+    {
+        super( props );
+    }
+
     public boolean pageExists( String page )
     {
         return true;
