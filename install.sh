0ce684b3d913506afd5924f255c27c72f91a0f58
Janne Jalkanen <jalkanen@apache.org>
2001-07-13 20:56:51 +0000

    Fixed copying of property file.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@620920 13f79535-47bb-0310-9956-ffa450edef68

--- install.sh
+++ install.sh
@@ -7,15 +7,27 @@ if [ "$1" = "public" ]
 then
     echo "Installing in public"
     TOMCAT_HOME=/p/web/tomcat/current/
+    private=0
 else
     echo "Installing in private"
     TOMCAT_HOME=$HOME/jakarta-tomcat-3.2.2
+    private=1
 fi
 
 ant war
 
 rm -rf $TOMCAT_HOME/webapps/JSPWiki
 cp /tmp/jalkanen/JSPWiki/*.war $TOMCAT_HOME/webapps
+
+cd $TOMCAT_HOME/webapps
+mkdir JSPWiki
+cd JSPWiki
+jar xf ../JSPWiki.war
+
+if [ $private -eq 1 ]
+then
+    cp -v /home/jalkanen/Projects/JSPWiki/jspwiki.properties $TOMCAT_HOME/webapps/JSPWiki/WEB-INF/
+fi
 
 $TOMCAT_HOME/bin/shutdown.sh
 $TOMCAT_HOME/bin/startup.sh
