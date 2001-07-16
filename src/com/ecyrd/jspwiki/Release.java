ddc4b36c68a3effed7f87f56a7d60bac1c82b03f
Janne Jalkanen <jalkanen@apache.org>
2001-07-16 07:16:39 +0000

    1.0.7
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@620944 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/Release.java
+++ src/com/ecyrd/jspwiki/Release.java
@@ -13,7 +13,7 @@ public class Release
     public static final String     RELEASE       = "R1";
     public static final int        VERSION       = 1;
     public static final int        REVISION      = 0;
-    public static final int        MINORREVISION = 6;
+    public static final int        MINORREVISION = 7;
 
     public static final String     VERSTR        = 
         VERSION+"."+REVISION+"."+MINORREVISION;
