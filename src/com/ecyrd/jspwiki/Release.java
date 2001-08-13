7f92eb7b43f49acb649ba2e4fecc5f7b381acd43
Janne Jalkanen <jalkanen@apache.org>
2001-08-13 19:41:00 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@620994 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/Release.java
+++ src/com/ecyrd/jspwiki/Release.java
@@ -12,8 +12,8 @@ public class Release
 
     public static final String     RELEASE       = "R1";
     public static final int        VERSION       = 1;
-    public static final int        REVISION      = 1;
-    public static final int        MINORREVISION = 3;
+    public static final int        REVISION      = 2;
+    public static final int        MINORREVISION = 0;
 
     public static final String     VERSTR        = 
         VERSION+"."+REVISION+"."+MINORREVISION;
