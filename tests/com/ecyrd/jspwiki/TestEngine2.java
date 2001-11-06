96e3f273f97ae111478c514ec7d7196b5c942738
Janne Jalkanen <jalkanen@apache.org>
2001-11-06 21:11:09 +0000

    Initial version.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@621165 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/com/ecyrd/jspwiki/TestEngine2.java
@@ -0,0 +1,13 @@
+
+package com.ecyrd.jspwiki;
+import java.util.Properties;
+
+/** Real test engine */
+public class TestEngine2 extends WikiEngine
+{
+    public TestEngine2( Properties props )
+        throws NoRequiredPropertyException
+    {
+        super( props );
+    }
+}
