7009df9c6a6b96b60be4f44f06555e5aae5c4214
Janne Jalkanen <jalkanen@apache.org>
2002-01-09 00:12:22 +0000

    testSingleReferral(),testMaxReferences().
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@621315 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/com/ecyrd/jspwiki/plugin/ReferringPagesPluginTest.java
@@ -0,0 +1,94 @@
+
+package com.ecyrd.jspwiki.plugin;
+
+import com.ecyrd.jspwiki.*;
+import junit.framework.*;
+import java.io.*;
+import java.util.*;
+
+public class ReferringPagesPluginTest extends TestCase
+{
+    Properties props = new Properties();
+    TestEngine2 engine;
+    WikiContext context;
+    PluginManager manager;
+    
+    public ReferringPagesPluginTest( String s )
+    {
+        super( s );
+    }
+
+    public void setUp()
+        throws Exception
+    {
+        props.load( getClass().getClassLoader().getResourceAsStream("/jspwiki.properties") );
+
+        engine = new TestEngine2(props);
+
+        engine.saveText( "TestPage", "Reference to [Foobar]." );
+        engine.saveText( "Foobar", "Reference to [TestPage]." );
+        engine.saveText( "Foobar2", "Reference to [TestPage]." );
+        engine.saveText( "Foobar3", "Reference to [TestPage]." );
+        engine.saveText( "Foobar4", "Reference to [TestPage]." );
+        engine.saveText( "Foobar5", "Reference to [TestPage]." );
+        engine.saveText( "Foobar6", "Reference to [TestPage]." );
+        engine.saveText( "Foobar7", "Reference to [TestPage]." );
+
+        context = new WikiContext( engine, "TestPage" );
+        manager = new PluginManager();
+    }
+
+    public void tearDown()
+    {
+        engine.deletePage( "TestPage" );
+        engine.deletePage( "Foobar" );
+        engine.deletePage( "Foobar2" );
+        engine.deletePage( "Foobar3" );
+        engine.deletePage( "Foobar4" );
+        engine.deletePage( "Foobar5" );
+        engine.deletePage( "Foobar6" );
+        engine.deletePage( "Foobar7" );
+    }
+
+    private String mkLink( String page )
+    {
+        return "<A CLASS=\"wikipage\" HREF=\"Wiki.jsp?page="+page+"\">"+page+"</A>";        
+    }
+
+    public void testSingleReferral()
+        throws Exception
+    {
+        WikiContext context2 = new WikiContext( engine, "Foobar" );
+
+        String res = manager.execute( context2,
+                                      "{INSERT com.ecyrd.jspwiki.plugin.ReferringPagesPlugin WHERE max=5}");
+
+        assertEquals( mkLink( "TestPage" )+"\n<BR>\n",
+                      res );
+    }
+
+    public void testMaxReferences()
+        throws Exception
+    {
+        String res = manager.execute( context,
+                                      "{INSERT com.ecyrd.jspwiki.plugin.ReferringPagesPlugin WHERE max=5}");
+
+        int count = 0, index = -1;
+
+        // Count the number of hyperlinks.  We could check their
+        // correctness as well, though.
+
+        while( (index = res.indexOf("<A",index+1)) != -1 )
+        {
+            count++;
+        }
+
+        assertEquals( 5, count );
+    }
+
+
+    public static Test suite()
+    {
+        return new TestSuite( ReferringPagesPluginTest.class );
+    }
+}
