8d94a95ae339941baaf8f916d4a9aab0ad54b855
Janne Jalkanen <jalkanen@apache.org>
2002-01-13 18:46:09 +0000

    Added deletePage().
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@621329 13f79535-47bb-0310-9956-ffa450edef68

--- tests/com/ecyrd/jspwiki/TestEngine2.java
+++ tests/com/ecyrd/jspwiki/TestEngine2.java
@@ -1,6 +1,7 @@
 
 package com.ecyrd.jspwiki;
 import java.util.Properties;
+import java.io.File;
 
 /** Real test engine */
 public class TestEngine2 extends WikiEngine
@@ -9,5 +10,14 @@ public class TestEngine2 extends WikiEngine
         throws NoRequiredPropertyException
     {
         super( props );
+    }
+
+    public void deletePage( String name )
+    {
+        String files = getWikiProperties().getProperty( FileSystemProvider.PROP_PAGEDIR );
+
+        File f = new File( files, name+FileSystemProvider.FILE_EXT );
+
+        f.delete();
     }
 }
