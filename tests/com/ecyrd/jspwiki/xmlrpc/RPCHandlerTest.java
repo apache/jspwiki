52327356ffe363d06efd60744c8eed01db2415b3
Janne Jalkanen <jalkanen@apache.org>
2002-02-10 21:04:11 +0000

    Initial version, just TIMEZONE checks.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@621421 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/com/ecyrd/jspwiki/xmlrpc/RPCHandlerTest.java
@@ -0,0 +1,79 @@
+
+package com.ecyrd.jspwiki.xmlrpc;
+
+import com.ecyrd.jspwiki.*;
+import junit.framework.*;
+import java.io.*;
+import java.util.*;
+import org.apache.log4j.*;
+
+public class RPCHandlerTest extends TestCase
+{
+    WikiEngine m_engine;
+    RPCHandler m_handler;
+    Properties m_props;
+
+    static final String NAME1 = "Test";
+
+    public RPCHandlerTest( String s )
+    {
+        super( s );
+    }
+
+    public void setUp()
+        throws Exception
+    {
+        m_props = new Properties();
+        m_props.load( getClass().getClassLoader().getResourceAsStream("/jspwiki.properties") );
+        m_engine = new TestEngine( m_props );
+
+        m_handler = new RPCHandler( m_engine );
+    }
+
+    public void tearDown()
+    {
+        String files = m_props.getProperty( FileSystemProvider.PROP_PAGEDIR );
+
+        File f = new File( files, NAME1+FileSystemProvider.FILE_EXT );
+
+        f.delete();
+    }
+
+    public void testPageInfo()
+        throws Exception
+    {
+        String text = "Foobar.";
+        String pageName = NAME1;
+
+        m_engine.saveText( pageName, text );
+
+        WikiPage directInfo = m_engine.getPage( NAME1 );
+
+        Hashtable ht = m_handler.getPageInfo( NAME1 );
+
+        assertEquals( "name", (String)ht.get( "name" ), NAME1 );
+        
+        Date d = (Date) ht.get( "lastModified" );
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime( d );
+
+        System.out.println("Real: "+directInfo.getLastModified() );
+        System.out.println("RPC:  "+d );
+
+        // Offset the ZONE offset and DST offset away.  DST only
+        // if we're actually in DST.
+        cal.add( Calendar.MILLISECOND, 
+                 (cal.get( Calendar.ZONE_OFFSET )+
+                  (cal.getTimeZone().inDaylightTime( d ) ? cal.get( Calendar.DST_OFFSET ) : 0 ) ) );
+        System.out.println("RPC2: "+cal.getTime() );
+
+        assertEquals( "date", cal.getTime().getTime(), 
+                      directInfo.getLastModified().getTime() );
+    }
+
+    public static Test suite()
+    {
+        return new TestSuite( RPCHandlerTest.class );
+    }
+}
