badf9161d7def76982df3c56336661168f2ab7a3
Janne Jalkanen <jalkanen@apache.org>
2002-03-07 21:16:01 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@621498 13f79535-47bb-0310-9956-ffa450edef68

--- tests/com/ecyrd/jspwiki/TestEngine.java
+++ tests/com/ecyrd/jspwiki/TestEngine.java
@@ -1,11 +1,16 @@
 
 package com.ecyrd.jspwiki;
 import java.util.Properties;
+import javax.servlet.*;
 
+/**
+ *  Simple test engine that always assumes pages are found.
+ */
 public class TestEngine extends WikiEngine
 {
     public TestEngine( Properties props )
-        throws NoRequiredPropertyException
+        throws NoRequiredPropertyException,
+               ServletException
     {
         super( props );
     }
--- tests/com/ecyrd/jspwiki/TestEngine2.java
+++ tests/com/ecyrd/jspwiki/TestEngine2.java
@@ -2,12 +2,14 @@
 package com.ecyrd.jspwiki;
 import java.util.Properties;
 import java.io.File;
+import javax.servlet.ServletException;
 
 /** Real test engine */
 public class TestEngine2 extends WikiEngine
 {
     public TestEngine2( Properties props )
-        throws NoRequiredPropertyException
+        throws NoRequiredPropertyException,
+               ServletException
     {
         super( props );
     }
