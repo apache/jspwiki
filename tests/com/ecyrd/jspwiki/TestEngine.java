9213c9571966b146688193f6d354aefc1cbc32c7
Janne Jalkanen <jalkanen@apache.org>
2002-03-10 18:04:55 +0000

    Added deleteAll(File)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@621514 13f79535-47bb-0310-9956-ffa450edef68

--- tests/com/ecyrd/jspwiki/TestEngine.java
+++ tests/com/ecyrd/jspwiki/TestEngine.java
@@ -2,6 +2,7 @@
 package com.ecyrd.jspwiki;
 import java.util.Properties;
 import javax.servlet.*;
+import java.io.*;
 
 /**
  *  Simple test engine that always assumes pages are found.
@@ -19,4 +20,29 @@ public class TestEngine extends WikiEngine
     {
         return true;
     }
+
+    /**
+     *  Deletes all files under this directory, and does them recursively.
+     */
+    public static void deleteAll( File file )
+    {
+        if( file.isDirectory() )
+        {
+            File[] files = file.listFiles();
+
+            for( int i = 0; i < files.length; i++ )
+            {
+                if( files[i].isDirectory() )
+                {
+                    deleteAll(files[i]);                
+                }
+
+                files[i].delete();
+            }
+        }
+
+        file.delete();
+    }
+
+
 }
