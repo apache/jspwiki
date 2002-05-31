6affa02ce8f2c28ce7b5cdbd8360d7194dd226b6
Janne Jalkanen <jalkanen@apache.org>
2002-05-31 07:21:17 +0000

    Added testDirectoryIsFile(), testNonExistantDirectory()
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@621620 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/com/ecyrd/jspwiki/FileSystemProviderTest.java
@@ -0,0 +1,101 @@
+
+package com.ecyrd.jspwiki;
+
+import junit.framework.*;
+import java.io.*;
+import java.util.*;
+
+public class FileSystemProviderTest extends TestCase
+{
+    FileSystemProvider m_provider;
+
+    public FileSystemProviderTest( String s )
+    {
+        super( s );
+    }
+
+    public void setUp()
+        throws Exception
+    {
+    }
+
+    public void tearDown()
+    {
+    }
+
+    public void testNonExistantDirectory()
+        throws Exception
+    {
+        String tmpdir = System.getProperties().getProperty("java.tmpdir");
+        String dirname = "non-existant-directory";
+
+        String newdir = tmpdir + File.separator + dirname;
+
+        Properties props = new Properties();
+
+        props.setProperty( FileSystemProvider.PROP_PAGEDIR, 
+                           newdir );
+
+        FileSystemProvider test = new FileSystemProvider();
+
+        try
+        {
+            test.initialize( props );
+
+            fail( "Wiki did not warn about wrong property." );
+        }
+        catch( IOException e )
+        {
+            if( e instanceof FileNotFoundException )
+            {
+                // This is okay.
+            }
+            else
+            {
+                fail("Wrong exception: "+e);
+            }
+        }
+    }
+
+    public void testDirectoryIsFile()
+        throws Exception
+    {
+        File tmpFile = null;
+
+        try
+        {
+            tmpFile = FileUtil.newTmpFile("foobar"); // Content does not matter.
+
+            Properties props = new Properties();
+
+            props.setProperty( FileSystemProvider.PROP_PAGEDIR, 
+                               tmpFile.getAbsolutePath() );
+
+            FileSystemProvider test = new FileSystemProvider();
+
+            try
+            {
+                test.initialize( props );
+
+                fail( "Wiki did not warn about wrong property." );
+            }
+            catch( IOException e )
+            {
+                // This is okay.
+            }
+        }
+        finally
+        {
+            if( tmpFile != null )
+            {
+                tmpFile.delete();
+            }
+        }
+    }
+
+
+    public static Test suite()
+    {
+        return new TestSuite( FileSystemProviderTest.class );
+    }
+}
