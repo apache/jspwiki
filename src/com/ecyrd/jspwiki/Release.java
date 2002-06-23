cdb6cc5f6743a85f5094a5da8a52c8801ad5feb1
Janne Jalkanen <jalkanen@apache.org>
2002-06-23 21:07:34 +0000

    1.8.1
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/branches/JSPWIKI_1_8_BRANCH@621668 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/Release.java
+++ src/com/ecyrd/jspwiki/Release.java
@@ -44,7 +44,7 @@ public class Release
 
     public static final int        VERSION       = 1;
     public static final int        REVISION      = 8;
-    public static final int        MINORREVISION = 0;
+    public static final int        MINORREVISION = 1;
 
     /**
      *  This is the generic version string you should use
