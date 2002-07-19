abefd0be1b0bd799f20406e17c2b62d0a5fab8cb
Janne Jalkanen <jalkanen@apache.org>
2002-07-19 10:14:49 +0000

    Initial version, many tests.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@621738 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/com/ecyrd/jspwiki/UserProfileTest.java
@@ -0,0 +1,85 @@
+
+package com.ecyrd.jspwiki;
+
+import junit.framework.*;
+import java.io.*;
+import java.util.*;
+import org.apache.log4j.*;
+
+/**
+ *  Tests the UserProfile class.
+ *  @author Janne Jalkanen
+ */
+public class UserProfileTest extends TestCase
+{
+    public UserProfileTest( String s )
+    {
+        super( s );
+        Properties props = new Properties();
+        try
+        {
+            props.load( getClass().getClassLoader().getResourceAsStream("/jspwiki.properties") );
+            PropertyConfigurator.configure(props);
+        }
+        catch( IOException e ) {}
+    }
+
+    public void setUp()
+        throws Exception
+    {
+    }
+
+    public void tearDown()
+    {
+    }
+
+    public void testStringRepresentation()
+        throws Exception
+    {
+        UserProfile p = new UserProfile("username=JanneJalkanen");
+
+        assertEquals( "name", "JanneJalkanen",p.getName() );
+    }
+
+    /**
+     *  Sometimes not all servlet containers offer you correctly
+     *  decoded cookies.  Reported by KalleKivimaa.
+     */
+    public void testBrokenStringRepresentation()
+        throws Exception
+    {
+        UserProfile p = new UserProfile("username%3DJanneJalkanen");
+
+        assertEquals( "name", "JanneJalkanen",p.getName() );
+    }
+
+    public void testUTFStringRepresentation()
+        throws Exception
+    {
+        UserProfile p = new UserProfile("username=MääMöö");
+
+        assertEquals( "name", "Määmöö",p.getName() );
+    }
+
+    public void testUTFURLStringRepresentation()
+        throws Exception
+    {
+        UserProfile p = new UserProfile("username="+TextUtil.urlEncodeUTF8("Määmöö"));
+
+        assertEquals( "name", "Määmöö",p.getName() );
+    }
+
+    public void testBrokenUTFStringRepresentation()
+        throws Exception
+    {
+        UserProfile p = new UserProfile("username%3DMääMöö");
+
+        assertEquals( "name", "Määmöö",p.getName() );
+    }
+
+
+    public static Test suite()
+    {
+        return new TestSuite( UserProfileTest.class );
+    }
+}
