dd81b35744a76e6c5b10ff26a06a83493f54965f
Janne Jalkanen <jalkanen@apache.org>
2002-07-31 22:04:48 +0000

    Initial version.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@621821 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/ecyrd/jspwiki/tags/PageInfoLinkTag.java
@@ -0,0 +1,99 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2001-2002 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.ecyrd.jspwiki.tags;
+
+import java.io.IOException;
+import javax.servlet.jsp.JspWriter;
+
+import com.ecyrd.jspwiki.WikiEngine;
+import com.ecyrd.jspwiki.WikiPage;
+
+/**
+ *  Writes a link to the Wiki PageInfo.  Body of the link becomes the actual text.
+ *
+ *  <P><B>Attributes<B></P>
+ *  <UL>
+ *    <LI>page - Page name to refer to.  Default is the current page.
+ *  </UL>
+ *
+ *  @author Janne Jalkanen
+ *  @since 2.0
+ */
+// FIXME: Refactor together with LinkToTag and EditLinkTag.
+public class PageInfoLinkTag
+    extends WikiTagBase
+{
+    protected String m_pageName;
+
+    public void setPage( String page )
+    {
+        m_pageName = page;
+    }
+
+    public String getPage()
+    {
+        return m_pageName;
+    }
+
+    public final int doWikiStartTag()
+        throws IOException
+    {
+        WikiEngine engine = m_wikiContext.getEngine();
+        String     pageName = m_pageName;
+
+        if( m_pageName == null )
+        {
+            if( m_wikiContext.getPage() != null )
+            {
+                pageName = m_wikiContext.getPage().getName();
+            }
+            else
+            {
+                return SKIP_BODY;
+            }
+        }
+
+        if( engine.pageExists(pageName) )
+        {
+            JspWriter out = pageContext.getOut();
+            String encodedlink = engine.encodeName( pageName );
+
+            out.print("<A HREF=\""+engine.getBaseURL()+"PageInfo.jsp?page="+encodedlink+"\">");
+
+            return EVAL_BODY_INCLUDE;
+        }
+
+        return SKIP_BODY;
+    }
+
+    public int doEndTag()
+    {
+        try
+        {
+            pageContext.getOut().print("</A>");
+        }
+        catch( IOException e )
+        {
+            // FIXME: Should do something?
+        }
+
+        return EVAL_PAGE;
+    }
+}
