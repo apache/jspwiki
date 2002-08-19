ddfea2c4ff36b3d1b794d554e15c597b25763ff8
Janne Jalkanen <jalkanen@apache.org>
2002-08-19 20:45:09 +0000

    Moved TestEngine2 code here.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@621889 13f79535-47bb-0310-9956-ffa450edef68

--- tests/com/ecyrd/jspwiki/TestEngine.java
+++ tests/com/ecyrd/jspwiki/TestEngine.java
@@ -4,20 +4,21 @@ import java.util.Properties;
 import javax.servlet.*;
 import java.io.*;
 
+import org.apache.log4j.Category;
+
+import com.ecyrd.jspwiki.providers.*;
+
 /**
  *  Simple test engine that always assumes pages are found.
  */
 public class TestEngine extends WikiEngine
 {
+    static Category log = Category.getInstance( TestEngine.class );
+
     public TestEngine( Properties props )
         throws WikiException
     {
         super( props );
-    }
-
-    public boolean pageExists( String page )
-    {
-        return true;
     }
 
     /**
@@ -43,5 +44,32 @@ public class TestEngine extends WikiEngine
         file.delete();
     }
 
+    /**
+     *  Copied from FileSystemProvider
+     */
+    protected String mangleName( String pagename )
+    {
+        // FIXME: Horrible kludge, very slow, etc.
+        if( "UTF-8".equals( getContentEncoding() ) )
+            return TextUtil.urlEncodeUTF8( pagename );
+
+        return java.net.URLEncoder.encode( pagename );
+    }
+
+    public void deletePage( String name )
+    {
+        String files = getWikiProperties().getProperty( FileSystemProvider.PROP_PAGEDIR );
+
+        try
+        {
+            File f = new File( files, mangleName(name)+FileSystemProvider.FILE_EXT );
+
+            f.delete();
+        }
+        catch( Exception e ) 
+        {
+            log.error("Couldn't delete "+name, e );
+        }
+    }
 
 }
