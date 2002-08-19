40d65be422c17e9c5143d19bd1c0cb6d5065a04a
Janne Jalkanen <jalkanen@apache.org>
2002-08-19 20:43:18 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@621886 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/com/ecyrd/jspwiki/plugin/UndefinedPagesPluginTest.java
@@ -0,0 +1,68 @@
+
+package com.ecyrd.jspwiki.plugin;
+
+import com.ecyrd.jspwiki.*;
+import junit.framework.*;
+import java.io.*;
+import java.util.*;
+
+public class UndefinedPagesPluginTest extends TestCase
+{
+    Properties props = new Properties();
+    TestEngine engine;
+    WikiContext context;
+    PluginManager manager;
+    
+    public UndefinedPagesPluginTest( String s )
+    {
+        super( s );
+    }
+
+    public void setUp()
+        throws Exception
+    {
+        props.load( getClass().getClassLoader().getResourceAsStream("/jspwiki.properties") );
+
+        engine = new TestEngine(props);
+
+        engine.saveText( "TestPage", "Reference to [Foobar]." );
+        engine.saveText( "Foobar", "Reference to [Foobar2], [Foobars]" );
+
+        context = new WikiContext( engine, "TestPage" );
+        manager = new PluginManager( props );
+    }
+
+    public void tearDown()
+    {
+        engine.deletePage( "TestPage" );
+        engine.deletePage( "Foobar" );
+    }
+
+    private String wikitize( String s )
+    {
+        return engine.textToHTML( context, s );
+    }
+
+    /**
+     *  Tests that only correct undefined links are found.
+     *  We also check against plural forms here, which should not
+     *  be listed as non-existant.
+     */
+    public void testSimpleUndefined()
+        throws Exception
+    {
+        WikiContext context2 = new WikiContext( engine, "Foobar" );
+
+        String res = manager.execute( context2,
+                                      "{INSERT com.ecyrd.jspwiki.plugin.UndefinedPagesPlugin");
+
+        String exp = "[Foobar2]\n\\\\";
+
+        assertEquals( wikitize(exp), res );
+    }
+
+    public static Test suite()
+    {
+        return new TestSuite( UndefinedPagesPluginTest.class );
+    }
+}
