53aa1c939de64ee9d7e2b5268cf60dcb2e6639c1
Janne Jalkanen <jalkanen@apache.org>
2002-08-28 21:41:35 +0000

    Initial version.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@621897 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/ecyrd/jspwiki/tags/CheckRequestContextTag.java
@@ -0,0 +1,63 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2001-2002 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.ecyrd.jspwiki.tags;
+
+import java.io.IOException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.JspException;
+import javax.servlet.ServletException;
+
+import com.ecyrd.jspwiki.WikiEngine;
+import com.ecyrd.jspwiki.WikiPage;
+import com.ecyrd.jspwiki.providers.ProviderException;
+
+/**
+ *  Includes body, if the request context matches.
+ *
+ *  @author Janne Jalkanen
+ *  @since 2.0
+ */
+public class CheckRequestContextTag
+    extends WikiTagBase
+{
+    private String m_context;
+
+    public String getContext()
+    {
+        return m_context;
+    }
+
+    public void setContext( String arg )
+    {
+        m_context = arg;
+    }
+
+    public final int doWikiStartTag()
+        throws IOException,
+               ProviderException
+    {
+        if( m_wikiContext.getRequestContext().equalsIgnoreCase(getContext()) )
+        {
+            return EVAL_BODY_INCLUDE;
+        }
+
+        return SKIP_BODY;
+    }
+}
