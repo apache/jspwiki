5f1f5f4a87178c152b400d8010ece35ca6346eab
Janne Jalkanen <jalkanen@apache.org>
2002-10-23 18:44:23 +0000

    Initial version.  Mostly incorporated ebu code.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@622024 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/ecyrd/jspwiki/providers/WikiAttachmentProvider.java
@@ -0,0 +1,79 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2001-2002 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.ecyrd.jspwiki.providers;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import java.util.Collection;
+
+import com.ecyrd.jspwiki.*;
+import com.ecyrd.jspwiki.attachment.Attachment;
+
+/**
+ *  Defines an attachment provider - a class which is capable of saving
+ *  binary data as attachments.
+ *  <P>
+ *  The difference between this class and WikiPageProvider is that there
+ *  PageProviders handle Unicode text, whereas we handle binary data.
+ *  While there are quite a lot of similarities in how we handle
+ *  things, many providers can really use just one.  In addition,
+ *  since binary files can be really large, we rely on
+ *  Input/OutputStreams.
+ *
+ *  @author Erik Bunn
+ *  @author Janne Jalkanen
+ */
+public interface WikiAttachmentProvider
+    extends WikiProvider
+{
+    /**
+     *  Put new attachment data.
+     */
+    public void putAttachmentData( Attachment att, InputStream data );
+
+    /**
+     *  Get attachment data.
+     */
+
+    public OutputStream getAttachmentData( Attachment att );
+
+    /**
+     *  Returns true if the attachment exists.
+     */
+    public boolean attachmentExists( String page );
+
+    /**
+     * Finds attachments based on the query.
+     */
+    public Collection findAttachments( QueryItem[] query );
+
+    /**
+     *  Returns info about the attachment.
+     */
+    public Attachment getAttachmentInfo( String name, int version );
+    /**
+     *  Returns version history.  Each element should be
+     *  an Attachment.
+     */
+    public Collection getVersionHistory( String wikiname );
+}
+
+
