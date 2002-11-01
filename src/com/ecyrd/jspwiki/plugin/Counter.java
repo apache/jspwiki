435a78674ec093087beb9a80efee5c86cdf4775b
Janne Jalkanen <jalkanen@apache.org>
2002-11-01 20:53:56 +0000

    Added.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@622036 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/ecyrd/jspwiki/plugin/Counter.java
@@ -0,0 +1,83 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2002 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.ecyrd.jspwiki.plugin;
+
+import org.apache.log4j.Category;
+import com.ecyrd.jspwiki.*;
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+/**
+ *  Provides a page-specific counter.
+ *  <P>Parameters
+ *  <UL>
+ *    <LI>name - Name of the counter.  Optional.
+ *  </UL>
+ *
+ *  Stores a variable in the WikiContext called "counter", with the name of the
+ *  optionally attached.  For example:<BR>
+ *  If name is "thispage", then the variable name is called "counter-thispage".
+ *
+ *  @since 1.9.30
+ *  @author Janne Jalkanen
+ */
+public class Counter
+    implements WikiPlugin
+{
+    private static Category log = Category.getInstance( Counter.class );
+
+    static final String VARIABLE_NAME = "counter";
+
+    public String execute( WikiContext context, Map params )
+        throws PluginException
+    {
+        //
+        //  First, determine which kind of name we use to store in
+        //  the WikiContext.
+        //
+        String  countername = (String)params.get( "name" );
+
+        if( countername == null ) 
+        {
+            countername = VARIABLE_NAME;
+        }
+        else
+        {
+            countername = VARIABLE_NAME+"-"+countername;
+        }
+
+        //
+        //  Fetch, increment, and store back.
+        //
+        Integer val = (Integer)context.getVariable( countername );
+
+        if( val == null )
+        {
+            val = new Integer( 0 );
+        }
+
+        val = new Integer( val.intValue() + 1 );
+
+        context.setVariable( countername, val );
+
+        return val.toString();
+    }
+
+}
