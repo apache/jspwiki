1c93006f66444bf367350a4cabc4dc3d058c94ea
Janne Jalkanen <jalkanen@apache.org>
2002-12-04 12:05:16 +0000

    Javadocs.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@622163 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/plugin/WikiPlugin.java
+++ src/com/ecyrd/jspwiki/plugin/WikiPlugin.java
@@ -23,10 +23,36 @@ import com.ecyrd.jspwiki.*;
 import java.util.Map;
 
 /**
- *  Defines an interface for plugins.
+ *  Defines an interface for plugins.  Any instance of a wiki plugin
+ *  should implement this interface.
+ *
+ *  @author Janne Jalkanen
  */
 public interface WikiPlugin
 {
+    /**
+     *  This is the main entry point for any plugin.  The parameters are parsed,
+     *  and a special parameter called "_body" signifies the name of the plugin
+     *  body, i.e. the part of the plugin that is not a parameter of
+     *  the form "key=value".  This has been separated using an empty
+     *  line.
+     *  <P>
+     *  Note that it is preferred that the plugin returns
+     *  XHTML-compliant HTML (i.e. close all tags, use &lt;br /&gt;
+     *  instead of &lt;br&gt;, etc.
+     *
+     *  @param context The current WikiContext.
+     *  @param params  A Map which contains key-value pairs.  Any
+     *                 parameter that the user has specified on the
+     *                 wiki page will contain String-String
+     *  parameters, but it is possible that at some future date,
+     *  JSPWiki will give you other things that are not Strings.
+     *
+     *  @return HTML, ready to be included into the rendered page.
+     *
+     *  @throws PluginException In case anything goes wrong.
+     */
+
     public String execute( WikiContext context, Map params )
         throws PluginException;
 }
