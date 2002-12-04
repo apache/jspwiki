44d1cad46951581aa4b67e30fe0c4dbb122bd85b
Janne Jalkanen <jalkanen@apache.org>
2002-12-04 12:43:19 +0000

    Javadoc fix.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@622166 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/WikiContext.java
+++ src/com/ecyrd/jspwiki/WikiContext.java
@@ -22,6 +22,24 @@ package com.ecyrd.jspwiki;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ *  Provides state information throughout the processing of a page.  A
+ *  WikiContext is born when the JSP pages that are the main entry
+ *  points, are invoked.  The JSPWiki engine creates the new
+ *  WikiContext, which basically holds information about the page, the
+ *  handling engine, and in which context (view, edit, etc) the
+ *  call was done.
+ *  <P>
+ *  A WikiContext also provides request-specific variables, which can
+ *  be used to communicate between plugins on the same page, or
+ *  between different instances of the same plugin.  A WikiContext
+ *  variable is valid until the processing of the page has ended.  For
+ *  an example, please see the Counter plugin.
+ *
+ *  @see com.ecyrd.jspwiki.plugin.Counter
+ *  
+ *  @author Janne Jalkanen
+ */
 public class WikiContext
 {
     WikiPage   m_page;
@@ -30,43 +48,83 @@ public class WikiContext
 
     Map        m_variableMap = new HashMap();
 
+    /** The VIEW context - the user just wants to view the page
+        contents. */
     public static final String    VIEW     = "view";
+
+    /** The EDIT context - the user is editing the page. */
     public static final String    EDIT     = "edit";
+
+    /** User is viewing a DIFF between the two versions of the page. */
     public static final String    DIFF     = "diff";
+
+    /** User is viewing page history. */
     public static final String    INFO     = "info";
+
+    /** User is previewing the changes he just made. */
     public static final String    PREVIEW  = "preview";
+
+    /** User has an internal conflict, and does quite not know what to
+        do. Please provide some counseling. */
     public static final String    CONFLICT = "conflict";
 
+    /**
+     *  Create a new WikiContext.
+     *
+     *  @param engine The WikiEngine that is handling the request.
+     *  @param pagename The name of the page.  A new WikiPage is
+     *         created.
+     */
     public WikiContext( WikiEngine engine, String pagename )
     {
         m_page   = new WikiPage( pagename );
         m_engine = engine;
     }
 
+    /**
+     *  Create a new WikiContext for the given WikiPage.
+     *
+     *  @param engine The WikiEngine that is handling the request.
+     *  @param page   The WikiPage.  If you want to create a
+     *  WikiContext for an older version of a page, you must use this
+     *  constructor. 
+     */
     public WikiContext( WikiEngine engine, WikiPage page )
     {
         m_page   = page;
         m_engine = engine;
     }
 
+    /**
+     *  Returns the handling engine.
+     */
     public WikiEngine getEngine()
     {
         return m_engine;
     }
 
+    /**
+     *  Returns the page that is being handled.
+     */
     public WikiPage getPage()
     {
         return m_page;
     }
 
 
+    /**
+     *  Returns the request context.
+     */
     public String getRequestContext()
     {
         return m_requestContext;
     }
 
     /**
-     *  Sets the request context.
+     *  Sets the request context.  See above for the different
+     *  request contexts (VIEW, EDIT, etc.)
+     *
+     *  @param arg The request context (one of the predefined contexts.)
      */
     public void setRequestContext( String arg )
     {
@@ -75,6 +133,9 @@ public class WikiContext
 
     /**
      *  Gets a previously set variable.
+     *
+     *  @param key The variable name.
+     *  @return The variable contents.
      */
     public Object getVariable( String key )
     {
@@ -85,6 +146,9 @@ public class WikiContext
      *  Sets a variable.  The variable is valid while the WikiContext is valid,
      *  i.e. while page processing continues.  The variable data is discarded
      *  once the page processing is finished.
+     *
+     *  @param key The variable name.
+     *  @param data The variable value.
      */
     public void setVariable( String key, Object data )
     {
