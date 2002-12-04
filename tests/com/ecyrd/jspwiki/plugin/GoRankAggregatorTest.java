a6727b9ac0840b2d3999f0d4d441bb8f95221e9e
Janne Jalkanen <jalkanen@apache.org>
2002-12-04 11:02:42 +0000

    Added testAggregate3()
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@622160 13f79535-47bb-0310-9956-ffa450edef68

--- tests/com/ecyrd/jspwiki/plugin/GoRankAggregatorTest.java
+++ tests/com/ecyrd/jspwiki/plugin/GoRankAggregatorTest.java
@@ -188,6 +188,49 @@ public class GoRankAggregatorTest extends TestCase
         assertEquals("User3 club", "Hobbiton", u3.m_club);
     }
 
+    /**
+     *  Check that igs/kgs nicks work.
+     */
+    public void testAggregate3()
+        throws Exception
+    {
+        testEngine.saveText("Rating1", "----\n\n{{{\nClub: Helsinki\n"+
+                            "Jalkanen, Janne, 7k, igs:Ecyrd, kgs:Blob\n"+
+                            "Eskelinen, Olli, 2k, KGS: Olli\n"+
+                            "\n"+
+                            "Club: Hobbiton\n"+
+                            "Gobble, Foo, 2k\n\n}}}\n");
+
+        List results = agg.getPersonsFromPage( context, "Rating1" );
+
+        assertEquals( "size", 3, results.size() );
+
+        GoRankAggregator.UserInfo u1 = (GoRankAggregator.UserInfo) results.get(0);
+        GoRankAggregator.UserInfo u2 = (GoRankAggregator.UserInfo) results.get(1);
+        GoRankAggregator.UserInfo u3 = (GoRankAggregator.UserInfo) results.get(2);
+
+        assertEquals("User1 igs", "Ecyrd", u1.getNick("igs") );
+        assertEquals("User1 kgs", "Blob", u1.getNick("kgs") );
+
+        assertEquals("User2 kgs", "Olli", u2.getNick("kgs") );
+        assertNull("User2 igs", u2.getNick("igs") );
+
+        assertNull("User3 kgs", u3.getNick("kgs") );
+        assertNull("User3 igs", u3.getNick("igs") );
+
+        assertEquals("User1 name", "Jalkanen", u1.m_lastName );
+        assertEquals("User2 name", "Eskelinen", u2.m_lastName );
+        assertEquals("User3 name", "Gobble", u3.m_lastName );
+
+        assertEquals("User1 rank", 7, u1.m_rank );
+        assertEquals("User2 rank", 2, u2.m_rank );
+        assertEquals("User3 rank", 2, u3.m_rank );
+
+        assertEquals("User1 club", "Helsinki", u1.m_club);
+        assertEquals("User2 club", "Helsinki", u2.m_club);
+        assertEquals("User3 club", "Hobbiton", u3.m_club);
+    }
+
     public static Test suite()
     {
         return new TestSuite( GoRankAggregatorTest.class );
