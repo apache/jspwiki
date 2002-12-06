befba91e08e3d07d7e37ea945408603c0ab95147
Janne Jalkanen <jalkanen@apache.org>
2002-12-06 14:19:59 +0000

    Returns the size of the page or attachment.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@622193 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/ecyrd/jspwiki/tags/PageSizeTag.java
@@ -0,0 +1,70 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2001-2002 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.ecyrd.jspwiki.tags;
+
+import java.io.IOException;
+
+import com.ecyrd.jspwiki.WikiEngine;
+import com.ecyrd.jspwiki.WikiPage;
+import com.ecyrd.jspwiki.providers.ProviderException;
+import com.ecyrd.jspwiki.attachment.Attachment;
+
+/**
+ *  Returns the currently requested page or attachment size.
+ *
+ *  @author Janne Jalkanen
+ *  @since 2.0
+ */
+public class PageSizeTag
+    extends WikiTagBase
+{
+    public final int doWikiStartTag()
+        throws IOException
+    {
+        WikiEngine engine = m_wikiContext.getEngine();
+        WikiPage   page   = m_wikiContext.getPage();
+
+        try
+        {
+            if( page != null && engine.pageExists(page) )
+            {
+                long size = -1;
+
+                if( page instanceof Attachment )
+                {
+                    size = ((Attachment) page).getSize();
+                }
+                else
+                {
+                    size = engine.getPureText( page.getName(), page.getVersion() ).length();
+                }
+
+                pageContext.getOut().write( Long.toString(size) );
+            }
+        }
+        catch( ProviderException e )
+        {
+            log.warn("Providers did not work: ",e);
+            pageContext.getOut().write("Error determining page size: "+e.getMessage());
+        }
+
+        return SKIP_BODY;
+    }
+}
