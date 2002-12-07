5613c291bf2c5493f67573458b18a8b503b2cb54
Janne Jalkanen <jalkanen@apache.org>
2002-12-07 20:28:14 +0000

    Added code for image caching.  However, it is not enabled due to some strange faults.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@622232 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/plugin/GoDiagram.java
+++ src/com/ecyrd/jspwiki/plugin/GoDiagram.java
@@ -22,18 +22,39 @@ package com.ecyrd.jspwiki.plugin;
 import org.apache.log4j.Category;
 import com.ecyrd.jspwiki.*;
 import java.util.*;
+import java.math.BigInteger;
 
 import java.io.StreamTokenizer;
 import java.io.StringReader;
 import java.io.IOException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ByteArrayInputStream;
+
+import java.awt.*;
+import java.awt.image.*;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.servlet.ServletContext;
+import javax.swing.ImageIcon;
+
+import com.keypoint.PngEncoderB;
 
 /**
  *  Plugin for doing go game diagrams.
  *
  *  @author Janne Jalkanen
  */
+
+// FIXME: Still alive, so very little documentation.
+// FIXME: Does not remove cached files.
+// FIXME: Still has many magic numbers.
+
 public class GoDiagram
-    implements WikiPlugin
+    implements WikiPlugin,
+               ImageObserver
 {
     private static Category log = Category.getInstance( GoDiagram.class );
 
@@ -45,25 +66,35 @@ public class GoDiagram
     {
     }
 
+    public boolean imageUpdate( Image img, int infoflags,
+                                int x,
+                                int y,
+                                int width,
+                                int height)
+    {
+        return true;
+    }
+
+    /**
+     *  Return the proper URL to the image location.
+     */
+    private String getImageLocation( WikiContext context, String content )
+    {
+        return context.getEngine().getBaseURL()+"images/godiagram/"+content+".png";
+    }
+
+    /**
+     *  For the table-based system, returns the image data.
+     */
     private String makeImage( WikiContext context, String content )
     {
-        //return "<IMG SRC=\"images/diagram/"+content+".gif\">";
-        return "<TD><IMG SRC=\""+context.getEngine().getBaseURL()+"images/godiagram/"+content+".png\"></TD>";
+        // return "<TD><IMG SRC=\""+getImageLocation(context,content)+"\"></TD>";
+        return "<IMG SRC=\""+getImageLocation(context,content)+"\">";
     }
 
-    public class DiagramInfo
-    {
-        boolean topSide = false;
-        boolean bottomSide = false;
-        boolean leftSide = false;
-        boolean rightSide = false;
-
-        int numRows = 0;
-        int numCols = 0;
-
-        Object[][] contents;
-    }
-
+    /**
+     *  Parses diagram and returns a filled DiagramInfo structure.
+     */
     protected DiagramInfo getDiagramInfo( String dia )
     {
         DiagramInfo info = new DiagramInfo();
@@ -140,6 +171,10 @@ public class GoDiagram
         return info;
     }
 
+    /**
+     *  Returns the proper file name for an empty square,
+     *  depending on the grid location.
+     */
     private String insertEmpty( DiagramInfo info, int row, int col )
     {
         if( row == 0 && info.topSide )
@@ -189,12 +224,13 @@ public class GoDiagram
         StringBuffer res = new StringBuffer();
 
         //res.append("<DIV CLASS=\"diagram\">\n");
-        res.append("<TABLE BORDER=\"0\" CELLSPACING=\"0\" CELLPADDING=\"0\">");
+        //res.append("<TABLE BORDER=\"0\" CELLSPACING=\"0\" CELLPADDING=\"0\">");
+        res.append("<pre>");
         int type;
 
         for( int row = 0; row < info.numRows; row++ )
         {
-            res.append("<TR>");
+            // res.append("<TR>");
 
             for( int col = 0; col < info.numCols; col++ )
             {
@@ -234,18 +270,215 @@ public class GoDiagram
                 }
             } // col
 
-            res.append("</TR>\n");
+            // res.append("</TR>\n");
+            res.append("<BR>");
         } // row
 
 
         //res.append("</DIV>\n");
-        res.append("</TABLE>");
+        res.append("</pre>\n");
+        //res.append("</TABLE>");
 
         return res.toString();
     }
 
+    private String getFilePath( byte[] data )
+    {
+        String filepart = (new BigInteger(1, data)).toString(36);
+        filepart += ".png";
+
+        return filepart;
+    }
+
+    private File findDiagramFileByDigest( WikiContext context, byte[] data )
+    {
+        ServletContext sc = context.getEngine().getServletContext();
+
+        String fileLocation = sc.getRealPath( "/images/godiagram/cache" );
+
+        File file = new File( fileLocation, getFilePath(data) );
+
+        return file;
+    }
+
+    private String findDiagramURLByDigest( WikiContext context, byte[] data )
+    {
+        File file = findDiagramFileByDigest( context, data );
+
+        // System.out.println("Attempting to locate "+file.getAbsolutePath());
+        
+        if( file.exists() )
+        {
+            return context.getEngine().getBaseURL()+"images/godiagram/cache/"+getFilePath(data);
+        }
+
+        return null;
+    }
+
+    private BufferedImage getImage( WikiContext context, String name )
+    {
+        ServletContext sc   = context.getEngine().getServletContext();
+
+        String fileLocation = sc.getRealPath( "/images/godiagram" );
+
+        File f = new File( fileLocation, name+".png" );
+
+        // System.out.println("   Loading file"+f.getAbsolutePath());
+
+        ImageIcon icon = new ImageIcon( f.getAbsolutePath() );
+        Image img = icon.getImage();
+        
+        BufferedImage bi = new BufferedImage( img.getWidth(null), 
+                                              img.getHeight(null), 
+                                              BufferedImage.TYPE_4BYTE_ABGR );
+
+        Graphics2D biContext = bi.createGraphics();
+
+        biContext.drawImage(img, 0, 0, null);
+
+        return bi;
+    }
+
+    private void blitImage( BufferedImage dest, BufferedImage src, int row, int col )
+    {
+        Raster srcRaster = src.getData();
+
+        Graphics targetGfx = dest.getGraphics();
+
+        targetGfx.drawImage( src, col, row, null );
+    }
+
+    private void blitImage( WikiContext context, BufferedImage dest, 
+                            String code, int row, int col )
+    {
+        BufferedImage src = getImage( context, code );
+
+        blitImage( dest, src, row*15, col*15 );
+    }
+
+    /**
+     *  Writes image data to a temporary cache file.
+     */
+    private void saveImage( WikiContext context, byte[] digest, BufferedImage img )
+        throws IOException
+    {
+        FileOutputStream     out = null;
+        ByteArrayInputStream in  = null;
+
+        try
+        {
+            File dest = findDiagramFileByDigest( context, digest );
+
+            dest.getParentFile().mkdirs();
+
+            PngEncoderB enc = new PngEncoderB( img, true );
+        
+            out = new FileOutputStream( dest );
+            in  = new ByteArrayInputStream( enc.pngEncode() );
+
+            FileUtil.copyContents( in, out );
+        }
+        finally
+        {
+            if( in != null )  in.close();
+            if( out != null ) out.close();
+        }
+    }
+
+    /**
+     *  @param first 'b', if black should have the first move, 'w' otherwise.
+     */
+    private String parseDiagramCache( WikiContext context, String dia, int first )
+        throws IOException
+    {
+        //
+        //  Get the message digest and see whether there already exists such
+        //  an image.
+        //
+
+        byte[] digData;
+
+        try
+        {
+            MessageDigest digest = MessageDigest.getInstance("SHA");
+            digData = digest.digest( dia.getBytes() );
+        }
+        catch( NoSuchAlgorithmException e )
+        {
+            log.fatal("SHA algorithm support is missing!",e);
+            return "<I>Error, check logs</I>";
+        }
+
+        String url = findDiagramURLByDigest( context, digData );
+
+        if( url != null )
+        {
+            return "<IMG SRC=\""+url+"\">";
+        }
+
+        DiagramInfo info = getDiagramInfo( dia );
+        int imageWidth  = 15; // FIXME: Magic
+        int imageHeight = 15; // FIXME: Magic
+
+        BufferedImage buf = new BufferedImage( info.numCols*imageWidth,
+                                               info.numRows*imageHeight,
+                                               BufferedImage.TYPE_4BYTE_ABGR );
+
+        Graphics2D biContext = buf.createGraphics();
+        biContext.setColor( Color.white );
+        biContext.fillRect( 0, 0, buf.getWidth(), buf.getHeight() );
+
+        for( int row = 0; row < info.numRows; row++ )
+        {
+            for( int col = 0; col < info.numCols; col++ )
+            {
+                String item = (String)info.contents[row][col];
+
+                if( Character.isDigit(item.charAt(0)) )
+                {
+                    int num = Integer.parseInt( item );
+
+                    String which = (num % 2 == first) ? "b" : "w";
+                    blitImage( context, buf, which+Integer.toString(num),
+                               row, col );
+                    continue;
+                }
+
+                switch( item.charAt(0) )
+                {
+                  case '#':
+                  case 'X':
+                    blitImage( context, buf, "b", row, col );
+                    break;
+
+                  case 'O':
+                    blitImage( context, buf, "w", row, col );
+                    break;
+
+                  case '.':
+                    blitImage( context, buf, insertEmpty( info, row, col ),
+                               row, col );
+                    break;
+
+                  case ',':
+                    blitImage( context, buf, "hoshi", row, col );
+                    break;
+
+                  default:
+                    blitImage( context, buf, "lc"+item, row, col );
+                    break;
+                }
+            } // col
+        } // row
+
+        saveImage( context, digData, buf );
+
+        return "<IMG SRC=\""+findDiagramURLByDigest( context, digData )+"\">";
+    }
+
     // FIXME: Parameters should be checked against HTML entities.
     // FIXME: "label" should be run through parser
+    // FIXME: "align" should be cleaned from crud.
 
     public String execute( WikiContext context, Map params )
         throws PluginException
@@ -294,4 +527,22 @@ public class GoDiagram
             throw new PluginException("Error in diagram: "+e.getMessage());
         }
     }
+
+    /**
+     *  This class just stores everything that we need about
+     *  the diagram.
+     */
+    public class DiagramInfo
+    {
+        boolean topSide    = false;
+        boolean bottomSide = false;
+        boolean leftSide   = false;
+        boolean rightSide  = false;
+
+        int     numRows    = 0;
+        int     numCols    = 0;
+
+        Object[][] contents;
+    }
+
 }
