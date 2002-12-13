75e8ee3ef6e7834dfccb04fa4908f128b9d3018e
Janne Jalkanen <jalkanen@apache.org>
2002-12-13 09:29:17 +0000

    Reorganized testBrokenPageText().
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@622322 13f79535-47bb-0310-9956-ffa450edef68

--- tests/com/ecyrd/jspwiki/TranslatorReaderTest.java
+++ tests/com/ecyrd/jspwiki/TranslatorReaderTest.java
@@ -15,100 +15,6 @@ public class TranslatorReaderTest extends TestCase
 
     TestEngine testEngine;
 
-    // This is a random find: the following page text caused an eternal loop in V2.0.x.
-    public static final String brokenPageText = 
-        "Please ''check [RecentChanges].\n" + 
-        "\n" + 
-        "Testing. fewfwefe\n" + 
-        "\n" + 
-        "CHeck [testpage]\n" + 
-        "\n" + 
-        "More testing.\n" + 
-        "dsadsadsa''\n" + 
-        "Is this {{truetype}} or not?\n" + 
-        "What about {{{This}}}?\n" + 
-        "How about {{this?\n" + 
-        "\n" + 
-        "{{{\n" + 
-        "{{text}}\n" + 
-        "}}}\n" + 
-        "goo\n" + 
-        "\n" + 
-        "<B>Not bold</b>\n" + 
-        "\n" + 
-        "motto\n" + 
-        "\n" + 
-        "* This is a list which we\n" + 
-        "shall continue on a other line.\n" + 
-        "* There is a list item here.\n" + 
-        "*  Another item.\n" + 
-        "* More stuff, which continues\n" + 
-        "on a second line.  And on\n" + 
-        "a third line as well.\n" + 
-        "And a fourth line.\n" + 
-        "* Third item.\n" + 
-        "\n" + 
-        "Foobar.\n" + 
-        "\n" + 
-        "----\n" + 
-        "\n" + 
-        "!!!Really big heading\n" + 
-        "Text.\n" + 
-        "!! Just a normal heading [with a hyperlink|Main]\n" + 
-        "More text.\n" + 
-        "!Just a small heading.\n" + 
-        "\n" + 
-        "This should be __bold__ text.\n" + 
-        "\n" + 
-        "__more bold text continuing\n" + 
-        "on the next line.__\n" + 
-        "\n" + 
-        "__more bold text continuing\n" + 
-        "\n" + 
-        "on the next paragraph.__\n" + 
-        "\n" + 
-        "\n" + 
-        "This should be normal.\n" + 
-        "\n" + 
-        "Now, let's try ''italic text''.\n" + 
-        "\n" + 
-        "Bulleted lists:\n" + 
-        "* One\n" + 
-        "Or more.\n" + 
-        "* Two\n" + 
-        "\n" + 
-        "** Two.One\n" + 
-        "\n" + 
-        "*** Two.One.One\n" + 
-        "\n" + 
-        "* Three\n" + 
-        "\n" + 
-        "Numbered lists.\n" + 
-        "# One\n" + 
-        "# Two\n" + 
-        "# Three\n" + 
-        "## Three.One\n" + 
-        "## Three.Two\n" + 
-        "## Three.Three\n" + 
-        "### Three.Three.One\n" + 
-        "# Four\n" + 
-        "\n" + 
-        "End?\n" + 
-        "\n" + 
-        "No, let's {{break}} things.\\ {{{ {{{ {{text}} }}} }}}\n" + 
-        "\n" + 
-        "More breaking.\n" + 
-        "\n" + 
-        "{{{\n" + 
-        "code.}}\n" + 
-        "----\n" + 
-        "author: [Asser], [Ebu], [JanneJalkanen], [Jarmo|mailto:jarmo@regex.com.au]\n";
-    
-
-
-
-
-
 
     public TranslatorReaderTest( String s )
     {
@@ -1287,21 +1193,105 @@ public class TranslatorReaderTest extends TestCase
                       translate(src) );
     }
 
+    /**
+     *  in 2.0.0, this one throws OutofMemoryError.
+     */
     public void testBrokenPageText()
+        throws Exception
     {
-        boolean translated = false;
-        try
-        {
-            String translation = translate( brokenPageText );
-            translated = true;
-        }
-        catch( Throwable t )
-        {
-            // This is what will happen if the thing fails.
-        }
+        String translation = translate( brokenPageText );
 
-        assertTrue( translated );
+        assertNotNull( translation );
     }
+    
+    // This is a random find: the following page text caused an eternal loop in V2.0.x.
+    private static final String brokenPageText = 
+        "Please ''check [RecentChanges].\n" + 
+        "\n" + 
+        "Testing. fewfwefe\n" + 
+        "\n" + 
+        "CHeck [testpage]\n" + 
+        "\n" + 
+        "More testing.\n" + 
+        "dsadsadsa''\n" + 
+        "Is this {{truetype}} or not?\n" + 
+        "What about {{{This}}}?\n" + 
+        "How about {{this?\n" + 
+        "\n" + 
+        "{{{\n" + 
+        "{{text}}\n" + 
+        "}}}\n" + 
+        "goo\n" + 
+        "\n" + 
+        "<B>Not bold</b>\n" + 
+        "\n" + 
+        "motto\n" + 
+        "\n" + 
+        "* This is a list which we\n" + 
+        "shall continue on a other line.\n" + 
+        "* There is a list item here.\n" + 
+        "*  Another item.\n" + 
+        "* More stuff, which continues\n" + 
+        "on a second line.  And on\n" + 
+        "a third line as well.\n" + 
+        "And a fourth line.\n" + 
+        "* Third item.\n" + 
+        "\n" + 
+        "Foobar.\n" + 
+        "\n" + 
+        "----\n" + 
+        "\n" + 
+        "!!!Really big heading\n" + 
+        "Text.\n" + 
+        "!! Just a normal heading [with a hyperlink|Main]\n" + 
+        "More text.\n" + 
+        "!Just a small heading.\n" + 
+        "\n" + 
+        "This should be __bold__ text.\n" + 
+        "\n" + 
+        "__more bold text continuing\n" + 
+        "on the next line.__\n" + 
+        "\n" + 
+        "__more bold text continuing\n" + 
+        "\n" + 
+        "on the next paragraph.__\n" + 
+        "\n" + 
+        "\n" + 
+        "This should be normal.\n" + 
+        "\n" + 
+        "Now, let's try ''italic text''.\n" + 
+        "\n" + 
+        "Bulleted lists:\n" + 
+        "* One\n" + 
+        "Or more.\n" + 
+        "* Two\n" + 
+        "\n" + 
+        "** Two.One\n" + 
+        "\n" + 
+        "*** Two.One.One\n" + 
+        "\n" + 
+        "* Three\n" + 
+        "\n" + 
+        "Numbered lists.\n" + 
+        "# One\n" + 
+        "# Two\n" + 
+        "# Three\n" + 
+        "## Three.One\n" + 
+        "## Three.Two\n" + 
+        "## Three.Three\n" + 
+        "### Three.Three.One\n" + 
+        "# Four\n" + 
+        "\n" + 
+        "End?\n" + 
+        "\n" + 
+        "No, let's {{break}} things.\\ {{{ {{{ {{text}} }}} }}}\n" + 
+        "\n" + 
+        "More breaking.\n" + 
+        "\n" + 
+        "{{{\n" + 
+        "code.}}\n" + 
+        "----\n" + 
+        "author: [Asser], [Ebu], [JanneJalkanen], [Jarmo|mailto:jarmo@regex.com.au]\n";
     
 
     public static Test suite()
