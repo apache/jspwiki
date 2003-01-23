1e5c8827a799526038986f679a1ee7e94dfc1827
Janne Jalkanen <jalkanen@apache.org>
2003-01-23 08:07:16 +0000

    Added testPlugin4-5()
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@622503 13f79535-47bb-0310-9956-ffa450edef68

--- tests/com/ecyrd/jspwiki/TranslatorReaderTest.java
+++ tests/com/ecyrd/jspwiki/TranslatorReaderTest.java
@@ -1051,6 +1051,22 @@ public class TranslatorReaderTest extends TestCase
         assertEquals( "Test  (a[]+b+)", translate(src) );
     }
 
+    public void testPluginEnd4()
+        throws Exception
+    {
+        String src="Test [{INSERT SamplePlugin text='}'}]";
+
+        assertEquals( "Test }", translate(src) );
+    }
+
+    public void testPluginEnd5()
+        throws Exception
+    {
+        String src="Test [{INSERT SamplePlugin\n\na[]+b{}\nGlob.\n}]";
+
+        assertEquals( "Test  (a[]+b{}+Glob.+)", translate(src) );
+    }
+
     public void testVariableInsert()
         throws Exception
     {
