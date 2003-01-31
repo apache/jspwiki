3032e9ec5b99818a6f92f6a03b0be7d86b27be03
Janne Jalkanen <jalkanen@apache.org>
2003-01-31 08:13:20 +0000

    Now in correct package.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@622504 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/auth/WikiSecurityException.java
+++ src/com/ecyrd/jspwiki/auth/WikiSecurityException.java
@@ -17,7 +17,9 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-package com.ecyrd.jspwiki;
+package com.ecyrd.jspwiki.auth;
+
+import com.ecyrd.jspwiki.WikiException;
 
 /**
  *  WikiSecurityException is used to provide authentication and authorization
