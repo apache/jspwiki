7f61198bcf8d8c16679576909d179e3a7adcae7f
Janne Jalkanen <jalkanen@apache.org>
2003-05-31 10:53:01 +0000

    Added comments.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@622842 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/providers/ProviderException.java
+++ src/com/ecyrd/jspwiki/providers/ProviderException.java
@@ -1,7 +1,32 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2001 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package com.ecyrd.jspwiki.providers;
 
 import com.ecyrd.jspwiki.WikiException;
 
+/**
+ *  This exception represents the superclass of all exceptions that providers
+ *  may throw.  It is okay to throw it in case you cannot use any of
+ *  the specific subclasses, in which case the page loading is
+ *  considered to be broken, and the user is notified.
+ */
 public class ProviderException
     extends WikiException
 {
