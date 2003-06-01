46763dfdd539348b0435afb800ee0907f243b47d
Janne Jalkanen <jalkanen@apache.org>
2003-06-01 15:48:31 +0000

    Completely rewritten. Now uses "status" instead of "exists".
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@622897 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/tags/UserCheckTag.java
+++ src/com/ecyrd/jspwiki/tags/UserCheckTag.java
@@ -25,47 +25,86 @@ import com.ecyrd.jspwiki.WikiEngine;
 import com.ecyrd.jspwiki.auth.UserProfile;
 
 /**
- *  Includes the content if an user check validates.
+ *  Includes the content if an user check validates.  This has
+ *  been considerably enhanced for 2.2.
+ *
+ * <ul>
+ * <li>"unknown"     - the body of the tag is included 
+ *                     if the user is completely unknown (no cookie, no password)
+ * <li>"known"       - the body of the tag is included 
+ *                     if the user is not unknown (i.e has a cookie, 
+ *                     or has been authenticated.
+ * <li>"named"       - the body of the tag is included 
+ *                     if the user has either been named by a cookie, but
+ *                     not been authenticated.
+ * <li>"validated"   - the body of the tag is included 
+ *                     if the user is validated either through the container,
+ *                     or by our own authentication.
+ * <li>"unvalidated" - the body of the tag is included 
+ *                     if the user is not validated (i.e. he could have a cookie,
+ *                     but has not been authenticated.)
  *
  *  @author Janne Jalkanen
+ *  @author Erik Bunn
  *  @since 2.0
  */
 public class UserCheckTag
     extends WikiTagBase
 {
-    private String m_exists;
+    private String m_status;
 
-    public String getExists()
+    public String getStatus()
     {
-        return m_exists;
+        return( m_status );
     }
 
-    public void setExists( String arg )
+    public void setStatus( String arg )
     {
-        m_exists = arg;
+        m_status = arg;
     }
 
     public final int doWikiStartTag()
         throws IOException
     {
-        WikiEngine engine = m_wikiContext.getEngine();
-        UserProfile user  = m_wikiContext.getCurrentUser();
+        WikiEngine  engine = m_wikiContext.getEngine();
+        UserProfile wup    = m_wikiContext.getCurrentUser();
 
-        if( "true".equals(getExists()) && user != null && 
-            user.getLoginStatus() != UserProfile.NONE )
+        if( m_status != null )
         {
-            return EVAL_BODY_INCLUDE;
-        }
-        else if( "false".equals(getExists()) && user != null &&
-                 user.getLoginStatus() == UserProfile.NONE )
-        {
-            return EVAL_BODY_INCLUDE;
-        }
-        else if( "false".equals(getExists()) && user == null )
-        {
-            return EVAL_BODY_INCLUDE;
+            if( wup == null )
+            {
+                // This may happen when strict login policy is used.
+                return( SKIP_BODY );
+            }
+
+            if( "unknown".equals( m_status ) && 
+                wup.getLoginStatus() == UserProfile.NONE )
+            {
+                return EVAL_BODY_INCLUDE;
+            }
+            else if( "known".equals( m_status ) && 
+                     wup.getLoginStatus() > UserProfile.NONE )
+            {
+                return EVAL_BODY_INCLUDE;
+            }
+            else if( "named".equals( m_status ) && 
+                     wup.getLoginStatus() == UserProfile.COOKIE )
+            {
+                return EVAL_BODY_INCLUDE;
+            }
+            else if( "validated".equals( m_status ) && 
+                     wup.getLoginStatus() > UserProfile.CONTAINER )
+            {
+                return EVAL_BODY_INCLUDE;
+            }
+            else if( "unvalidated".equals( m_status ) && 
+                     wup.getLoginStatus() < UserProfile.CONTAINER )
+            {
+                return EVAL_BODY_INCLUDE;
+            }
         }
 
         return SKIP_BODY;
     }
+
 }
