75ba0add8653ce8cc23eeda44e055c9790bc3595
Janne Jalkanen <jalkanen@apache.org>
2003-06-01 15:49:19 +0000

    Added javadocs.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@622898 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/util/ClassUtil.java
+++ src/com/ecyrd/jspwiki/util/ClassUtil.java
@@ -1,15 +1,50 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2001-2003 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package com.ecyrd.jspwiki.util;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ *  Contains useful utilities for class file manipulation.
+ *
+ *  @author Janne Jalkanen
+ *  @since 2.1.29.
+ */
 public class ClassUtil
 {
     /**
-     *  Attempts to find a class from a collection of packages.
+     *  Attempts to find a class from a collection of packages.  This will first
+     *  attempt to find the class based on just the className parameter, but
+     *  should that fail, will iterate through the "packages" -list, prefixes
+     *  the package name to the className, and then tries to find the class
+     *  again.
+     *
+     *  @param packages A List of Strings, containing different package names.
+     *  @param className The name of the class to find.
+     *  @return The class, if it was found.
+     *  @throws ClassNotFoundException if this particular class cannot be found
+     *          from the list.
      */
-    public static Class findClass( List m_packages, String className )
+    public static Class findClass( List packages, String className )
         throws ClassNotFoundException
     {
         ClassLoader loader = ClassUtil.class.getClassLoader();
@@ -20,7 +55,7 @@ public class ClassUtil
         }
         catch( ClassNotFoundException e )
         {
-            for( Iterator i = m_packages.iterator(); i.hasNext(); )
+            for( Iterator i = packages.iterator(); i.hasNext(); )
             {
                 String packageName = (String)i.next();
 
@@ -39,7 +74,14 @@ public class ClassUtil
     }
     
     /**
-     *  A shortcut for findClass when you only have a singular package.
+     *  A shortcut for findClass when you only have a singular package to search.
+     *  It will first attempt to instantiate the class directly from the className,
+     *  and will then try to prefix it with the packageName.
+     *
+     *  @param packageName A package name (such as "com.ecyrd.jspwiki.plugins").
+     *  @param className The class name to find.
+     *  @return The class, if it was found.
+     *  @throws ClassNotFoundException if this particular class cannot be found.
      */
 
     public static Class findClass( String packageName, String className )
--- src/com/ecyrd/jspwiki/util/PriorityList.java
+++ src/com/ecyrd/jspwiki/util/PriorityList.java
@@ -1,3 +1,22 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2001-2003 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package com.ecyrd.jspwiki.util;
 
 import java.util.AbstractList;
@@ -24,6 +43,13 @@ public class PriorityList
      */
     public static final int DEFAULT_PRIORITY = 0;
 
+    /**
+     *  Adds an object to its correct place in the list, using the
+     *  given priority.
+     *
+     *  @param o Object to add.
+     *  @param priority Priority.
+     */
     public void add( Object o, int priority )
     {
         int i = 0;
@@ -45,6 +71,12 @@ public class PriorityList
         m_elements.add( i, newItem );
     }
 
+    /**
+     *  Adds an object using the default priority to the List.
+     *
+     *  @param o Object to add.
+     *  @return true, as per the general Collections.add contract.
+     */
     public boolean add( Object o )
     {
         add( o, DEFAULT_PRIORITY );
@@ -52,16 +84,28 @@ public class PriorityList
         return true;
     }
 
+    /**
+     *  Returns the object at index "index".
+     *
+     *  @param index The index.
+     *  @return The object at the list at the position "index".
+     */
     public Object get( int index )
     {
         return ((Item)m_elements.get( index )).m_object;
     }
 
+    /**
+     *  Returns the current size of the list.
+     */
     public int size()
     {
         return m_elements.size();
     }
 
+    /**
+     *  Provides a holder for the priority-object 2-tuple.
+     */
     private class Item
     {
         public int     m_priority;
