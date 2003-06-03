65f9bd8ebaf1b44b588ee132e9ea0a28ef4a5647
Janne Jalkanen <jalkanen@apache.org>
2003-06-03 20:27:44 +0000

    ClassUtil.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@622931 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/com/ecyrd/jspwiki/util/AllTests.java
@@ -0,0 +1,21 @@
+
+package com.ecyrd.jspwiki.util;
+
+import junit.framework.*;
+
+public class AllTests extends TestCase
+{
+    public AllTests( String s )
+    {
+        super( s );
+    }
+
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite("Utility suite tests");
+
+        suite.addTest( ClassUtilTest.suite() );
+
+        return suite;
+    }
+}
