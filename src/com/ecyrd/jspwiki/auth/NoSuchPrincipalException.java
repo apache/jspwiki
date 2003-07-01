42bf0af3c5cad33b210e51fcb4820248e87512e4
Janne Jalkanen <jalkanen@apache.org>
2003-07-01 22:00:54 +0000

    Initial version.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@622993 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/ecyrd/jspwiki/auth/NoSuchPrincipalException.java
@@ -0,0 +1,34 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2001-2003 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.ecyrd.jspwiki.auth;
+
+import com.ecyrd.jspwiki.WikiException;
+
+/**
+ *  Thrown in some error situations where a WikiPrincipal object does not exist.
+ */
+public class NoSuchPrincipalException
+    extends WikiSecurityException
+{
+    public NoSuchPrincipalException( String msg )
+    {
+        super(msg);
+    }
+}
