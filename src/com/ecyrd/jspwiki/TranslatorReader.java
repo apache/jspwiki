88a134f84997df41027ee1df0607899c38519995
Janne Jalkanen <jalkanen@apache.org>
2003-07-01 22:03:36 +0000

    Added memberlist parsing.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@622996 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/TranslatorReader.java
+++ src/com/ecyrd/jspwiki/TranslatorReader.java
@@ -461,6 +461,10 @@ public class TranslatorReader extends Reader
         return link.startsWith("{ALLOW") || link.startsWith("{DENY");
     }
 
+    private static boolean isMemberList( String link )
+    {
+        return link.startsWith("{MEMBERS");
+    }
 
     /**
      *  Matches the given link to the list of image name patterns
@@ -655,6 +659,35 @@ public class TranslatorReader extends Reader
         return "";
     }
 
+    private String handleMemberList( String memberLine )
+    {
+        if( !m_parseAccessRules ) return "";
+
+        log.debug("Parsing member list: "+memberLine);
+
+        UserManager       mgr  = m_context.getEngine().getUserManager();
+        WikiPage          page = m_context.getPage();
+
+        StringTokenizer tok = new StringTokenizer( memberLine, ", {}" );
+
+        tok.nextToken(); // skip "MEMBERS"
+
+        ArrayList members = new ArrayList();
+
+        while( tok.hasMoreTokens() )
+        {
+            String uid = tok.nextToken();
+
+            log.debug("  Adding member: "+uid);
+
+            members.add( uid );
+        }
+
+        page.setAttribute( "_members", members );
+
+        return ""; // FIXME: Should return a list of user names.
+    }
+
     private String handleAccessRule( String ruleLine )
     {
         if( !m_parseAccessRules ) return "";
@@ -754,7 +787,7 @@ public class TranslatorReader extends Reader
         }
         catch( Exception e )
         {
-            return "<span class=\"error\" Invalid SET found: "+link+"<span>";
+            m_renderer.makeError(" Invalid SET found: "+link);
         }
 
         return "";
@@ -768,6 +801,11 @@ public class TranslatorReader extends Reader
         StringBuffer sb        = new StringBuffer();
         String       reallink;
         int          cutpoint;
+
+        if( isMemberList( link ) )
+        {
+            return handleMemberList( link );
+        }
 
         if( isAccessRule( link ) )
         {
