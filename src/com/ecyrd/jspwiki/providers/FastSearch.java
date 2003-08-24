850fe8c4552ddf8987efc7b0496e2a8d9e3a5394
Janne Jalkanen <jalkanen@apache.org>
2003-08-24 21:09:37 +0000

    Added
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@623106 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/ecyrd/jspwiki/providers/FastSearch.java
@@ -0,0 +1,33 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2001-2003 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package com.ecyrd.jspwiki.providers;
+
+/**
+ *  If a provider implements this interface, then CachingProvider
+ *  will never attempt to search on its own; it will always pass any searches
+ *  through to the actual provider.
+ *
+ *  @author Janne Jalkanen
+ *  @since  2.1.57
+ */
+public interface FastSearch
+{
+}
