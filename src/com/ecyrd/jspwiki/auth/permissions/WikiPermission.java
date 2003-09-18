6f42231d2f641970c4cf3adfcccf7e0dc7f0dd19
Janne Jalkanen <jalkanen@apache.org>
2003-09-18 19:40:14 +0000

    Added upload.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@623201 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/auth/permissions/WikiPermission.java
+++ src/com/ecyrd/jspwiki/auth/permissions/WikiPermission.java
@@ -14,7 +14,8 @@ public abstract class WikiPermission
     private static WikiPermission c_editPermission = new EditPermission();
     private static WikiPermission c_commentPermission = new CommentPermission();
     private static WikiPermission c_deletePermission = new DeletePermission();
-    
+    private static WikiPermission c_uploadPermission = new UploadPermission();
+
     /**
      *  This method should return true, if the this permission implies
      *  also the given permission.  For example "Edit" should always imply "Comment" 
@@ -34,6 +35,8 @@ public abstract class WikiPermission
             return c_commentPermission;
         else if( representation.equalsIgnoreCase( "delete" ) )
             return c_deletePermission;
+        else if( representation.equalsIgnoreCase( "upload") )
+            return c_uploadPermission;
         
         throw new IllegalArgumentException("No such permission: "+representation);
     }
