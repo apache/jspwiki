0d5430d870da7231f9010b0aa7e0e02c7d4418e8
Janne Jalkanen <jalkanen@apache.org>
2003-10-19 17:17:38 +0000

    Slight bug fix: forgot to take this month into account =).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@623307 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/plugin/WeblogArchivePlugin.java
+++ src/com/ecyrd/jspwiki/plugin/WeblogArchivePlugin.java
@@ -133,10 +133,6 @@ public class WeblogArchivePlugin implements WikiPlugin
 
             Calendar cal = new ArchiveCalendar( urCalendar );
             cal.setTime( d );
-            cal.set( Calendar.DATE, 0 );
-            cal.set( Calendar.MINUTE, 0 );
-            cal.set( Calendar.SECOND, 0 );
-            cal.set( Calendar.MILLISECOND, 0 );
 
             res.add( cal );
         }
@@ -194,7 +190,7 @@ public class WeblogArchivePlugin implements WikiPlugin
 
                 if( equals( c ) ) return 0;
 
-                return c.getTime().after( getTime() ) ? 1 : -1;
+                return c.getTime().before( getTime() ) ? 1 : -1;
             }
 
             return 0;
