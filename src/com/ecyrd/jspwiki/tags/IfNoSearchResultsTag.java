0dda2423d359b1e0b5291cbde67758367bdcd9ea
Janne Jalkanen <jalkanen@apache.org>
2004-06-27 20:40:23 +0000

    Added.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@623439 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/ecyrd/jspwiki/tags/IfNoSearchResultsTag.java
@@ -0,0 +1,52 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2001-2004 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.ecyrd.jspwiki.tags;
+
+import java.io.IOException;
+import java.util.Collection;
+import javax.servlet.jsp.PageContext;
+
+import com.ecyrd.jspwiki.WikiEngine;
+import com.ecyrd.jspwiki.WikiPage;
+import com.ecyrd.jspwiki.TranslatorReader;
+
+/**
+ *  If there have been no search results, then outputs the body text.
+ *
+ *  @author Janne Jalkanen
+ *  @since 2.0
+ */
+public class IfNoSearchResultsTag
+    extends WikiTagBase
+{
+    public final int doWikiStartTag()
+        throws IOException
+    {
+        Collection list = (Collection)pageContext.getAttribute( "searchresults",
+                                                                PageContext.REQUEST_SCOPE );
+        if( list == null || list.size() == 0 )
+        {   
+            log.info("No list: "+list+" sz="+(list!=null?""+list.size():""));
+            return EVAL_BODY_INCLUDE;
+        }
+
+        return SKIP_BODY;
+    }
+}
