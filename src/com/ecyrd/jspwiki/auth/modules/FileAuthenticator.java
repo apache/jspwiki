27f023086f7f8cc8f8e2d8bb82fa796d69a30588
Janne Jalkanen <jalkanen@apache.org>
2004-06-27 10:05:58 +0000

    Ebu's npe patch. Added missing copyright text.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@623438 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/auth/modules/FileAuthenticator.java
+++ src/com/ecyrd/jspwiki/auth/modules/FileAuthenticator.java
@@ -1,3 +1,22 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2001-2004 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package com.ecyrd.jspwiki.auth.modules;
 
 import java.util.Properties;
@@ -8,7 +27,7 @@ import java.io.File;
 import com.ecyrd.jspwiki.WikiEngine;
 import com.ecyrd.jspwiki.NoRequiredPropertyException;
 import com.ecyrd.jspwiki.auth.*;
-import org.apache.log4j.Category;
+import org.apache.log4j.Logger;
 
 /**
  *  Provides a simple file-based authenticator.  This is really simple,
@@ -24,7 +43,7 @@ public class FileAuthenticator
 
     private String m_fileName;
 
-    static Category log = Category.getInstance( FileAuthenticator.class );
+    static Logger log = Logger.getLogger( FileAuthenticator.class );
 
     public void initialize( Properties props )
         throws NoRequiredPropertyException
@@ -61,6 +80,9 @@ public class FileAuthenticator
 
     public boolean authenticate( UserProfile wup )
     {
+        if( wup == null || wup.getName() == null )
+            return( false );
+
         try
         {
             Properties props = readPasswords( m_fileName );
