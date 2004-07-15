ff2e96701ed5c9841b9c079c1cb75820a96f2869
Janne Jalkanen <jalkanen@apache.org>
2004-07-15 20:42:39 +0000

    Just added a bunch of comments.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@623450 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/auth/WikiPrincipal.java
+++ src/com/ecyrd/jspwiki/auth/WikiPrincipal.java
@@ -1,9 +1,31 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2001-2004 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package com.ecyrd.jspwiki.auth;
 
 import java.security.Principal;
 
 /**
  *  This is the master class for all wiki UserProfiles and WikiGroups.
+ *
+ *  @author Janne Jalkanen
+ *  @since  2.2
  */
 public abstract class WikiPrincipal
     implements Principal
@@ -19,11 +41,17 @@ public abstract class WikiPrincipal
         m_name = name;
     }
 
+    /**
+     *  Returns the WikiName of the Principal.
+     */
     public String getName()
     {
         return m_name;
     }
 
+    /**
+     *  Is used to set the WikiName of the principal.
+     */
     public void setName( String arg )
     {
         m_name = arg;
