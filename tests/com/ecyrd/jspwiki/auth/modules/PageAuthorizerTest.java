dfe6b23c37a70d893ee53f403a42c8866e18e023
Janne Jalkanen <jalkanen@apache.org>
2004-07-15 22:22:30 +0000

    Created.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@623458 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/com/ecyrd/jspwiki/auth/modules/PageAuthorizerTest.java
@@ -0,0 +1,67 @@
+package com.ecyrd.jspwiki.auth.modules;
+
+import junit.framework.*;
+import java.util.*;
+import com.ecyrd.jspwiki.*;
+import com.ecyrd.jspwiki.auth.*;
+import com.ecyrd.jspwiki.auth.permissions.*;
+
+public class PageAuthorizerTest
+    extends TestCase
+{
+    TestEngine m_engine;
+
+    public PageAuthorizerTest( String s )
+    {
+        super( s );
+    }
+
+    public void setUp()
+        throws Exception
+    {
+        Properties props = new Properties();
+        props.load( TestEngine.findTestProperties() );
+        
+        m_engine = new TestEngine(props);
+
+        String text1 = "Foobar.\n\n[{SET defaultpermissions='ALLOW EDIT Charlie;DENY VIEW Bob'}]\n\nBlood.";
+        String text2 = "Foo";
+
+        m_engine.saveText( "DefaultPermissions", text1 );
+        m_engine.saveText( "TestPage", text2 );
+    }
+
+    public void tearDown()
+    {
+        m_engine.deletePage( "DefaultPermissions" );
+        m_engine.deletePage( "TestPage" );
+    }
+
+    public void testDefaultPermissions()
+    {
+        AuthorizationManager mgr = m_engine.getAuthorizationManager();
+
+        UserProfile wup = new UserProfile();
+        wup.setName( "Charlie" );
+        wup.setLoginStatus( UserProfile.PASSWORD );
+
+        assertTrue( "Charlie", mgr.checkPermission( m_engine.getPage( "TestPage" ),
+                                                    wup,
+                                                    WikiPermission.newInstance( "edit" ) ) );
+
+        wup.setName( "Bob" );
+        assertTrue( "Bob", mgr.checkPermission( m_engine.getPage( "TestPage" ),
+                                                wup,
+                                                WikiPermission.newInstance( "view" ) ) );
+
+
+                                                    
+    }
+
+
+    public static Test suite()
+    {
+        return new TestSuite( PageAuthorizerTest.class );
+    }
+
+}
