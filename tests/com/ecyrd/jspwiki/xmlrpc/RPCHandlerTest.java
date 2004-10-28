a3b65b87f26ab7ac81786f9fb5d32f2650a36633
Janne Jalkanen <jalkanen@apache.org>
2004-10-28 20:01:52 +0000

    Oops, saveText() throws an exception. Added testPermissions()
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@623532 13f79535-47bb-0310-9956-ffa450edef68

--- tests/com/ecyrd/jspwiki/xmlrpc/RPCHandlerTest.java
+++ tests/com/ecyrd/jspwiki/xmlrpc/RPCHandlerTest.java
@@ -197,6 +197,28 @@ public class RPCHandlerTest extends TestCase
         assertEquals( "href", "attach?page="+NAME1+"%2FTestAtt.txt", linkinfo.get("href") );
     }
 
+    public void testPermissions()
+        throws Exception
+    {
+        String text ="Blaa. [{DENY view Guest}] [{ALLOW view NamedGuest}]";
+
+        m_engine.saveText( NAME1, text );
+
+        try
+        {
+            Vector links = m_handler.listLinks( NAME1 );
+            fail("Didn't get an exception in listLinks()");
+        }
+        catch( XmlRpcException e ) {}
+
+        try
+        {
+            Hashtable ht = m_handler.getPageInfo( NAME1 );
+            fail("Didn't get an exception in getPageInfo()");
+        }
+        catch( XmlRpcException e ) {}
+    }
+
     public static Test suite()
     {
         return new TestSuite( RPCHandlerTest.class );
