6ccd71fc2f6361026a98b0cff06c8744065e4492
Janne Jalkanen <jalkanen@apache.org>
2004-12-01 21:22:57 +0000

    Removed some methods to SettablePassword.  Authenticate() now throws a SecurityException
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@623573 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/auth/WikiAuthenticator.java
+++ src/com/ecyrd/jspwiki/auth/WikiAuthenticator.java
@@ -1,3 +1,22 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2001-2004 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package com.ecyrd.jspwiki.auth;
 
 import java.util.Properties;
@@ -25,21 +44,9 @@ public interface WikiAuthenticator
      * parameter.
      *
      * @return true, if this is a valid UserProfile, false otherwise.
+     * @throws PasswordExpiredException If the password has expired, but is
+     *                                  valid otherwise.
      */
-    public boolean authenticate( UserProfile wup );
-
-    /**
-     *  Does this authenticator support changing passwords?
-     */
-
-    public boolean canChangePasswords();
-
-    /**
-     *  Sets the user password.  This is an optional operation.
-     *
-     *  @throws ProviderException If the password cannot be set.
-     */
-
-    public void setPassword( UserProfile wup, String password )
-        throws ProviderException;
+    public boolean authenticate( UserProfile wup )
+        throws WikiSecurityException;
 }
