47858b7756bfa14614c9da5b0b53aa3f96dafcb4
Janne Jalkanen <jalkanen@apache.org>
2005-01-09 23:19:53 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@623674 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/ecyrd/jspwiki/tags/PreviousVersionTag.java
@@ -0,0 +1,51 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2001-2005 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.ecyrd.jspwiki.tags;
+
+import java.io.IOException;
+
+import com.ecyrd.jspwiki.WikiEngine;
+import com.ecyrd.jspwiki.WikiPage;
+
+/**
+ *  Outputs the version number of the previous version of this page.
+ *
+ *  @author Janne Jalkanen
+ *  @since 2.2
+ */
+public class PreviousVersionTag
+    extends WikiTagBase
+{
+    public final int doWikiStartTag()
+        throws IOException
+    {
+        WikiEngine engine = m_wikiContext.getEngine();
+        WikiPage   page   = m_wikiContext.getPage();
+
+        int version = page.getVersion();
+
+        version--;
+
+        if( version > 0 )
+            pageContext.getOut().print( version );
+
+        return SKIP_BODY;
+    }
+}
