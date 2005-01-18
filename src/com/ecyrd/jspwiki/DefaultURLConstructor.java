cd52a6d84dac05e3665d0fb25fab656d8d29ad13
Janne Jalkanen <jalkanen@apache.org>
2005-01-18 00:18:57 +0000

    Added.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@623700 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/ecyrd/jspwiki/DefaultURLConstructor.java
@@ -0,0 +1,119 @@
+package com.ecyrd.jspwiki;
+
+import java.util.Properties;
+import java.io.UnsupportedEncodingException;
+import javax.servlet.http.HttpServletRequest;
+
+public class DefaultURLConstructor
+    implements URLConstructor
+{
+    private WikiEngine m_engine;
+    private String m_viewURLPattern = "%uWiki.jsp?page=%n";
+
+    /** Are URL styles relative or absolute? */
+    private boolean          m_useRelativeURLStyle = true;
+
+    public void initialize( WikiEngine engine, 
+                            Properties properties )
+    {
+        m_engine = engine;
+
+        m_useRelativeURLStyle = "relative".equals( properties.getProperty( WikiEngine.PROP_REFSTYLE,
+                                                                           "relative" ) );
+    }
+
+    private final String doReplacement( String baseptrn, String name )
+    {
+        String baseurl = (m_useRelativeURLStyle ? "" : m_engine.getBaseURL() );
+
+        baseptrn = TextUtil.replaceString( baseptrn, "%u", baseurl );
+        baseptrn = TextUtil.replaceString( baseptrn, "%U", m_engine.getBaseURL() );
+        baseptrn = TextUtil.replaceString( baseptrn, "%n", m_engine.encodeName(name) );
+
+        return baseptrn;
+    }
+
+    /**
+     *  Constructs the URL.
+     */
+    public String makeURL( String context,
+                           String name )
+    {
+        if( context.equals(WikiContext.VIEW) )
+        {
+            if( name == null ) return makeURL("%uWiki.jsp",""); // FIXME
+            return doReplacement( m_viewURLPattern, name );
+        }
+        else if( context.equals(WikiContext.EDIT) )
+        {
+            return doReplacement( "%uEdit.jsp?page=%n", name );
+        }
+        else if( context.equals(WikiContext.ATTACH) )
+        {
+            return doReplacement( "%uattach/%n", name );
+        }
+        throw new InternalWikiException("Requested unsupported context "+context);
+    }
+
+    /**
+     *  Constructs the URL with a bunch of parameters.
+     */
+    public String makeURL( String context,
+                           String name,
+                           String parameters )
+    {
+        if( context.equals(WikiContext.ATTACH) )
+        {
+            return makeURL( context, name )+"?"+parameters;
+        }
+        return makeURL( context, name )+"&amp;"+parameters;
+    }
+
+    /**
+     *  Should parse the "page" parameter from the actual
+     *  request.
+     */
+    public String parsePage( String context,
+                             HttpServletRequest request,
+                             String encoding )
+        throws UnsupportedEncodingException
+    {
+        String pagereq = m_engine.safeGetParameter( request, "page" );
+
+        if( context.equals(WikiContext.ATTACH) )
+        {
+            pagereq = parsePageFromURL( request, encoding );
+
+            if( pagereq != null ) pagereq = TextUtil.urlDecodeUTF8(pagereq);
+        }
+
+        return pagereq;
+    }
+
+    /**
+     *  Takes the name of the page from the request URI.
+     *  The initial slash is also removed.  If there is no page,
+     *  returns null.
+     */
+    public static String parsePageFromURL( HttpServletRequest request,
+                                           String encoding )
+        throws UnsupportedEncodingException
+    {
+        String name = request.getPathInfo();
+
+        if( name == null || name.length() <= 1 )
+        {
+            return null;
+        }
+        else if( name.charAt(0) == '/' )
+        {
+            name = name.substring(1);
+        }
+       
+        name = new String(name.getBytes("ISO-8859-1"),
+                          encoding );
+
+        return name;
+    }
+
+}
