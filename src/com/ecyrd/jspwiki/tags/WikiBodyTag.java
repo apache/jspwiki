d6b8a0c18c34c8c21de4de2d6aee1b2fa3fc3ea4
Janne Jalkanen <jalkanen@apache.org>
2005-02-18 23:31:21 +0000

    Created.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@623818 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/ecyrd/jspwiki/tags/WikiBodyTag.java
@@ -0,0 +1,69 @@
+/* 
+     JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2001-2005 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+package com.ecyrd.jspwiki.tags;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+
+import org.apache.log4j.Logger;
+
+import com.ecyrd.jspwiki.WikiContext;
+
+/**
+ *  This is a class that provides the same services as the WikiTagBase, but this time it
+ *   works for the BodyTagSupport base class.
+ * 
+ *  @author jalkanen
+ *
+ */
+public abstract class WikiBodyTag extends BodyTagSupport
+{
+    protected WikiContext m_wikiContext;
+    static    Logger    log = Logger.getLogger( WikiBodyTag.class );
+
+    /* (non-Javadoc)
+     * @see javax.servlet.jsp.tagext.Tag#doStartTag()
+     */
+    public int doStartTag() throws JspException
+    {
+        try
+        {
+            m_wikiContext = (WikiContext) pageContext.getAttribute( WikiTagBase.ATTR_CONTEXT,
+                                                                                                                      PageContext.REQUEST_SCOPE );
+
+            if( m_wikiContext == null )
+            {
+                throw new JspException("WikiContext may not be NULL - serious internal problem!");
+            }
+
+            return doWikiStartTag();
+        }
+        catch( Exception e )
+        {
+            log.error( "Tag failed", e );
+            throw new JspException( "Tag failed, check logs: "+e.getMessage() );
+        }
+    }
+
+    public abstract int doWikiStartTag() throws JspException, IOException;   
+}
