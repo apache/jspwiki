e433c643aed85b44f740357628fd60c9196230df
Janne Jalkanen <jalkanen@apache.org>
2005-02-22 20:26:42 +0000

    Reworked to figure out the wiki context to support things like images, etc. Added support for PROP_PREFIX.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@623832 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/ShortURLConstructor.java
+++ src/com/ecyrd/jspwiki/ShortURLConstructor.java
@@ -2,15 +2,54 @@ package com.ecyrd.jspwiki;
 
 import java.util.Properties;
 import java.io.UnsupportedEncodingException;
-import javax.servlet.http.HttpServletRequest;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.servlet.http.HttpServletRequest;
+import org.apache.log4j.Logger;
+
+
+/**
+ *  Provides a way to do short URLs of the form /wiki/PageName.
+ *
+ *  @author jalkanen
+ *
+ *  @since 2.2
+ */
 public class ShortURLConstructor
     extends DefaultURLConstructor
 {
+    static Logger log = Logger.getLogger( ShortURLConstructor.class );
+    
+    private String m_urlPrefix = "";
+    
+    public static final String PROP_PREFIX = "jspwiki.shortURLConstructor.prefix";
+    
     public void initialize( WikiEngine engine, 
                             Properties properties )
     {
         super.initialize( engine, properties );
+        
+        m_urlPrefix = TextUtil.getStringProperty( properties, PROP_PREFIX, null );
+        
+        if( m_urlPrefix == null )
+        {
+            String baseurl = engine.getBaseURL();
+            try
+            {
+                URL url = new URL( baseurl );
+            
+                String path = url.getPath();
+            
+                m_urlPrefix = path+"wiki/";
+            
+                log.info("Short URL prefix path="+m_urlPrefix+" (You can use "+PROP_PREFIX+" to override this)");
+            }
+            catch( MalformedURLException e )
+            {
+                log.error( "Malformed base URL!" );
+            }
+        }
     }
 
     /**
@@ -20,7 +59,7 @@ public class ShortURLConstructor
                             String name,
                             boolean absolute )
     {
-        String viewurl = "/wiki/%n";
+        String viewurl = m_urlPrefix+"%n";
 
         if( absolute ) 
             viewurl = "%uwiki/%n";
@@ -80,7 +119,10 @@ public class ShortURLConstructor
             {
                 parameters = "?"+parameters;
             }
-            parameters = "&amp;"+parameters;
+            else
+            {
+                parameters = "&amp;"+parameters;
+            }
         }
         else
         {
