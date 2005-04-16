7df440ce8eaafc991610307e18e3128d3557a1e4
Janne Jalkanen <jalkanen@apache.org>
2005-04-16 13:55:22 +0000

    Added modification choking
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@624040 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/filters/SpamFilter.java
+++ src/com/ecyrd/jspwiki/filters/SpamFilter.java
@@ -1,7 +1,7 @@
 /* 
     JSPWiki - a JSP-based WikiWiki clone.
 
-    Copyright (C) 2001-2004 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+    Copyright (C) 2001-2005 Janne Jalkanen (Janne.Jalkanen@iki.fi)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
@@ -21,6 +21,8 @@ package com.ecyrd.jspwiki.filters;
 
 import java.util.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 import com.ecyrd.jspwiki.*;
 
 import org.apache.oro.text.regex.*;
@@ -28,8 +30,16 @@ import org.apache.oro.text.regex.*;
 import org.apache.log4j.Logger;
 
 /**
- *  A regular expression-based spamfilter.
+ *  A regular expression-based spamfilter that can also do choke modifications.
  *
+ *  Parameters:
+ *  <ul>
+ *    <li>wordlist - Page name where the regexps are found.  Use [{SET spamwords='regexp list separated with spaces'}] on
+ *     that page.  Default is "SpamFilterWordList".
+ *    <li>errorpage - The page to which the user is redirected.  Has a special variable $msg which states the reason. Default is "RejectedMessage".
+ *    <li>pagechangesinminute - How many page changes are allowed/minute.  Default is 3.
+ *    <li>bantime - How long an IP address stays on the temporary ban list (default is 60 for 60 minutes).
+ *  </ul>
  *  @since 2.1.112
  *  @author Janne Jalkanen
  */
@@ -51,13 +61,37 @@ public class SpamFilter
 
     public static final String PROP_WORDLIST  = "wordlist";
     public static final String PROP_ERRORPAGE = "errorpage";
-
+    public static final String PROP_PAGECHANGES = "pagechangesinminute";
+    public static final String PROP_BANTIME   = "bantime";
+    
+    private Vector m_temporaryBanList = new Vector();
+    
+    private int m_banTime = 60; // minutes
+    
+    private Vector m_lastModifications = new Vector();
+    
+    /**
+     *  How many times a single IP address can change a page per minute?
+     */
+    private int m_limitSinglePageChanges = 5;
+    
     public void initialize( Properties properties )
     {
         m_forbiddenWordsPage = properties.getProperty( PROP_WORDLIST, 
                                                        m_forbiddenWordsPage );
         m_errorPage = properties.getProperty( PROP_ERRORPAGE, 
                                               m_errorPage );
+
+        m_limitSinglePageChanges = TextUtil.getIntegerProperty( properties,
+                                                                PROP_PAGECHANGES,
+                                                                m_limitSinglePageChanges );
+        
+        m_banTime = TextUtil.getIntegerProperty( properties,
+                                                 PROP_BANTIME,
+                                                 m_banTime );
+        
+        log.info("Spam filter initialized.  Temporary ban time "+m_banTime+
+                 " mins, max page changes/minute: "+m_limitSinglePageChanges );
     }
 
     private Collection parseWordList( WikiPage source, String list )
@@ -88,10 +122,102 @@ public class SpamFilter
         return compiledpatterns;
     }
 
+    private synchronized void checkSinglePageChange( WikiContext context )
+        throws RedirectException
+    {
+        HttpServletRequest req = context.getHttpRequest();
+        
+        if( req != null )
+        {
+            String addr = req.getRemoteAddr();
+            int counter = 0;
+                
+            long time = System.currentTimeMillis()-60*1000L; // 1 minute
+            
+            for( Iterator i = m_lastModifications.iterator(); i.hasNext(); )
+            {
+                Host host = (Host)i.next();
+                
+                //
+                //  Check if this item is invalid
+                //
+                if( host.getAddedTime() < time )
+                {
+                    log.debug("Removed host "+host.getAddress()+" from modification queue (expired)");
+                    i.remove();
+                    continue;
+                }
+                
+                if( host.getAddress().equals(addr) )
+                {
+                    counter++;
+                }
+            }
+            
+            if( counter > m_limitSinglePageChanges )
+            {
+                Host host = new Host( addr );
+                
+                m_temporaryBanList.add( host );
+                
+                log.info("Added host "+addr+" to temporary ban list for doing too many modifications/minute" );
+                throw new RedirectException( "Too many modifications/minute",
+                                             context.getViewURL( m_errorPage ) );
+            }
+            
+            m_lastModifications.add( new Host( addr ) );
+        }
+    }
 
+    private synchronized void cleanBanList()
+    {
+        long now = System.currentTimeMillis();
+        
+        for( Iterator i = m_temporaryBanList.iterator(); i.hasNext(); )
+        {
+            Host host = (Host)i.next();
+            
+            if( host.getReleaseTime() < now )
+            {
+                log.debug("Removed host "+host.getAddress()+" from temporary ban list (expired)");
+                i.remove();
+            }
+        }
+    }
+    
+    
+    private void checkBanList( WikiContext context )
+        throws RedirectException
+    {
+        HttpServletRequest req = context.getHttpRequest();
+        
+        if( req != null )
+        {
+            String remote = req.getRemoteAddr();
+            
+            long now = System.currentTimeMillis();
+            
+            for( Iterator i = m_temporaryBanList.iterator(); i.hasNext(); )
+            {
+                Host host = (Host)i.next();
+                
+                if( host.getAddress().equals(remote) )
+                {
+                    long timeleft = (host.getReleaseTime() - now) / 1000L;
+                    throw new RedirectException( "You have been temporarily banned from modifying this wiki. ("+timeleft+" seconds of ban left)",
+                                                 context.getViewURL( m_errorPage ) );
+                }
+            }
+        }
+        
+    }
     public String preSave( WikiContext context, String content )
         throws RedirectException
     {
+        cleanBanList();
+        checkBanList( context );
+        checkSinglePageChange( context );
+        
         WikiPage source = context.getEngine().getPage( m_forbiddenWordsPage );
 
         if( source != null )
@@ -134,5 +260,41 @@ public class SpamFilter
         }
 
         return content;
+    }
+    
+    /**
+     *  A local class for storing host information.
+     * 
+     *  @author jalkanen
+     *
+     *  @since
+     */
+    public class Host
+    {
+        private  long m_addedTime = System.currentTimeMillis();
+        private  long m_releaseTime;
+        private  String m_address;
+        
+        public String getAddress()
+        {
+            return m_address;
+        }
+        
+        public long getReleaseTime()
+        {
+            return m_releaseTime;
+        }
+        
+        public long getAddedTime()
+        {
+            return m_addedTime;
+        }
+        
+        public Host( String ipaddress )
+        {
+            m_address = ipaddress;
+            
+            m_releaseTime = System.currentTimeMillis() + m_banTime * 60 * 1000L;
+        }
     }
 }
