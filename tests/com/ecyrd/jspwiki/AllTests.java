e3aef774d4bd245a3ed58718846ffc71c023fd29
Janne Jalkanen <jalkanen@apache.org>
2005-04-17 21:53:15 +0000

    rss tests
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@624057 13f79535-47bb-0310-9956-ffa450edef68

--- tests/com/ecyrd/jspwiki/AllTests.java
+++ tests/com/ecyrd/jspwiki/AllTests.java
@@ -57,6 +57,7 @@ public class AllTests extends TestCase
         // suite.addTest( com.ecyrd.jspwiki.auth.AllTests.suite() );
         suite.addTest( com.ecyrd.jspwiki.util.AllTests.suite() );
         suite.addTest( com.ecyrd.jspwiki.filters.AllTests.suite() );
+        suite.addTest( com.ecyrd.jspwiki.rss.AllTests.suite() );
 
         return suite;
     }
