c6f4fcb42c36a689988f1d15c9fb24c8821b0289
Janne Jalkanen <jalkanen@apache.org>
2005-04-25 20:12:43 +0000

    Added; only has setskincookie and zebra tables stuff
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@624112 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/webdocs/scripts/jspwiki-common.js
@@ -0,0 +1,65 @@
+// 004. Zebra tables  DF / May 2004
+// %%zebra-table
+//
+var zebraCnt ;
+
+// called after loading the page
+function validateZebraTable()
+{
+  if (!document.createElement) return;
+
+  // find a <div class="zebra-table"> element
+  var divArr = document.getElementsByTagName("div");
+  if (! divArr) return; 
+
+  for (var i=0; i<divArr.length; i++) 
+  {
+    if ( divArr[i].className == "zebra-table" )
+    {
+      zebraCnt = 0;
+      validateZebraTableNode(divArr[i]);   
+    } 
+  }  
+
+}
+
+function validateZebraTableNode(node)
+{
+  if ( node.nodeName == "TR") 
+  {
+     zebraCnt++;
+     if (zebraCnt % 2 == 1) node.className = "odd";
+  }
+  
+  if (node.hasChildNodes) 
+  {
+    for (var i=0; i<node.childNodes.length; i++) 
+    { 
+      validateZebraTableNode(node.childNodes[i]);
+    }
+  }
+
+}
+
+// Select skin
+function skinSelect(skin)
+{
+  //var skin = document.forms["skinForm"].skinSelector;
+  if (! skin) return; 
+
+  for (var i=0; i<skin.length; i++)
+  {
+    if ( skin[i].selected )
+    {
+      document.cookie = "JspWikiSkin=" + skin[i].value + "#skin#" ;
+    }
+  }
+  location.reload(); /* reload page */
+}
+
+function runOnLoad()
+{ 
+  validateZebraTable();
+}
+
+window.onload = runOnLoad;
