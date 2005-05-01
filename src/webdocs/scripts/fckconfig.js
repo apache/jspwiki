f8e8a7de0328a2fc84d5f12141b77f2eb4354af7
Janne Jalkanen <jalkanen@apache.org>
2005-05-01 21:40:37 +0000

    JSPWiki default toolbar.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@624126 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/webdocs/scripts/fckconfig.js
@@ -0,0 +1,17 @@
+/* You may add anything you like to this file to configure the FCKEditor. */
+
+FCKConfig.ToolbarSets["JSPWiki"] = [
+        ['Source'],
+        ['Cut','Copy','Paste','PasteText','PasteWord'],
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        ['Bold','Italic','Underline','StrikeThrough'],
+        ['OrderedList','UnorderedList','-','Outdent','Indent'],
+        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+        ['Link','Unlink'],
+        ['Image','Table','Rule','SpecialChar','UniversalKey'],
+        ['Style'],
+        ['TextColor','BGColor'],
+        ['About']
+] ;
+
+FCKConfig.StylesXmlPath = '../../fckstyles.xml' ;
