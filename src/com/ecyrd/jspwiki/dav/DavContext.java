5324d931a82af729db3218882b606a9f0dba9c78
Janne Jalkanen <jalkanen@apache.org>
2005-05-15 10:44:57 +0000

    Added patch from someone to protect against NPEs
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@624162 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/dav/DavContext.java
+++ src/com/ecyrd/jspwiki/dav/DavContext.java
@@ -9,27 +9,33 @@ import javax.servlet.http.HttpServletRequest;
 
 public class DavContext
 {
+    public String m_davroot = "/";
     public String m_davcontext = "";
     public String m_page = null;
     public int    m_depth = -1;
     
     public DavContext( HttpServletRequest req )
     {
+        m_davroot = req.getContextPath();
+        
         String path = req.getPathInfo();
-        
-        if( path.startsWith("/") ) path = path.substring(1);
-        
-        int idx = path.indexOf('/');
-        
-        if( idx != -1 )
+
+        if (path != null)
         {
-            m_davcontext = path.substring( 0, idx );
+            if( path.startsWith("/") ) path = path.substring(1);
+        
+            int idx = path.indexOf('/');
+        
+            if( idx != -1 )
+            {
+                m_davcontext = path.substring( 0, idx );
             
-            m_page = path.substring( idx+1 );
-        }
-        else
-        {
-            m_davcontext = path;
+                m_page = path.substring( idx+1 );
+            }
+            else
+            {
+                m_davcontext = path;
+            }
         }
         
         String depth = req.getHeader("Depth");
