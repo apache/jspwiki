65ba592717b7b965d4db24eb35de05437cc0ce83
Janne Jalkanen <jalkanen@apache.org>
2005-05-17 21:25:09 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@624183 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/Release.java
+++ src/com/ecyrd/jspwiki/Release.java
@@ -39,7 +39,7 @@ public class Release
      *  values are "-alpha" and "-beta" for alpha and beta versions,
      *  respectively.
      */
-    private static final String    POSTFIX       = "-beta";
+    private static final String    POSTFIX       = "-cvs";
 
     /**
      *  This should be increased every time you do a release.
@@ -48,7 +48,7 @@ public class Release
 
     public static final int        VERSION       = 2;
     public static final int        REVISION      = 2;
-    public static final int        MINORREVISION = 14;
+    public static final int        MINORREVISION = 15;
 
     /**
      *  This is the generic version string you should use
