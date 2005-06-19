7d5b4f5d5d666ac91c2bba388af7363c8d6f989b
Janne Jalkanen <jalkanen@apache.org>
2005-06-19 11:45:44 +0000

    Added native2Ascii()
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@624248 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/TextUtil.java
+++ src/com/ecyrd/jspwiki/TextUtil.java
@@ -652,4 +652,44 @@ public class TextUtil
 
         return sb.toString();
     }
+    
+    /**
+     *  Converts a string from the Unicode representation into something that can be
+     *  embedded in a java properties file.  All references outside the ASCII range 
+     *  are replaced with \\uXXXX.
+     *  
+     *  @param s
+     *  @return 
+     */
+    public static String native2Ascii(String s)
+    {
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < s.length(); i++)
+        {
+            char aChar = s.charAt(i);
+            if ((aChar < 0x0020) || (aChar > 0x007e)) 
+            {
+                sb.append('\\');
+                sb.append('u');
+                sb.append(toHex((aChar >> 12) & 0xF));
+                sb.append(toHex((aChar >>  8) & 0xF));
+                sb.append(toHex((aChar >>  4) & 0xF));
+                sb.append(toHex( aChar        & 0xF));
+            } 
+            else 
+            {
+                sb.append(aChar);
+            }
+        }
+        return sb.toString();
+    }
+    
+    private static char toHex(int nibble) 
+    {
+        final char[] hexDigit = 
+        {
+            '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
+        };
+        return hexDigit[(nibble & 0xF)];
+    }
 }
