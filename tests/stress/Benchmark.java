1332c5b82ac70310e43d48d1dff9275619297bec
Janne Jalkanen <jalkanen@apache.org>
2005-09-10 22:00:56 +0000

    Simplified a lot by extending StopWatch
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@624541 13f79535-47bb-0310-9956-ffa450edef68

--- tests/stress/Benchmark.java
+++ tests/stress/Benchmark.java
@@ -1,46 +1,15 @@
 package stress;
 
-public class Benchmark
+import org.apache.commons.lang.time.StopWatch;
+
+public class Benchmark extends StopWatch
 {
-    long m_start;
-    long m_stop;
-
-    public Benchmark()
-    {
-    }
-
-    public final void start()
-    {
-        m_start = System.currentTimeMillis();
-    }
-
-    public final void stop()
-    {
-        m_stop = System.currentTimeMillis();
-    }
-
-    /**
-     *  Returns duration in milliseconds.
-     */
-    public long getDurationMs()
-    {
-        return m_stop-m_start;
-    }
-
-    /**
-     *  Returns seconds.
-     */
-    public String toString()
-    {
-        return Double.toString( ((double)m_stop - (double)m_start) / 1000.0 );
-    }
-    
     /**
      *  How many operations/second?
      */
     public String toString( int operations )
     {
-        double totalTime = (double)m_stop - (double)m_start;
+        double totalTime = getTime();
 
         return Double.toString( (operations/totalTime) * 1000.0 );
     }
