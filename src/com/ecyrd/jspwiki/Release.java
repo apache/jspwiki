df291eadf40d50ef77e60a89c618dd79e04de0fe
Janne Jalkanen <jalkanen@apache.org>
2005-10-09 19:52:26 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@624655 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/Release.java
+++ src/com/ecyrd/jspwiki/Release.java
@@ -49,7 +49,7 @@ public class Release
 
     public static final int        VERSION       = 2;
     public static final int        REVISION      = 3;
-    public static final int        MINORREVISION = 28;
+    public static final int        MINORREVISION = 29;
 
     /**
      *  This is the generic version string you should use
