19f19fbae128cdc02a9b8d7bc9ef20736279ba2d
Janne Jalkanen <jalkanen@apache.org>
2005-10-22 11:27:42 +0000

    Tests are now run only if jspwiki.tests.auth is true
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@624704 13f79535-47bb-0310-9956-ffa450edef68

--- tests/com/ecyrd/jspwiki/auth/AllTests.java
+++ tests/com/ecyrd/jspwiki/auth/AllTests.java
@@ -1,6 +1,8 @@
 
 package com.ecyrd.jspwiki.auth;
 
+import com.ecyrd.jspwiki.TextUtil;
+
 import junit.framework.*;
 
 public class AllTests extends TestCase
@@ -14,14 +16,19 @@ public class AllTests extends TestCase
     {
         TestSuite suite = new TestSuite("AAA package tests");
 
-        suite.addTest( AuthenticationManagerTest.suite() );
-        suite.addTest( AuthorizationManagerTest.suite() );
-        suite.addTest( com.ecyrd.jspwiki.auth.acl.AllTests.suite() );
-        suite.addTest( com.ecyrd.jspwiki.auth.authorize.AllTests.suite() );
-        suite.addTest( com.ecyrd.jspwiki.auth.login.AllTests.suite() );
-        suite.addTest( com.ecyrd.jspwiki.auth.permissions.AllTests.suite() );
-        suite.addTest( com.ecyrd.jspwiki.auth.user.AllTests.suite() );
-
+        String runTests = System.getProperty( "jspwiki.tests.auth", "false" );
+        
+        if( TextUtil.isPositive(runTests) )
+        {
+            suite.addTest( AuthenticationManagerTest.suite() );
+            suite.addTest( AuthorizationManagerTest.suite() );
+            suite.addTest( com.ecyrd.jspwiki.auth.acl.AllTests.suite() );
+            suite.addTest( com.ecyrd.jspwiki.auth.authorize.AllTests.suite() );
+            suite.addTest( com.ecyrd.jspwiki.auth.login.AllTests.suite() );
+            suite.addTest( com.ecyrd.jspwiki.auth.permissions.AllTests.suite() );
+            suite.addTest( com.ecyrd.jspwiki.auth.user.AllTests.suite() );
+        }
+        
         return suite;
     }
 }
