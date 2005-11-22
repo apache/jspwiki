4cf92cf3ed583992c95099dfac9a1578b3ef6329
Janne Jalkanen <jalkanen@apache.org>
2005-11-22 21:35:54 +0000

    Added serialVersionUID. Rewrote entirely - now includes the proper JSP page.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@624778 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/tags/EditorTag.java
+++ src/com/ecyrd/jspwiki/tags/EditorTag.java
@@ -21,18 +21,11 @@ package com.ecyrd.jspwiki.tags;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
 import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.PageContext;
-import javax.servlet.jsp.tagext.BodyContent;
 
-import com.ecyrd.jspwiki.WikiContext;
 import com.ecyrd.jspwiki.WikiEngine;
-import com.ecyrd.jspwiki.WikiPage;
-import com.ecyrd.jspwiki.auth.authorize.DefaultGroupManager;
-
-import org.apache.ecs.xhtml.*;
-import org.apache.ecs.GenericElement;
-import org.apache.ecs.ConcreteElement;
+import com.ecyrd.jspwiki.editor.EditorManager;
 
 /**
  *  Creates an editor component with all the necessary parts
@@ -47,178 +40,40 @@ import org.apache.ecs.ConcreteElement;
 public class EditorTag
     extends WikiBodyTag
 {
-    private String m_submit  = "Save";
-    private String m_preview = "Preview";
-    private String m_cancel  = "Cancel";
-    private String m_formName = "editForm";
-    private String m_action = null;
-    private boolean m_isGroupPage = false;
-    
-    public void setSubmit( String s )
-    {
-        m_submit = s;
-    }
-
-    public void setPreview( String s )
-    {
-        m_preview = s;
-    }
-
-    public void setCancel( String s )
-    {
-        m_cancel = s;
-    }
-
-    public void setName( String s )
-    {
-        m_formName = s;
-    }
-    
-    public void setAction( String s )
-    {
-        m_action = s;
-    }
-    
-    private GenericElement createInput( String name, String value )
-    {
-        input in = new input();
-        in.setType("hidden");
-        in.setName( name );
-        in.setValue( value );
-
-        return in;
-    }
-
-    private GenericElement createSubmit( String name, String value )
-    {
-        input in = new input();
-        in.setType("submit");
-        in.setName( name );
-        in.setValue( value );
-
-        return in;
-    }
-
-    private form createSimpleEditor()
-    {
-        WikiPage   page   = m_wikiContext.getPage();
-        WikiEngine engine = m_wikiContext.getEngine();
-        
-        form f = new form();
-        f.setName(m_formName);
- 
-        if( m_action != null ) 
-        {
-            f.setAction( m_action );
-        }
-        else if( m_wikiContext.getRequestContext().equals( WikiContext.COMMENT ) ||
-                 "comment".equals(m_wikiContext.getHttpParameter("action")) )
-        {
-            f.setAction( m_wikiContext.getURL( WikiContext.COMMENT, page.getName() ));
-        }
-        else
-        {
-            f.setAction( m_wikiContext.getURL( WikiContext.EDIT, page.getName() ));
-        }
-        
-        f.setMethod( "post" );
-        f.setAcceptCharset( engine.getContentEncoding() );
-
-        p para1 = new p();
-
-        // Kludge to get preview working from Comment.jsp
-        if( m_wikiContext.getHttpParameter("author") != null )
-        {
-            para1.addElement( createInput("author", m_wikiContext.getHttpParameter("author")) );
-        }
-        
-        f.addElement(para1);
-
-        para1.addElement( createInput("page", page.getName() ) );
-        para1.addElement( createInput("action", "save" ) );
-        para1.addElement( createInput("edittime",
-                                      (String)pageContext.getAttribute("lastchange",
-                                                                       PageContext.REQUEST_SCOPE ) ) );
-       
-        if( m_wikiContext.getRequestContext().equals("comment") )
-        {
-            para1.addElement( createInput("comment","true") );
-        }
-        
-        return f;
-    }
-
-    private ConcreteElement getEditorArea()
-    {
-        return EditorAreaTag.getEditorArea( m_wikiContext );
-        
-        // FIXME: ARJ branch has code here for injecting a default ACL list
-        
-        /*
-            // This is a major HACK
-            // If this is a new Group, inject a default ACL and member list
-            if ( "".equals( content ) && m_isGroupPage )
-            {
-                String user = m_wikiContext.getCurrentUser().getName();
-                String page = m_wikiContext.getPage().getName();
-                String group = page.substring(DefaultGroupManager.GROUP_PREFIX.length());
-                content = "[{SET members=" + user + "}]\n"
-                        + "[{ALLOW edit " + group + "}]\n";
-            }
-		*/
-    }
-    /**
-     *  Returns an edit button block.
-     * 
-     * @return
-     */
-    private ConcreteElement getButtons()
-    {
-        p para2 = new p();
-
-        para2.addElement( createSubmit( "ok",      m_submit ) );
-        para2.addElement( "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" );
-        para2.addElement( createSubmit( "preview", m_preview ) );
-        para2.addElement( "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" );
-        para2.addElement( createSubmit( "cancel",  m_cancel ) );
-
-        return para2;
-    }
+    private static final long serialVersionUID = 0L;
     
     public final int doWikiStartTag()
         throws IOException
     {
-    	// ARJ: this is a bit of a hack
-        String pageName = m_wikiContext.getPage().getName();
-        m_isGroupPage = pageName.startsWith( DefaultGroupManager.GROUP_PREFIX );
-    
-        return EVAL_BODY_BUFFERED;
+        return SKIP_BODY;
     }
        
     public int doEndTag() throws JspException
     {
-        BodyContent bc = getBodyContent();
+        WikiEngine engine = m_wikiContext.getEngine();
+        EditorManager mgr = engine.getEditorManager();
         
-        form editor = createSimpleEditor();
-    
-        // 
-        // If there is no body tag content, then we'll assume old
-        // behaviour and append the stuff ourselves.
-        //
-        if( bc == null || bc.getString().length() == 0 )
-        {
-            editor.addElement( getEditorArea() );
-
-            editor.addElement( getButtons() );            
-        }
-        else
-        {
-            editor.addElement( bc.getString() );
-        }
+        String editorPath = mgr.getEditorPath( m_wikiContext );
         
         try
         {
-            pageContext.getOut().print( editor.toString() );
+            String page = engine.getTemplateManager().findJSP( pageContext,
+                                                               m_wikiContext.getTemplate(),
+                                                               editorPath );
+            
+            if( page == null )
+            {
+                pageContext.getOut().println("Unable to find editor '"+editorPath+"'");
+            }
+            else
+            {
+                pageContext.include( page );
+            }
+        }
+        catch( ServletException e )
+        {
+            log.error("Failed to include editor",e);
+            throw new JspException("Failed to include editor: "+e.getMessage() );
         }
         catch( IOException e )
         {
