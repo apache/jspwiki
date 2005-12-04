96d5aa886ff4bba1943907cd6044f856b0e85a5a
Janne Jalkanen <jalkanen@apache.org>
2005-12-04 19:00:55 +0000

    Added Wiki.insertBrowserSpecificCSS()
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@624847 13f79535-47bb-0310-9956-ffa450edef68

--- src/webdocs/scripts/jspwiki-common.js
+++ src/webdocs/scripts/jspwiki-common.js
@@ -219,6 +219,43 @@ Wiki.onPageLoad = function()
   this.username = ( reUsername.test(u) ) ? RegExp.$1 : null;
 }
 
+/*
+ *  Chooses a suitable stylesheet based on browser.
+ *
+ * issue a document.write statement with the link to the browser specific stylesheet
+ * should always be execute from direct javascript during page-load
+ */
+Wiki.loadBrowserSpecificCSS  = function ( baseurl, templatePath )
+{
+    var IE4 = (document.all && !document.getElementById) ? true : false;
+    var NS4 = (document.layers) ? true : false;
+    var IE5 = (document.all && document.getElementById) ? true : false;
+    var NS6 = (document.getElementById && !document.all) ? true : false;
+    var IE  = IE4 || IE5;
+    var NS  = NS4 || NS6;
+    var Mac = (navigator.platform.indexOf("Mac") == -1) ? false : true;
+
+    var sheet = "";
+
+    if( NS4 )
+    {
+        sheet = "jspwiki_ns.css";
+    }
+    else if( Mac )
+    {
+        sheet = "jspwiki_mac.css";
+    }
+    else if( IE )
+    {
+        sheet = "jspwiki_ie.css";
+    }
+
+    if( sheet != "" )
+    {
+        sheet = baseurl+"templates/" +templatePath + "/" + sheet;
+        document.write("<link rel=\"stylesheet\" href=\""+sheet+"\" />");
+    }
+}
 
 /**
  ** 040  cookie stuff 
