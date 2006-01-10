aa4d6243c5359d0376062a53664d2ff18e1a1655
Janne Jalkanen <jalkanen@apache.org>
2006-01-10 22:25:11 +0000

    Now supports shorturlconstructors
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@624965 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/htmltowiki/XHtmlToWikiConfig.java
+++ src/com/ecyrd/jspwiki/htmltowiki/XHtmlToWikiConfig.java
@@ -1,31 +1,68 @@
 package com.ecyrd.jspwiki.htmltowiki;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.ecyrd.jspwiki.WikiContext;
 
 /**
- * Config class for XHtmlToWikiTranslator
+ *  Defines a Wiki configuration to XHtmlToWikiTranslator, including things like
+ *  URLs.
  * 
  * @author Sebastian Baltes (sbaltes@gmx.com)
  */
 public class XHtmlToWikiConfig
 {
+    private String m_outlink = "outlink";
 
-    private String outlink = "outlink";
+    private String m_pageInfoJsp = "PageInfo.jsp";
 
-    private String pageInfoJsp = "PageInfo.jsp";
+    private String m_wikiJspPage = "Wiki.jsp?page=";
 
-    private String wikiJspPage = "Wiki.jsp?page=";
+    private String m_attachPage = "attach?page=";
 
-    private String attachPage = "attach?page=";
-
-    private String pageName;
+    private String m_pageName;
 
     public XHtmlToWikiConfig()
     {}
 
+    /**
+     *  The constructor initializes the different internal fields
+     *  according to the current URLConstructor.
+     *  
+     * @param wikiContext
+     */
     public XHtmlToWikiConfig( WikiContext wikiContext )
     {
         setWikiContext( wikiContext );
+        
+        //
+        //  Figure out the actual URLs.
+        //
+        //  NB: The logic here will fail if you add something else after
+        //      the Wiki page name in VIEW or ATTACH
+        //
+        String page = wikiContext.getPage().getName();
+        
+        String href = wikiContext.getURL( WikiContext.VIEW, page );
+        m_wikiJspPage = removeLast(href,page);
+        
+        href = wikiContext.getURL( WikiContext.ATTACH, page );
+        m_attachPage = removeLast( href, page );
+        
+        href = wikiContext.getURL( WikiContext.INFO, page );
+        m_pageInfoJsp = removeLast( href, page );
+    }
+
+    private String removeLast(String str, String remove )
+    {
+        int idx = str.lastIndexOf( remove );
+        
+        if( idx != -1 )
+        {
+            str = StringUtils.left( str, idx ) + StringUtils.substring( str, idx+remove.length() );
+        }
+        
+        return str;
     }
 
     private void setWikiContext( WikiContext wikiContext )
@@ -38,51 +75,51 @@ public class XHtmlToWikiConfig
 
     public String getAttachPage()
     {
-        return attachPage;
+        return m_attachPage;
     }
 
     public void setAttachPage( String attachPage )
     {
-        this.attachPage = attachPage;
+        m_attachPage = attachPage;
     }
 
     public String getOutlink()
     {
-        return outlink;
+        return m_outlink;
     }
 
     public void setOutlink( String outlink )
     {
-        this.outlink = outlink;
+        m_outlink = outlink;
     }
 
     public String getPageInfoJsp()
     {
-        return pageInfoJsp;
+        return m_pageInfoJsp;
     }
 
     public void setPageInfoJsp( String pageInfoJsp )
     {
-        this.pageInfoJsp = pageInfoJsp;
+        m_pageInfoJsp = pageInfoJsp;
     }
 
     public String getPageName()
     {
-        return pageName;
+        return m_pageName;
     }
 
     public void setPageName( String pageName )
     {
-        this.pageName = pageName;
+        m_pageName = pageName;
     }
 
     public String getWikiJspPage()
     {
-        return wikiJspPage;
+        return m_wikiJspPage;
     }
 
     public void setWikiJspPage( String wikiJspPage )
     {
-        this.wikiJspPage = wikiJspPage;
+        m_wikiJspPage = wikiJspPage;
     }
 }
