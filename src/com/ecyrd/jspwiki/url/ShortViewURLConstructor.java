97d5551427a49a57ab9af96459825fad8e4766b6
Janne Jalkanen <jalkanen@apache.org>
2006-03-22 18:10:11 +0000

    Removed extra comment.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@625141 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/url/ShortViewURLConstructor.java
+++ src/com/ecyrd/jspwiki/url/ShortViewURLConstructor.java
@@ -54,7 +54,7 @@ public class ShortViewURLConstructor
 
         if( context.equals(WikiContext.VIEW) )
         {
-            if( name == null ) return doReplacement("%u","",absolute); // FIXME
+            if( name == null ) return doReplacement("%u","",absolute);
             return doReplacement( viewurl, name, absolute );
         }
 
