396a8670a5357edebdd8499b7bfd5525cf485ee5
Janne Jalkanen <jalkanen@apache.org>
2006-04-03 19:49:30 +0000

    Added tolerance against too long lines.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@625168 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/parser/JSPWikiMarkupParser.java
+++ src/com/ecyrd/jspwiki/parser/JSPWikiMarkupParser.java
@@ -577,8 +577,23 @@ public class JSPWikiMarkupParser
         throws IOException
     {
         String s = readUntilEOL().toString();
-        pushBack( s );
-
+        
+        if( s.length() > PUSHBACK_BUFFER_SIZE )
+        {
+            log.warn("Line is longer than maximum allowed size ("+PUSHBACK_BUFFER_SIZE+" characters.  Attempting to recover...");
+            pushBack( s.substring(0,PUSHBACK_BUFFER_SIZE-1) );
+        }
+        else
+        {
+            try
+            {
+                pushBack( s );
+            }
+            catch( IOException e )
+            {
+                log.warn("Pushback failed: the line is probably too long.  Attempting to recover.");
+            }
+        }
         return s;
     }
     
