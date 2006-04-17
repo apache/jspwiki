126795480e1b85b6ea992a9fa34e0ccd08be6fb0
Janne Jalkanen <jalkanen@apache.org>
2006-04-17 09:49:02 +0000

    javadocs added.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@625221 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/parser/VariableContent.java
+++ src/com/ecyrd/jspwiki/parser/VariableContent.java
@@ -1,7 +1,22 @@
-/*
- * (C) Janne Jalkanen 2005
- * 
- */
+/* 
+  JSPWiki - a JSP-based WikiWiki clone.
+
+  Copyright (C) 2001-2006 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 package com.ecyrd.jspwiki.parser;
 
 import org.apache.commons.lang.StringEscapeUtils;
@@ -10,6 +25,11 @@ import org.jdom.Text;
 import com.ecyrd.jspwiki.NoSuchVariableException;
 import com.ecyrd.jspwiki.WikiContext;
 
+/**
+ *  Stores the contents of a WikiVariable in a WikiDocument DOM tree.
+ *  @author Janne Jalkanen
+ *  @since  2.4
+ */
 public class VariableContent extends Text
 {
     private static final long serialVersionUID = 1L;
--- src/com/ecyrd/jspwiki/parser/WikiDocument.java
+++ src/com/ecyrd/jspwiki/parser/WikiDocument.java
@@ -1,3 +1,22 @@
+/* 
+   JSPWiki - a JSP-based WikiWiki clone.
+
+   Copyright (C) 2001-2006 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation; either version 2.1 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 package com.ecyrd.jspwiki.parser;
 
 import org.jdom.Document;
@@ -5,6 +24,17 @@ import org.jdom.Document;
 import com.ecyrd.jspwiki.WikiContext;
 import com.ecyrd.jspwiki.WikiPage;
 
+/**
+ *  Stores the DOM tree of a rendered WikiPage.  This class
+ *  extends the org.jdom.Document to provide some extra metadata
+ *  specific to JSPWiki.
+ *  <p>
+ *  The document is not stored as metadata in the WikiPage because
+ *  otherwise it could not be cached separately.
+ *  
+ *  @author Janne Jalkanen
+ *  @since  2.4
+ */
 public class WikiDocument extends Document
 {
     private static final long serialVersionUID = 0L;
@@ -14,6 +44,11 @@ public class WikiDocument extends Document
 
     private WikiContext m_context;
     
+    /**
+     *  Creates a new WikiDocument for a specific page.
+     * 
+     *  @param page The page to which this document refers to.
+     */
     public WikiDocument( WikiPage page )
     {
         m_page     = page;
@@ -33,7 +68,7 @@ public class WikiDocument extends Document
     {
         return m_page;
     }
-    
+
     public void setContext( WikiContext ctx )
     {
         m_context = ctx;
