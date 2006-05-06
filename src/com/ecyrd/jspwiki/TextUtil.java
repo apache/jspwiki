03b5284116307ec68f2d28a0db7a3c73ccc96187
Janne Jalkanen <jalkanen@apache.org>
2006-05-06 14:37:44 +0000

    added generateRandomPassword()
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@625295 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/TextUtil.java
+++ src/com/ecyrd/jspwiki/TextUtil.java
@@ -747,4 +747,25 @@ public class TextUtil
     
         return true;
     }
+
+    /** Length of password. @see #generateRandomPassword() */
+    public static final int PASSWORD_LENGTH = 8;
+    /**
+     * Generate a random String suitable for use as a temporary password.
+     * 
+     * @return String suitable for use as a temporary password
+     * @since 2.4
+     */
+    public static String generateRandomPassword() {
+        // Pick from some letters that won't be easily mistaken for each
+        // other. So, for example, omit o O and 0, 1 l and L.
+        String letters = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789+@";
+        
+        String pw = "";
+        for (int i=0; i<PASSWORD_LENGTH; i++) {
+            int index = (int)(Math.random()*letters.length());
+            pw += letters.substring(index, index+1);
+        }
+        return pw;
+    }
 }
