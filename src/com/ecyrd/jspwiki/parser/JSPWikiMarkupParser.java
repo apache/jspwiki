cc44cd076943a2b2265e6e05b65b07df8319b0e8
Janne Jalkanen <jalkanen@apache.org>
2006-07-25 11:11:30 +0000

    Added tolerance for bad data in the parser.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@625454 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/parser/JSPWikiMarkupParser.java
+++ src/com/ecyrd/jspwiki/parser/JSPWikiMarkupParser.java
@@ -631,86 +631,97 @@ public class JSPWikiMarkupParser
             //
 
             m_plainTextBuf = new StringBuffer(20);
-              
-            //
-            //  This is the heaviest part of parsing, and therefore we can
-            //  do some optimization here.
-            //
-            //  1) Only when the length of the buffer is big enough, we try to do the match
-            //
-
-            if( m_camelCaseLinks && !m_isEscaping && buf.length() > 3 )
-            {            
-                // System.out.println("Buffer="+buf);
-
-                while( m_camelCaseMatcher.contains( buf, m_camelCasePattern ) )
-                {
-                    MatchResult result = m_camelCaseMatcher.getMatch();
-
-                    String firstPart = buf.substring(0,result.beginOffset(0)); 
-                    String prefix = result.group(1);
-                    
-                    if( prefix == null ) prefix = "";
-                    
-                    String camelCase = result.group(2);
-                    String protocol  = result.group(3);
-                    String uri       = protocol+result.group(4);
-                    buf              = buf.substring(result.endOffset(0));
-                    
-                    m_currentElement.addContent( firstPart );
-                    
-                    //
-                    //  Check if the user does not wish to do URL or WikiWord expansion
-                    //
-                    if( prefix.endsWith("~") || prefix.indexOf('[') != -1 )
-                    {
-                        if( prefix.endsWith("~") ) prefix = prefix.substring(0,prefix.length()-1);
-                        if( camelCase != null )
-                        {
-                            m_currentElement.addContent( prefix+camelCase );
-                        }
-                        else if( protocol != null )
-                        {
-                            m_currentElement.addContent( prefix+uri );
-                        }
-                        continue;
-                    }
-                    
-                    //
-                    //  Fine, then let's check what kind of a link this was
-                    //  and emit the proper elements
-                    //
-                    if( protocol != null )
-                    {
-                        char c = uri.charAt(uri.length()-1);
-                        if( c == '.' || c == ',' )
-                        {
-                            uri = uri.substring(0,uri.length()-1);
-                            buf = c + buf;
-                        }
-                        // System.out.println("URI match "+uri);
-                        m_currentElement.addContent( prefix );
-                        makeDirectURILink( uri );
-                    }
-                    else
-                    {
-                        // System.out.println("Matched: '"+camelCase+"'");
-                        // System.out.println("Split to '"+firstPart+"', and '"+buf+"'");
-                        // System.out.println("prefix="+prefix);
-                        m_currentElement.addContent( prefix );
-                        
-                        makeCamelCaseLink( camelCase );
-                    }
-                }
-
-                m_currentElement.addContent( buf );
-            }
-            else
+             
+            try
             {
                 //
-                //  No camelcase asked for, just add the elements
+                //  This is the heaviest part of parsing, and therefore we can
+                //  do some optimization here.
                 //
-                m_currentElement.addContent( buf );
+                //  1) Only when the length of the buffer is big enough, we try to do the match
+                //
+
+                if( m_camelCaseLinks && !m_isEscaping && buf.length() > 3 )
+                {            
+                    // System.out.println("Buffer="+buf);
+
+                    while( m_camelCaseMatcher.contains( buf, m_camelCasePattern ) )
+                    {
+                        MatchResult result = m_camelCaseMatcher.getMatch();
+
+                        String firstPart = buf.substring(0,result.beginOffset(0)); 
+                        String prefix = result.group(1);
+                    
+                        if( prefix == null ) prefix = "";
+                    
+                        String camelCase = result.group(2);
+                        String protocol  = result.group(3);
+                        String uri       = protocol+result.group(4);
+                        buf              = buf.substring(result.endOffset(0));
+                    
+                        m_currentElement.addContent( firstPart );
+                    
+                        //
+                        //  Check if the user does not wish to do URL or WikiWord expansion
+                        //
+                        if( prefix.endsWith("~") || prefix.indexOf('[') != -1 )
+                        {
+                            if( prefix.endsWith("~") ) prefix = prefix.substring(0,prefix.length()-1);
+                            if( camelCase != null )
+                            {
+                                m_currentElement.addContent( prefix+camelCase );
+                            }
+                            else if( protocol != null )
+                            {
+                                m_currentElement.addContent( prefix+uri );
+                            }
+                            continue;
+                        }
+                    
+                        //
+                        //  Fine, then let's check what kind of a link this was
+                        //  and emit the proper elements
+                        //
+                        if( protocol != null )
+                        {
+                            char c = uri.charAt(uri.length()-1);
+                            if( c == '.' || c == ',' )
+                            {
+                                uri = uri.substring(0,uri.length()-1);
+                                buf = c + buf;
+                            }
+                            // System.out.println("URI match "+uri);
+                            m_currentElement.addContent( prefix );
+                            makeDirectURILink( uri );
+                        }
+                        else
+                        {
+                            // System.out.println("Matched: '"+camelCase+"'");
+                            // System.out.println("Split to '"+firstPart+"', and '"+buf+"'");
+                            // System.out.println("prefix="+prefix);
+                            m_currentElement.addContent( prefix );
+                        
+                            makeCamelCaseLink( camelCase );
+                        }
+                    }
+
+                    m_currentElement.addContent( buf );
+                }
+                else
+                {
+                    //
+                    //  No camelcase asked for, just add the elements
+                    //
+                    m_currentElement.addContent( buf );
+                }
+            }
+            catch( IllegalDataException e )
+            {
+                //
+                // Sometimes it's possible that illegal XML chars is added to the data.
+                // Here we make sure it does not stop parsing.
+                //
+                m_currentElement.addContent( makeError(e.getMessage()) );
             }
         }
         
