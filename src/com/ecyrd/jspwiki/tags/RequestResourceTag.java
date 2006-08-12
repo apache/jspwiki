f0c4a5ebc348c7a02cc6ecb0238c765bb9969def
Janne Jalkanen <jalkanen@apache.org>
2006-08-12 08:48:17 +0000

    Implemented.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@625605 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/tags/RequestResourceTag.java
+++ src/com/ecyrd/jspwiki/tags/RequestResourceTag.java
@@ -1,32 +1,45 @@
 package com.ecyrd.jspwiki.tags;
 
+import com.ecyrd.jspwiki.ui.TemplateManager;
+
+/**
+ *  Provides easy access to TemplateManager.addResourceRequest().  You may use
+ *  any of the request types defined there.
+ * 
+ *  @author jalkanen
+ *
+ */
 public class RequestResourceTag extends WikiTagBase
 {
     private static final long serialVersionUID = 0L;
     
     private String m_type;
-    private String m_path;
+    private String m_resource;
 
     public void initTag()
     {
         super.initTag();
-        m_type = m_path = null;
+        m_type = m_resource = null;
     }
     
     public int doWikiStartTag() throws Exception
-    {
-        m_wikiContext.getEngine().getTemplateManager();
+    {   
+        if( m_type != null && m_resource != null )
+        {
+            TemplateManager.addResourceRequest( m_wikiContext, m_type, m_resource );
+        }
+
         return SKIP_BODY;
     }
 
-    public String getPath()
+    public String getResource()
     {
-        return m_path;
+        return m_resource;
     }
 
-    public void setPath(String m_path)
+    public void setResource(String r)
     {
-        this.m_path = m_path;
+        m_resource = r;
     }
 
     public String getType()
@@ -34,9 +47,9 @@ public class RequestResourceTag extends WikiTagBase
         return m_type;
     }
 
-    public void setType(String m_type)
+    public void setType(String type)
     {
-        this.m_type = m_type;
+        m_type = type;
     }
 
 }
