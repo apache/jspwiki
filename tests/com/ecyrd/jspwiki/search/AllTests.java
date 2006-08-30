34c1ee91ba685c88c5ab89da7099aed3d99da261
Janne Jalkanen <jalkanen@apache.org>
2006-08-30 20:46:48 +0000

    Added.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@625652 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/com/ecyrd/jspwiki/search/AllTests.java
@@ -0,0 +1,24 @@
+package com.ecyrd.jspwiki.search;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import com.ecyrd.jspwiki.providers.*;
+
+public class AllTests extends TestCase
+{
+    public AllTests( String s )
+    {
+        super( s );
+    }
+
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite("Search engines");
+
+        suite.addTest( SearchManagerTest.suite() );
+
+        return suite;
+    }
+}
