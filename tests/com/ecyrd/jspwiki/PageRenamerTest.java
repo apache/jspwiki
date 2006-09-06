4e37c02920ff7d59cdaf1ef6e901021a7414680a
Janne Jalkanen <jalkanen@apache.org>
2006-09-06 07:04:47 +0000

    Added a bunch of initial tests.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@625697 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/com/ecyrd/jspwiki/PageRenamerTest.java
@@ -0,0 +1,181 @@
+package com.ecyrd.jspwiki;
+
+import java.util.Collection;
+import java.util.Properties;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class PageRenamerTest extends TestCase
+{
+    TestEngine m_engine;
+    
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+        
+        Properties props = new Properties();
+        
+        props.load( TestEngine.findTestProperties() );
+
+        props.setProperty( WikiEngine.PROP_MATCHPLURALS, "true" );
+        
+        TestEngine.emptyWorkDir();
+        m_engine = new TestEngine(props);  
+    }
+
+    protected void tearDown() throws Exception
+    {
+        super.tearDown();
+        
+        TestEngine.deleteTestPage("TestPage");
+        TestEngine.deleteTestPage("TestPage2");
+        TestEngine.deleteTestPage("FooTest");
+        TestEngine.deleteTestPage("Test");
+    }
+
+    public void testSimpleRename()
+        throws Exception
+    {
+        m_engine.saveText("TestPage", "the big lazy dog thing" );
+        
+        WikiPage p = m_engine.getPage("TestPage");
+        
+        WikiContext context = new WikiContext(m_engine, p);
+        
+        m_engine.renamePage(context, "TestPage", "FooTest", false);
+        
+        WikiPage newpage = m_engine.getPage("FooTest");
+        
+        assertNotNull( "no new page", newpage );
+        assertNull( "old page not gone", m_engine.getPage("TestPage") );
+        
+        // Refmgr
+        
+        Collection refs = m_engine.getReferenceManager().findCreated();
+        
+        assertEquals( "wrong list size", 1, refs.size() );
+        assertEquals( "wrong item", "FooTest", (String)refs.iterator().next() );
+    }
+    
+    public void testReferrerChange()
+       throws Exception
+    {
+        m_engine.saveText("TestPage", "foofoo" );
+        m_engine.saveText("TestPage2", "[TestPage]");
+        
+        WikiPage p = m_engine.getPage("TestPage");
+        
+        WikiContext context = new WikiContext(m_engine, p);
+        
+        m_engine.renamePage(context, "TestPage", "FooTest", true);
+        
+        String data = m_engine.getPureText("TestPage2", WikiProvider.LATEST_VERSION);
+        
+        assertEquals( "no rename", "[FooTest]", data.trim() );
+        
+        Collection refs = m_engine.getReferenceManager().findReferrers("TestPage");
+        
+        assertNull( "oldpage", refs );
+        
+        refs = m_engine.getReferenceManager().findReferrers( "FooTest" );
+        assertEquals( "new size", 1, refs.size() );
+        assertEquals( "wrong ref", "TestPage2", (String)refs.iterator().next() );
+    }
+
+    public void testReferrerChangeCC()
+        throws Exception
+    {
+        m_engine.saveText("TestPage", "foofoo" );
+        m_engine.saveText("TestPage2", "TestPage");
+     
+        WikiPage p = m_engine.getPage("TestPage");
+     
+        WikiContext context = new WikiContext(m_engine, p);
+     
+        m_engine.renamePage(context, "TestPage", "FooTest", true);
+     
+        String data = m_engine.getPureText("TestPage2", WikiProvider.LATEST_VERSION);
+     
+        assertEquals( "no rename", "FooTest", data.trim() );
+        Collection refs = m_engine.getReferenceManager().findReferrers("TestPage");
+        
+        assertNull( "oldpage", refs );
+        
+        refs = m_engine.getReferenceManager().findReferrers( "FooTest" );
+        assertEquals( "new size", 1, refs.size() );
+        assertEquals( "wrong ref", "TestPage2", (String)refs.iterator().next() );
+    }
+    
+    public void testReferrerChangeAnchor()
+        throws Exception
+    {
+        m_engine.saveText("TestPage", "foofoo" );
+        m_engine.saveText("TestPage2", "[TestPage#heading1]");
+     
+        WikiPage p = m_engine.getPage("TestPage");
+     
+        WikiContext context = new WikiContext(m_engine, p);
+     
+        m_engine.renamePage(context, "TestPage", "FooTest", true);
+     
+        String data = m_engine.getPureText("TestPage2", WikiProvider.LATEST_VERSION);
+     
+        assertEquals( "no rename", "[FooTest#heading1]", data.trim() );
+        Collection refs = m_engine.getReferenceManager().findReferrers("TestPage");
+        
+        assertNull( "oldpage", refs );
+        
+        refs = m_engine.getReferenceManager().findReferrers( "FooTest" );
+        assertEquals( "new size", 1, refs.size() );
+        assertEquals( "wrong ref", "TestPage2", (String)refs.iterator().next() );
+    }
+    
+    public void testReferrerChangeMultilink()
+        throws Exception
+    {
+        m_engine.saveText("TestPage", "foofoo" );
+        m_engine.saveText("TestPage2", "[TestPage] [TestPage] [linktext|TestPage] TestPage [linktext|TestPage] [TestPage#Anchor] [TestPage] TestPage [TestPage]");
+     
+        WikiPage p = m_engine.getPage("TestPage");
+     
+        WikiContext context = new WikiContext(m_engine, p);
+     
+        m_engine.renamePage(context, "TestPage", "FooTest", true);
+     
+        String data = m_engine.getPureText("TestPage2", WikiProvider.LATEST_VERSION);
+     
+        assertEquals( "no rename", "[FooTest] [FooTest] [linktext|FooTest] FooTest [linktext|FooTest] [FooTest#Anchor] [FooTest] FooTest [FooTest]", data );
+
+        Collection refs = m_engine.getReferenceManager().findReferrers("TestPage");
+        
+        assertNull( "oldpage", refs );
+        
+        refs = m_engine.getReferenceManager().findReferrers( "FooTest" );
+        assertEquals( "new size", 1, refs.size() );
+        assertEquals( "wrong ref", "TestPage2", (String)refs.iterator().next() );
+    }
+    
+    public void testReferrerNoWikiName()
+        throws Exception
+    {
+        m_engine.saveText("Test","foo");
+        m_engine.saveText("TestPage2", "[Test] [Test#anchor] test Test [test] [link|test] [link|test]");
+        
+        WikiPage p = m_engine.getPage("TestPage");
+        
+        WikiContext context = new WikiContext(m_engine, p);
+     
+        m_engine.renamePage(context, "Test", "TestPage", true);
+        
+        String data = m_engine.getPureText("TestPage2", WikiProvider.LATEST_VERSION );
+        
+        assertEquals( "wrong data", "[TestPage] [TestPage#anchor] test Test [TestPage] [link|TestPage] [link|TestPage]", data.trim() );
+    }
+    public static Test suite()
+    {
+        return new TestSuite( PageRenamerTest.class );
+    }
+
+}
