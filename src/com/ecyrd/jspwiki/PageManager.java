1389d48253192e522a5fd35b73e2ae450c695617
Janne Jalkanen <jalkanen@apache.org>
2006-09-24 19:22:50 +0000

    Now extends ModuleManager
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@625759 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/PageManager.java
+++ src/com/ecyrd/jspwiki/PageManager.java
@@ -24,12 +24,13 @@ import java.util.*;
 
 import org.apache.log4j.Logger;
 
+import com.ecyrd.jspwiki.event.WikiEventManager;
+import com.ecyrd.jspwiki.event.WikiPageEvent;
+import com.ecyrd.jspwiki.modules.ModuleManager;
 import com.ecyrd.jspwiki.providers.ProviderException;
 import com.ecyrd.jspwiki.providers.RepositoryModifiedException;
 import com.ecyrd.jspwiki.providers.VersioningProvider;
 import com.ecyrd.jspwiki.providers.WikiPageProvider;
-import com.ecyrd.jspwiki.event.WikiPageEvent;
-import com.ecyrd.jspwiki.event.WikiEventManager;
 import com.ecyrd.jspwiki.util.ClassUtil;
 import com.ecyrd.jspwiki.util.WikiBackgroundThread;
 
@@ -44,7 +45,7 @@ import com.ecyrd.jspwiki.util.WikiBackgroundThread;
 // FIXME: This class currently only functions just as an extra layer over providers,
 //        complicating things.  We need to move more provider-specific functionality
 //        from WikiEngine (which is too big now) into this class.
-public class PageManager
+public class PageManager extends ModuleManager
 {
     private static final long serialVersionUID = 1L;
     
@@ -71,6 +72,8 @@ public class PageManager
     public PageManager( WikiEngine engine, Properties props )
         throws WikiException
     {
+        super( engine );
+        
         String classname;
 
         m_engine = engine;
