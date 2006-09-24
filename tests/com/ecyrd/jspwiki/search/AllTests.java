851e379ba5c3f8a7a97a2f35f44906b6481495ab
Janne Jalkanen <jalkanen@apache.org>
2006-09-24 14:41:33 +0000

    Cleaned warning
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@625754 13f79535-47bb-0310-9956-ffa450edef68

--- tests/com/ecyrd/jspwiki/search/AllTests.java
+++ tests/com/ecyrd/jspwiki/search/AllTests.java
@@ -4,8 +4,6 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import com.ecyrd.jspwiki.providers.*;
-
 public class AllTests extends TestCase
 {
     public AllTests( String s )
