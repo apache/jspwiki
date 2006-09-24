fc15dbc7cbe0d2bd2e5f699e1e2a2bdbfee32ea3
Janne Jalkanen <jalkanen@apache.org>
2006-09-24 19:23:54 +0000

    Added checkCompatibility()
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@625761 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/modules/ModuleManager.java
+++ src/com/ecyrd/jspwiki/modules/ModuleManager.java
@@ -1,5 +1,8 @@
 package com.ecyrd.jspwiki.modules;
 
+import com.ecyrd.jspwiki.Release;
+import com.ecyrd.jspwiki.WikiEngine;
+
 /**
  *  Superclass for all JSPWiki managers for modules (plugins, etc).
  *  @author jalkanen
@@ -13,4 +16,27 @@ public abstract class ModuleManager
      */
     public static final String PLUGIN_RESOURCE_LOCATION = "ini/jspwiki_module.xml";
     
+    public static final String LOAD_INCOMPATIBLE_MODULES = "jspwiki.loadIncompatibleModules";
+    
+    protected WikiEngine m_engine;
+    
+    private boolean m_loadIncompatibleModules = false;
+    
+    public ModuleManager( WikiEngine engine )
+    {
+        m_engine = engine;
+    }
+    
+    public boolean checkCompatibility( WikiModuleInfo info )
+    {
+        if( !m_loadIncompatibleModules )
+        {
+            String minVersion = info.getMinVersion();
+            String maxVersion = info.getMaxVersion();
+            
+            return Release.isNewerOrEqual( minVersion ) && Release.isOlderOrEqual( maxVersion );
+        }
+        
+        return true;
+    }
 }
