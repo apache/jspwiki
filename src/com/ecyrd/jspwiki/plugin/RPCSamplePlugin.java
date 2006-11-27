e7962255849c900f0e78d6ed6fc73fa0523e2aab
Janne Jalkanen <jalkanen@apache.org>
2006-11-27 21:39:58 +0000

    Added as an example.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@625920 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/com/ecyrd/jspwiki/plugin/RPCSamplePlugin.java
@@ -0,0 +1,39 @@
+package com.ecyrd.jspwiki.plugin;
+
+import java.util.Map;
+
+import com.ecyrd.jspwiki.WikiContext;
+import com.ecyrd.jspwiki.rpc.RPCCallable;
+import com.ecyrd.jspwiki.rpc.json.JSONRPCManager;
+
+/**
+ *  Simple plugin which shows how to add JSON calls to your plugin.
+ * 
+ *  @author Janne Jalkanen
+ *  @since  2.5.4
+ */
+public class RPCSamplePlugin implements WikiPlugin, RPCCallable
+{  
+    /**
+     *  This method is called when the Javascript is encountered by
+     *  the browser.
+     *  @param echo
+     *  @return
+     */
+    public String myFunction(String echo)
+    {
+        return "JSON says: "+echo;
+    }
+    
+
+    
+    public String execute(WikiContext context, Map params) throws PluginException
+    {
+        JSONRPCManager.registerJSONObject( context, this );
+        
+        String s = JSONRPCManager.emitJSONCall( context, this, "myFunction", "'foo'" );
+        
+        return s;
+    }
+
+}
