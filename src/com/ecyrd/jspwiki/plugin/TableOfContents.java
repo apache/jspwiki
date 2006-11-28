0e815e07469293c92f448d8917fcf2bc8fb9ded8
Janne Jalkanen <jalkanen@apache.org>
2006-11-28 21:58:47 +0000

    wrong resource
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@625933 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/plugin/TableOfContents.java
+++ src/com/ecyrd/jspwiki/plugin/TableOfContents.java
@@ -95,7 +95,7 @@ public class TableOfContents
         }
 
         String titleSection = hd.m_titleSection.replace( '%', '_' );
-	String pageName = context.getEngine().encodeName(context.getPage().getName()).replace( '%', '_' );
+        String pageName = context.getEngine().encodeName(context.getPage().getName()).replace( '%', '_' );
         
         String url = context.getURL( WikiContext.VIEW, context.getPage().getName() );
         String sectref = "#section-"+pageName+"-"+titleSection;
@@ -128,7 +128,7 @@ public class TableOfContents
     {
         WikiEngine engine = context.getEngine();
         WikiPage   page   = context.getPage();
-        ResourceBundle rb = context.getBundle("com.ecyrd.jspwiki.plugin.languagebundle");
+        ResourceBundle rb = context.getBundle("com.ecyrd.jspwiki.plugin.PluginResources");
         
         if( context.getVariable( VAR_ALREADY_PROCESSING ) != null )
             return "Table of Contents";
