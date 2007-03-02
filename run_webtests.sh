6f145273b89bf672ce6918765fcfcc9784c16263
Janne Jalkanen <jalkanen@apache.org>
2007-03-02 17:27:43 +0000

    Improved to start/shutdown tomcat and clean away previous contexts.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/branches/JSPWIKI_2_4_BRANCH@626143 13f79535-47bb-0310-9956-ffa450edef68

--- run_webtests.sh
+++ run_webtests.sh
@@ -2,12 +2,24 @@
 
 # Couple of notes:
 #
-# Make sure your CATALINA_HOME is set before you run this, and that
-# your tomcat is running
-# Also, make sure that your junit.jar can be found in the classpath.
+# This script is destructive in the sense that it will restart your
+# tomcat, and also remove configuration files.
+#
+# Make sure that your junit.jar can be found in the classpath.
 # The last resort is to drop it in your $JAVA_HOME/lib/ext, or on OSX
 # /Library/Java/External/
+#
+# This is tuned for Tomcat 5.5.  Any previous/later version may have
+# unintended consequences.
+#
 
-export CATALINA_HOME=${HOME}/Java/tomcat/
+export CATALINA_HOME=${HOME}/Java/tomcat-webtest
+
+${CATALINA_HOME}/bin/shutdown.sh
+
+rm -rf ${CATALINA_HOME}/webapps/test*
+rm -rf ${CATALINA_HOME}/conf/Catalina/localhost/test*
+
+${CATALINA_HOME}/bin/startup.sh
 
 ant -Dbuild.properties=build.properties -find build.xml webtests
