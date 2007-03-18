1e3dac18c866a49e49e2c2551fe05e5283f480b5
Janne Jalkanen <jalkanen@apache.org>
2007-03-18 18:08:56 +0000

    Fixed to follow 2.5.30 BasicPageFilter signature.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@626162 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/search/SearchManager.java
+++ src/com/ecyrd/jspwiki/search/SearchManager.java
@@ -31,6 +31,7 @@ import com.ecyrd.jspwiki.event.WikiEventListener;
 import com.ecyrd.jspwiki.event.WikiEventUtils;
 import com.ecyrd.jspwiki.event.WikiPageEvent;
 import com.ecyrd.jspwiki.filters.BasicPageFilter;
+import com.ecyrd.jspwiki.filters.FilterException;
 import com.ecyrd.jspwiki.modules.InternalModule;
 import com.ecyrd.jspwiki.parser.MarkupParser;
 import com.ecyrd.jspwiki.providers.ProviderException;
@@ -119,7 +120,7 @@ public class SearchManager
      * @throws WikiException
      */
     public void initialize(WikiEngine engine, Properties properties)
-        throws WikiException
+        throws FilterException
     {
         m_engine = engine;
         
