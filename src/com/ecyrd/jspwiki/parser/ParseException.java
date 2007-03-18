470150994968ab1b0715d219671c36378833eab5
Janne Jalkanen <jalkanen@apache.org>
2007-03-18 18:05:39 +0000

    Added serialVersionUID
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@626157 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/parser/ParseException.java
+++ src/com/ecyrd/jspwiki/parser/ParseException.java
@@ -10,6 +10,8 @@ import com.ecyrd.jspwiki.WikiException;
  */
 public class ParseException extends WikiException
 {
+    private static final long serialVersionUID = 1L;
+
     public ParseException(String msg)
     {
         super(msg);
