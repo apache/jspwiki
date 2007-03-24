91b1be2bab0e3b7458f289ad8a1be7f48006ff4c
Janne Jalkanen <jalkanen@apache.org>
2007-03-24 22:24:49 +0000

    Added setCacheable()/isCacheable() Improved javadocs.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@626182 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/attachment/Attachment.java
+++ src/com/ecyrd/jspwiki/attachment/Attachment.java
@@ -35,11 +35,20 @@ public class Attachment
     private String m_fileName;
     private String m_parentName;
     private int    m_status = CREATED;
+    private boolean m_cacheable = true;
 
     public static final int CREATED       = 0;
     public static final int UPLOADING     = 1;
     public static final int COMPLETE      = 2;
 
+    /**
+     *  Creates a new attachment.  The final name of the attachment will be 
+     *  a synthesis of the parent page name and the file name.
+     *  
+     *  @param engine     The WikiEngine which is hosting this attachment.
+     *  @param parentPage The page which will contain this attachment.
+     *  @param fileName   The file name for the attachment.
+     */
     public Attachment( WikiEngine engine, String parentPage, String fileName )
     {
         super( engine, parentPage+"/"+fileName );
@@ -48,17 +57,31 @@ public class Attachment
         m_fileName   = fileName;
     }
 
+    /**
+     *  Returns a human-readable, only-debugging-suitable description.
+     */
     public String toString()
     {
         return "Attachment ["+getName()+";mod="+getLastModified()+
                ";status="+m_status+"]";
     }
 
+    /**
+     *  Returns the file name of the attachment.
+     *  
+     *  @return A String with the file name.
+     */
     public String getFileName()
     {
-        return( m_fileName );
+        return m_fileName;
     }
 
+    /**
+     *  Sets the file name of this attachment. 
+     *  
+     *  @param name The name of the attachment.  Must be a legal file name without
+     *              the path.
+     */
     public void setFileName( String name )
     {
         m_fileName = name;
@@ -74,8 +97,40 @@ public class Attachment
         m_status = status;
     }
 
+    /**
+     *  Returns the name of the parent of this Attachment, i.e. the page
+     *  which contains this attachment.
+     *  
+     *  @return String depicting the parent of the attachment.
+     */
     public String getParentName()
     {
         return m_parentName;
+    }
+
+    /**
+     *  Returns true, if this attachment can be cached by the user agent.  By default
+     *  attachments are cacheable.
+     *  
+     *  @return False, if the attachment should not be cached by the user agent.
+     *  @since 2.5.34
+     */
+    public boolean isCacheable()
+    {
+        return m_cacheable;
+    }
+
+    /**
+     *  Sets this attachment to be cacheable or not.  This mostly concerns things
+     *  like DynamicAttachments, but it may be useful for certain AttachmentProviders
+     *  as well.
+     *  
+     *  @param value True or false, depending on whether you want this attachment
+     *               to be cacheable or not.
+     *  @since 2.5.34
+     */
+    public void setCacheable(boolean value)
+    {
+        m_cacheable = value;
     }
 }
