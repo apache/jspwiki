ed705cfa3c8fa76def571e0590ea2d857f335da8
Janne Jalkanen <jalkanen@apache.org>
2007-05-07 19:05:00 +0000

    Refactored to JSPWiki code conventions. Allocates StringBuffer now with a default, large buffer size to avoid excess allocation overhead.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@626351 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/tags/TabbedSectionTag.java
+++ src/com/ecyrd/jspwiki/tags/TabbedSectionTag.java
@@ -24,36 +24,40 @@ import javax.servlet.jsp.tagext.*;
 public class TabbedSectionTag extends BodyTagSupport
 {
     private static final long serialVersionUID = 1702437933960026481L;
-    private String       defaultTabId;
-    private String       firstTabId;
-    private boolean      defaultTabFound = false;
-    private StringBuffer sb = new StringBuffer();
+    private String       m_defaultTabId;
+    private String       m_firstTabId;
+    private boolean      m_defaultTabFound = false;
+  
+    private StringBuffer m_buffer = new StringBuffer(BUFFER_SIZE);
     
     private static final int FIND_DEFAULT_TAB = 0;
     private static final int GENERATE_TABMENU = 1;
     private static final int GENERATE_TABBODY = 2;
+    
+    private static final int BUFFER_SIZE      = 1024;
+    
     private              int state            = FIND_DEFAULT_TAB; 
 
     public void release()
     {
         super.release();
-        defaultTabId = firstTabId = null;
-        defaultTabFound = false;
-        sb = new StringBuffer();
+        m_defaultTabId = m_firstTabId = null;
+        m_defaultTabFound = false;
+        m_buffer = new StringBuffer();
         state = FIND_DEFAULT_TAB;
     }
     
     public void setDefaultTab(String anDefaultTabId)
     {
-        defaultTabId = anDefaultTabId;
+        m_defaultTabId = anDefaultTabId;
     }
     
     public boolean validateDefaultTab( String aTabId )
     {
-        if( firstTabId == null ) firstTabId = aTabId;
-        if( aTabId.equals( defaultTabId ) ) defaultTabFound = true;
+        if( m_firstTabId == null ) m_firstTabId = aTabId;
+        if( aTabId.equals( m_defaultTabId ) ) m_defaultTabFound = true;
         
-        return ( aTabId.equals( defaultTabId ) );
+        return ( aTabId.equals( m_defaultTabId ) );
     }
     
     public int doStartTag() throws JspTagException
@@ -75,7 +79,7 @@ public class TabbedSectionTag extends BodyTagSupport
     {
         if( isStateFindDefaultTab() )         
         { 
-            if( !defaultTabFound ) { defaultTabId = firstTabId; }
+            if( !m_defaultTabFound ) { m_defaultTabId = m_firstTabId; }
             state = GENERATE_TABMENU; 
             return EVAL_BODY_BUFFERED;
         }
@@ -83,10 +87,10 @@ public class TabbedSectionTag extends BodyTagSupport
         { 
             if( bodyContent != null )
             {
-                sb.append( "<div class=\"tabmenu\">" );
-                sb.append( bodyContent.getString() );
+                m_buffer.append( "<div class=\"tabmenu\">" );
+                m_buffer.append( bodyContent.getString() );
                 bodyContent.clearBody();
-                sb.append( "</div>\n" );
+                m_buffer.append( "</div>\n" );
             }
             state = GENERATE_TABBODY; 
             return EVAL_BODY_BUFFERED;
@@ -95,10 +99,10 @@ public class TabbedSectionTag extends BodyTagSupport
         { 
             if( bodyContent != null )
             {      
-                sb.append( "<div class=\"tabs\">" );
-                sb.append( bodyContent.getString() );
+                m_buffer.append( "<div class=\"tabs\">" );
+                m_buffer.append( bodyContent.getString() );
                 bodyContent.clearBody();
-                sb.append( "<div style=\"clear:both; height:0px;\" > </div>\n</div>\n" );
+                m_buffer.append( "<div style=\"clear:both; height:0px;\" > </div>\n</div>\n" );
             }
             return SKIP_BODY; 
         }
@@ -109,9 +113,9 @@ public class TabbedSectionTag extends BodyTagSupport
     {
         try
         {
-            if( sb.length() > 0 ) 
+            if( m_buffer.length() > 0 ) 
             {
-                getPreviousOut().write( sb.toString() );
+                getPreviousOut().write( m_buffer.toString() );
             }
         }
         catch(java.io.IOException e)
@@ -120,11 +124,11 @@ public class TabbedSectionTag extends BodyTagSupport
         }
         
         //now reset some stuff for the next run -- ugh.
-        sb    = new StringBuffer();
+        m_buffer    = new StringBuffer(BUFFER_SIZE);
         state = FIND_DEFAULT_TAB; 
-        defaultTabId    = null;
-        firstTabId      = null;
-        defaultTabFound = false;
+        m_defaultTabId    = null;
+        m_firstTabId      = null;
+        m_defaultTabFound = false;
         return EVAL_PAGE;
     }
     
