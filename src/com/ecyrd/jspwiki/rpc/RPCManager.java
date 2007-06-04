5712f44b3ebc2d5667eccca8d4a83a6a034b4fbf
Janne Jalkanen <jalkanen@apache.org>
2007-06-04 18:58:58 +0000

    Proper header
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@626493 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/rpc/RPCManager.java
+++ src/com/ecyrd/jspwiki/rpc/RPCManager.java
@@ -1,3 +1,22 @@
+/*
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Copyright (C) 2001-2007 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package com.ecyrd.jspwiki.rpc;
 
 /**
@@ -8,6 +27,7 @@ package com.ecyrd.jspwiki.rpc;
  */
 public class RPCManager
 {
+    
     /**
      *  Gets an unique RPC ID for a callable object.  This is required because a plugin
      *  does not know how many times it is already been invoked.
@@ -16,7 +36,7 @@ public class RPCManager
      *  it always starts with an ASCII character.  Therefore the id is suitable as a
      *  programming language construct directly (e.g. object name).
      *  
-     *  @param c
+     *  @param c An RPCCallable
      *  @return An unique id for the callable.
      */
     public static String getId( RPCCallable c )
