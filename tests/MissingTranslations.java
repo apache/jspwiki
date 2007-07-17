7c6b66ce3e0432d6f9f98d2faf7482faeead7dbb
Christoph Sauer <csauer@apache.org>
2007-07-17 11:07:07 +0000

    added utility to diff i18 property files
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@626645 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tests/MissingTranslations.java
@@ -0,0 +1,57 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+
+/**
+ * Simple utility that shows you what properties
+ * are missing in a i18n file from the default properties
+ * 
+ * @author Christoph Sauer
+ *
+ */
+public class MissingTranslations
+{
+
+    // Change this to your settings...
+    static String base = "C:/workspace/JSPWiki"; 
+    static String suffix = "de_DE";
+    
+    public static void main(String[] args) throws IOException
+    {
+        diff ("/etc/i18n/CoreResources.properties", 
+              "/etc/i18n/CoreResources_" + suffix + ".properties");
+
+        diff ("/etc/i18n/templates/default.properties", 
+              "/etc/i18n/templates/default_" + suffix + ".properties");
+
+        diff ("/src/com/ecyrd/jspwiki/plugin/PluginResources.properties", 
+              "/src/com/ecyrd/jspwiki/plugin/PluginResources_" + suffix + ".properties");
+    }   
+    
+    public static void diff(String en, String other) throws FileNotFoundException, IOException {
+
+        //Standard Properties
+        Properties p = new Properties();
+        p.load( new FileInputStream(new File(base + en)) );
+
+        Properties p2 = new Properties();
+        p2.load( new FileInputStream(new File(base + other)) );
+
+        System.out.println("Missing Properties in " + other + ":");
+        System.out.println("------------------------------------");
+        Enumeration enum = p.propertyNames();        
+        while(enum.hasMoreElements()) {
+            String name = (String)enum.nextElement();
+            String value = p.getProperty(name);
+            
+            if (p2.get(name) == null) {
+                System.out.println(name + " = " + value);
+            }
+        }
+        System.out.println("");
+
+    }
+}
