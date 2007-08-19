bcc3e5a63136715eab9e96f2704091c64706dc03
Janne Jalkanen <jalkanen@apache.org>
2007-08-19 16:54:47 +0000

    Whitespace removal
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@626715 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/rss/RSS20Feed.java
+++ src/com/ecyrd/jspwiki/rss/RSS20Feed.java
@@ -1,4 +1,4 @@
-/* 
+/*
   JSPWiki - a JSP-based WikiWiki clone.
 
   Copyright (C) 2001-2002 Janne Jalkanen (Janne.Jalkanen@iki.fi)
@@ -29,8 +29,10 @@ import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.ServletContext;
+import javax.xml.transform.Result;
 
 import org.jdom.Element;
+import org.jdom.ProcessingInstruction;
 import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
 
@@ -43,7 +45,7 @@ import com.ecyrd.jspwiki.providers.ProviderException;
 
 /**
  *  Represents an RSS 2.0 feed (with enclosures).
- *  
+ *
  *  @author jalkanen
  *
  *  @since 2.2.27
@@ -60,47 +62,47 @@ public class RSS20Feed extends Feed
     {
         ArrayList list = new ArrayList();
         SimpleDateFormat fmt = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss 'GMT'");
-        
+
         WikiEngine engine = m_wikiContext.getEngine();
         ServletContext servletContext = null;
-        
+
         if( m_wikiContext.getHttpRequest() != null )
             servletContext = m_wikiContext.getHttpRequest().getSession().getServletContext();
-        
+
         for( Iterator i = m_entries.iterator(); i.hasNext(); )
         {
             Entry e = (Entry)i.next();
             WikiPage p = e.getPage();
-            
+
             String url = e.getURL();
-            
+
             Element item = new Element("item");
-            
+
             item.addContent( new Element("link").setText(url) );
-            
+
             item.addContent( new Element("title").setText( e.getTitle()) );
-         
+
             item.addContent( new Element("description").setText( e.getContent()) );
-            
+
             //
             //  Attachments for enclosures
             //
-            
+
             if( engine.getAttachmentManager().hasAttachments(p) && servletContext != null )
             {
                 try
                 {
                     Collection c = engine.getAttachmentManager().listAttachments(p);
-                
+
                     for( Iterator a = c.iterator(); a.hasNext(); )
                     {
                         Attachment att = (Attachment) a.next();
-                    
+
                         Element attEl = new Element("enclosure");
                         attEl.setAttribute( "url", engine.getURL(WikiContext.ATTACH, att.getName(), null, true ) );
                         attEl.setAttribute( "length", Long.toString(att.getSize()) );
                         attEl.setAttribute( "type", getMimeType( servletContext, att.getFileName() ) );
-                        
+
                         item.addContent( attEl );
                     }
                 }
@@ -109,70 +111,70 @@ public class RSS20Feed extends Feed
                     // FIXME: log.info("Can't get attachment data",ex);
                 }
             }
-            
+
             //
             //  Modification date.
             //
             Calendar cal = Calendar.getInstance();
             cal.setTime( p.getLastModified() );
-            cal.add( Calendar.MILLISECOND, 
-                     - (cal.get( Calendar.ZONE_OFFSET ) + 
+            cal.add( Calendar.MILLISECOND,
+                     - (cal.get( Calendar.ZONE_OFFSET ) +
                         (cal.getTimeZone().inDaylightTime( p.getLastModified() ) ? cal.get( Calendar.DST_OFFSET ) : 0 )) );
 
             item.addContent( new Element("pubDate").setText(fmt.format(cal.getTime())) );
-            
+
             list.add( item );
         }
-        
+
         return list;
     }
-    
+
     public String getString()
     {
         WikiEngine engine = m_wikiContext.getEngine();
         Element root = new Element("rss");
         root.setAttribute("version","2.0");
-        
+
         Element channel = new Element("channel");
         root.addContent( channel );
-        
+
         //
         //  Mandatory parts
         //
         channel.addContent( new Element("title").setText( getChannelTitle() ) );
         channel.addContent( new Element("link").setText(engine.getBaseURL()));
         channel.addContent( new Element("description").setText( getChannelDescription() ));
-        
+
         //
         //  Optional
         //
         channel.addContent( new Element("language").setText(getChannelLanguage()));
         channel.addContent( new Element("generator").setText("JSPWiki "+Release.VERSTR));
-        
+
         String mail = engine.getVariable(m_wikiContext,RSSGenerator.PROP_RSS_AUTHOREMAIL);
         if( mail != null )
         {
             String editor = engine.getVariable( m_wikiContext,RSSGenerator.PROP_RSS_AUTHOR );
-            
+
             if( editor != null )
                 mail = mail + " ("+editor+")";
-            
+
             channel.addContent( new Element("managingEditor").setText(mail) );
         }
-        
+
         //
         //  Items
         //
-        
+
         channel.addContent( getItems() );
-        
+
         //
         //  aaand output
         //
         XMLOutputter output = new XMLOutputter();
-        
+
         output.setFormat( Format.getPrettyFormat() );
-        
+
         try
         {
             StringWriter res = new StringWriter();
