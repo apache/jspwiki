6112e202ca507f6e0d93dbd40012b3b8d57d8cc8
Janne Jalkanen <jalkanen@apache.org>
2007-12-25 22:42:38 +0000

    Added isValidUserProfile()
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@626948 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/filters/SpamFilter.java
+++ src/com/ecyrd/jspwiki/filters/SpamFilter.java
@@ -36,6 +36,7 @@ import org.apache.oro.text.regex.*;
 
 import com.ecyrd.jspwiki.*;
 import com.ecyrd.jspwiki.attachment.Attachment;
+import com.ecyrd.jspwiki.auth.user.UserProfile;
 import com.ecyrd.jspwiki.providers.ProviderException;
 import com.ecyrd.jspwiki.ui.EditorManager;
 
@@ -941,6 +942,30 @@ public class SpamFilter
             return ctx.getURL( WikiContext.NONE, "Captcha.jsp", "page="+ctx.getEngine().encodeName(ctx.getPage().getName()) );
         
         return ctx.getURL( WikiContext.VIEW, m_errorPage );
+    }
+
+    /**
+     *  Checks whether the UserProfile matches certain checks.
+     *  
+     *  @param profile
+     *  @return
+     *  @since 2.6.1
+     */
+    public boolean isValidUserProfile( WikiContext context, UserProfile profile )
+    {
+        try
+        {
+            checkPatternList( context, profile.getEmail(), profile.getEmail() );
+            checkPatternList( context, profile.getFullname(), profile.getFullname() );
+            checkPatternList( context, profile.getLoginName(), profile.getLoginName() );
+        }
+        catch( RedirectException e )
+        {
+            log.info("Detected attempt to create a spammer user account (see above for rejection reason)");
+            return false;
+        }
+        
+        return true;
     }
 
     /**
