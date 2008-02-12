eee998c6fdd5f0508ca9f32cde5e59450646206a
Janne Jalkanen <jalkanen@apache.org>
2008-02-12 21:42:05 +0000

    Switched to svn.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@627130 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/Release.java
+++ src/com/ecyrd/jspwiki/Release.java
@@ -57,7 +57,7 @@ public final class Release
      *  <p>
      *  If the POSTFIX is empty, it is not added to the version string.
      */
-    private static final String    POSTFIX       = "cvs";
+    private static final String    POSTFIX       = "svn";
 
     /** The JSPWiki major version. */
     public static final int        VERSION       = 2;
