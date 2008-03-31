664a4229f6a38fe7da39b3a1cc411dd3ae370b9e
Janne Jalkanen <jalkanen@apache.org>
2008-03-31 16:44:59 +0000

    Changed preamble to ASF copyright and removed extra @author tags.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@643054 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/QueryItem.java
+++ src/com/ecyrd/jspwiki/QueryItem.java
@@ -1,21 +1,19 @@
-/* 
+/*
     JSPWiki - a JSP-based WikiWiki clone.
 
-    Copyright (C) 2001-2002 Janne Jalkanen (Janne.Jalkanen@iki.fi)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation; either version 2.1 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    Copyright 2008 The Apache Software Foundation 
+    
+    Licensed under the Apache License, Version 2.0 (the "License"); 
+    you may not use this file except in compliance with the License. 
+    You may obtain a copy of the License at 
+    
+      http://www.apache.org/licenses/LICENSE-2.0 
+      
+    Unless required by applicable law or agreed to in writing, software 
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+    See the License for the specific language governing permissions and 
+    limitations under the License.    
  */
 package com.ecyrd.jspwiki;
 
@@ -23,7 +21,6 @@ package com.ecyrd.jspwiki;
  *  This simple class just fulfils the role of a container
  *  for searches.  It tells the word and whether it is requested or not.
  *
- *  @author Janne Jalkanen
  */
 public class QueryItem
 {
