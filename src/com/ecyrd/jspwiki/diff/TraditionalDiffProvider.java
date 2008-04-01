ce22389a0819885192b532da23a2d2b76652071f
Janne Jalkanen <jalkanen@apache.org>
2008-04-01 18:31:51 +0000

    Changed preamble to ASF copyright and removed extra @author tags.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@643495 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/diff/TraditionalDiffProvider.java
+++ src/com/ecyrd/jspwiki/diff/TraditionalDiffProvider.java
@@ -1,21 +1,22 @@
 /*
    JSPWiki - a JSP-based WikiWiki clone.
 
-   Copyright (C) 2001-2002 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
 
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU Lesser General Public License as published by
-   the Free Software Foundation; either version 2.1 of the License, or
-   (at your option) any later version.
+       http://www.apache.org/licenses/LICENSE-2.0
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.  
 */
 
 package com.ecyrd.jspwiki.diff;
@@ -41,7 +42,7 @@ import com.ecyrd.jspwiki.i18n.InternationalizationManager;
 
 /**
  * This is the JSPWiki 'traditional' diff.
- * @author Janne Jalkanen
+ * 
  * @author Erik Bunn
  * @author <a href="mailto:hps@intermeta.de">Henning P. Schmiedehausen</a>
  */
