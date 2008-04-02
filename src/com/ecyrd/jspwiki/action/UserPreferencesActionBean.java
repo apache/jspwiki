6e005ff21216973a09d0787ebc06d412d2d32088
Andrew R. Jaquith <ajaquith@apache.org>
2008-04-02 02:35:04 +0000

    Committed some items before syncing with current 2.7.0 branch. Not all of these things work!
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/branches/JSPWIKI_2_9_STRIPES_BRANCH@643690 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/action/UserPreferencesActionBean.java
+++ src/com/ecyrd/jspwiki/action/UserPreferencesActionBean.java
@@ -4,12 +4,14 @@ import java.security.Principal;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import net.sourceforge.stripes.action.*;
 import net.sourceforge.stripes.validation.Validate;
 
 import com.ecyrd.jspwiki.auth.login.CookieAssertionLoginModule;
 import com.ecyrd.jspwiki.auth.permissions.WikiPermission;
+import com.ecyrd.jspwiki.ui.EditorManager;
 
 /**
  * @author Andrew Jaquith
@@ -20,6 +22,10 @@ public class UserPreferencesActionBean extends AbstractActionBean
 {
     private String m_assertedName = null;
 
+    private String m_editor = null;
+    
+    private String m_redirect = null;
+
     /**
      * Clears the user's asserted name by removing the cookie from the user's
      * session, then logs out the user by redirecting to <code>/Login.jsp</code>.
@@ -27,7 +33,7 @@ public class UserPreferencesActionBean extends AbstractActionBean
      * @return a redirection to the logout page
      */
     @HandlesEvent("clearAssertedName")
-    @EventPermission(permissionClass=WikiPermission.class, target="${engine.applicationName}", actions=WikiPermission.EDIT_PREFERENCES_ACTION)
+    @EventPermission(permissionClass = WikiPermission.class, target = "${engine.applicationName}", actions = WikiPermission.EDIT_PREFERENCES_ACTION)
     public Resolution clearAssertedName()
     {
         HttpServletResponse response = getContext().getResponse();
@@ -44,9 +50,9 @@ public class UserPreferencesActionBean extends AbstractActionBean
     public Resolution editFavorites()
     {
         Principal principal = this.getCurrentUser();
-        return new RedirectResolution("/Edit.jsp?"+principal.getName()+"Favorites");
+        return new RedirectResolution("/Edit.jsp?" + principal.getName() + "Favorites");
     }
-    
+
     /**
      * Sets the user's asserted name by setting a cookie in the user's session,
      * then redirects to the wiki front page. This method will <em>not</em>
@@ -56,10 +62,10 @@ public class UserPreferencesActionBean extends AbstractActionBean
      */
     @DefaultHandler
     @HandlesEvent("createAssertedName")
-    @EventPermission(permissionClass=WikiPermission.class, target="${engine.applicationName}", actions=WikiPermission.EDIT_PREFERENCES_ACTION)
+    @EventPermission(permissionClass = WikiPermission.class, target = "${engine.applicationName}", actions = WikiPermission.EDIT_PREFERENCES_ACTION)
     public Resolution createAssertedName()
     {
-        if ( !getWikiSession().isAuthenticated() )
+        if (!getWikiSession().isAuthenticated())
         {
             HttpServletRequest request = getContext().getRequest();
             HttpServletResponse response = getContext().getResponse();
@@ -85,10 +91,59 @@ public class UserPreferencesActionBean extends AbstractActionBean
      * @param name
      *            the asserted name
      */
-    @Validate(required=true, on="createAssertedName")
+    @Validate(required = true, on = "createAssertedName")
     public void setAssertedName(String name)
     {
         m_assertedName = name;
     }
 
+    /**
+     * Returns the editor specified by the user.
+     * 
+     * @return the editor
+     */
+    public String getEditor()
+    {
+        return m_editor;
+    }
+
+    /**
+     * Sets the editor for editing preferences. If this
+     * UserPreferencesActionBean has an associated WikiActionBeanContext, and
+     * that context's request is not <code>null</code>, this method also sets
+     * the editor as an HTTP session attribute.
+     * <em>Note: this functionality was taken directly from the 2.6 UserPreferences.jsp.</em>
+     * 
+     * @param editor
+     *            the editor
+     */
+    @Validate(required = false)
+    public void setEditor(String editor)
+    {
+        m_editor = editor;
+        if (this.getContext() != null && getContext().getRequest() != null)
+        {
+            HttpSession session = getContext().getRequest().getSession();
+            session.setAttribute(EditorManager.PARA_EDITOR, editor);
+        }
+    }
+    
+    /**
+     * Sets the URL to redirect to after the event handler methods fire.
+     * @param url the URL to redirect to
+     */
+    @Validate(required=false)
+    public void setRedirect(String url)
+    {
+        m_redirect = url;
+    }
+
+    /**
+     * Returns the URL to redirect to after the event handler methods fire.
+     * @return the URL to redirect to
+     */
+    public String getRedirect()
+    {
+        return m_redirect;
+    }
 }
---
+++ src/webdocs-stripes/WEB-INF/jsp/templates/default/PreferencesContent.jsp
@@ -0,0 +1,29 @@
+<%@ page import="javax.servlet.jsp.jstl.fmt.*" %>
+<%@ page import="com.ecyrd.jspwiki.ui.*" %>
+<%@ taglib uri="/WEB-INF/jspwiki.tld" prefix="wiki" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+<% 
+  TemplateManager.addResourceRequest( pageContext.getAttribute("wikiActionBean"), "script", "scripts/jspwiki-prefs.js" );
+%>
+
+<wiki:TabbedSection defaultTab="${param.tab}">
+
+  <wiki:Tab id="prefs" title='<%=LocaleSupport.getLocalizedMessage(pageContext, "prefs.tab.prefs")%>' accesskey="p" >
+     <wiki:Include page="PreferencesTab.jsp" />
+  </wiki:Tab>
+
+  <wiki:UserCheck status="authenticated">
+  <wiki:Permission permission="editProfile">
+  <wiki:Tab id="profile" title='<%=LocaleSupport.getLocalizedMessage(pageContext, "prefs.tab.profile")%>' accesskey="o" >
+     <wiki:Include page="ProfileTab.jsp" />
+  </wiki:Tab>
+  </wiki:Permission>
+  </wiki:UserCheck>
+  
+  <wiki:Permission permission="createGroups"> <!-- FIXME check right permissions -->
+  <wiki:Tab id="group" title='<%=LocaleSupport.getLocalizedMessage(pageContext, "group.tab")%>' accesskey="g" >
+    <wiki:Include page="GroupTab.jsp" />
+  </wiki:Tab>
+  </wiki:Permission>
+
+</wiki:TabbedSection>
