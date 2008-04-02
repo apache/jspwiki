9c2cf8a3b27bab4c961fb5b380fcf26fb2062693
Andrew R. Jaquith <ajaquith@apache.org>
2008-04-02 03:49:23 +0000

    Merged with trunk 2.7.0-svn-2.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/branches/JSPWIKI_2_9_STRIPES_BRANCH@643703 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/attachment/AttachmentManager.java
+++ src/com/ecyrd/jspwiki/attachment/AttachmentManager.java
@@ -1,21 +1,22 @@
 /*
     JSPWiki - a JSP-based WikiWiki clone.
 
-    Copyright (C) 2001-2002 Janne Jalkanen (Janne.Jalkanen@iki.fi)
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation; either version 2.1 of the License, or
-    (at your option) any later version.
+       http://www.apache.org/licenses/LICENSE-2.0
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.    
  */
 package com.ecyrd.jspwiki.attachment;
 
@@ -43,7 +44,6 @@ import com.opensymphony.oscache.base.NeedsRefreshException;
  *  that is in use.  It is created by the WikiEngine as a singleton object, and
  *  can be requested through the WikiEngine.
  *
- *  @author Janne Jalkanen
  *  @since 1.9.28
  */
 public class AttachmentManager
@@ -479,6 +479,15 @@ public class AttachmentManager
             return;
         }
 
+        //
+        //  Checks if the actual, real page exists without any modifications
+        //  or aliases.  We cannot store an attachment to a non-existant page.
+        //
+        if( !m_engine.getPageManager().pageExists( att.getParentName() ) )
+        {
+            throw new ProviderException("Parent page does not exist");
+        }
+        
         m_provider.putAttachmentData( att, in );
 
         m_engine.getReferenceManager().updateReferences( att.getName(),
