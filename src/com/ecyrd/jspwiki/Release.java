f98383950cbb589e20755f7a8269e7fbbefc1e7a
Dirk Frederickx <brushed@apache.org>
2008-06-17 17:40:06 +0000

    2.7.0-svn-46: i18n fixes and ie7 fixes:[JSPWIKI-293],[JSPWIKI-294],[JSPWIKI-280]
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@668766 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/Release.java
+++ src/com/ecyrd/jspwiki/Release.java
@@ -77,7 +77,7 @@ public final class Release
      *  <p>
      *  If the build identifier is empty, it is not added.
      */
-    public static final String     BUILD         = "44";
+    public static final String     BUILD         = "46";
     
     /**
      *  This is the generic version string you should use
