1b4d84aeffd8eb746afd4a24ae570d708098c87e
Janne Jalkanen <jalkanen@apache.org>
2008-10-14 19:01:33 +0000

    RC1
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/branches/JSPWIKI_2_8_BRANCH@704621 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/Release.java
+++ src/com/ecyrd/jspwiki/Release.java
@@ -57,7 +57,7 @@ public final class Release
      *  <p>
      *  If the POSTFIX is empty, it is not added to the version string.
      */
-    private static final String    POSTFIX       = "beta";
+    private static final String    POSTFIX       = "";
 
     /** The JSPWiki major version. */
     public static final int        VERSION       = 2;
@@ -77,7 +77,7 @@ public final class Release
      *  <p>
      *  If the build identifier is empty, it is not added.
      */
-    public static final String     BUILD         = "21";
+    public static final String     BUILD         = "";
     
     /**
      *  This is the generic version string you should use
