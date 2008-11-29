2c61217400a00cba241325538b731dc80e74501a
Janne Jalkanen <jalkanen@apache.org>
2008-11-29 20:59:56 +0000

    Fixed to correct version number; 2.8.1 is already released.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/branches/JSPWIKI_2_8_BRANCH@721717 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/Release.java
+++ src/com/ecyrd/jspwiki/Release.java
@@ -66,7 +66,7 @@ public final class Release
     public static final int        REVISION      = 8;
 
     /** The minor revision.  */
-    public static final int        MINORREVISION = 1;
+    public static final int        MINORREVISION = 2;
 
     /** The build number/identifier.  This is a String as opposed to an integer, just
      *  so that people can add other identifiers to it.  The build number is incremented
@@ -77,7 +77,7 @@ public final class Release
      *  <p>
      *  If the build identifier is empty, it is not added.
      */
-    public static final String     BUILD         = "17";
+    public static final String     BUILD         = "1";
     
     /**
      *  This is the generic version string you should use
