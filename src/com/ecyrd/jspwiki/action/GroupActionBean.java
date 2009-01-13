3431f4118503f9785dee6ca66d0d1d5caa5f3f84
Harry Metske <metskem@apache.org>
2009-01-13 20:31:42 +0000

    3.0.0-svn-52 * moved a few messages to CoreResources * added missing Apache copyright templates * a few typos
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@734231 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/action/GroupActionBean.java
+++ src/com/ecyrd/jspwiki/action/GroupActionBean.java
@@ -1,3 +1,24 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.  
+ */
+
 package com.ecyrd.jspwiki.action;
 
 import java.security.Principal;
@@ -70,7 +91,7 @@ public class GroupActionBean extends AbstractActionBean
     }
 
     /**
-     * Handler method for that deletes the group supplied by {@link #getGroup()}.
+     * Handler method that deletes the group supplied by {@link #getGroup()}.
      * If the GroupManager throws a WikiSecurityException because it cannot
      * delete the group for some reason, the exception is re-thrown as a
      * StripesRuntimeException. <code>/Group.jsp</code>.
--- src/com/ecyrd/jspwiki/ui/stripes/HandlerPermission.java
+++ src/com/ecyrd/jspwiki/ui/stripes/HandlerPermission.java
@@ -1,3 +1,24 @@
+/* 
+    JSPWiki - a JSP-based WikiWiki clone.
+
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.  
+ */
+
 /**
  * 
  */
@@ -7,7 +28,7 @@ import java.lang.annotation.*;
 import java.security.Permission;
 
 /**
- * <p>Permission annotation that specifies the the class, target and actions of a
+ * <p>Permission annotation that specifies the class, target and actions of a
  * Permission required to execute a Stripes event handler method. Annotations are
  * specified as follows:</p>
  * <blockquote>
