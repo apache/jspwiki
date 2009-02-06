51a1014b23627a245b58f23ff3b0af5951cdfe38
Harry Metske <metskem@apache.org>
2009-02-06 17:42:21 +0000

    2.8.2-svn-9 JSPWIKI-477 Renamed pages are no longer in the Lucene index
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/branches/JSPWIKI_2_8_BRANCH@741650 13f79535-47bb-0310-9956-ffa450edef68

--- src/com/ecyrd/jspwiki/content/PageRenamer.java
+++ src/com/ecyrd/jspwiki/content/PageRenamer.java
@@ -156,6 +156,11 @@ public class PageRenamer
             updateReferrers( context, fromPage, toPage, referrers );
         }
 
+        //
+        //  re-index the page 
+        //
+        engine.getSearchManager().reindexPage(toPage);
+
 
         //
         //  Done, return the new name.
