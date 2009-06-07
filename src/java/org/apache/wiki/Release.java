ba8cbbe418ad3a9552d1aee18f09014db6914e0e
Janne Jalkanen <jalkanen@apache.org>
2009-06-07 14:25:52 +0000

    JSPWIKI-545: Fixed a number of RAT issues.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@782394 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/wiki/Release.java
+++ src/java/org/apache/wiki/Release.java
@@ -77,7 +77,7 @@ public final class Release
      *  <p>
      *  If the build identifier is empty, it is not added.
      */
-    public static final String     BUILD         = "125";
+    public static final String     BUILD         = "126";
     
     /**
      *  This is the generic version string you should use
