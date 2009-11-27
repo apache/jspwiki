1cdc0843bc1a651efb58bb9def9d1d15bba17845
Andrew R. Jaquith <ajaquith@apache.org>
2009-11-27 20:34:32 +0000

    3.0.0-svn-185
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@884994 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/wiki/Release.java
+++ src/java/org/apache/wiki/Release.java
@@ -77,7 +77,7 @@ public final class Release
      *  <p>
      *  If the build identifier is empty, it is not added.
      */
-    public static final String     BUILD         = "184";
+    public static final String     BUILD         = "185";
 
     /**
      *  This is the generic version string you should use
