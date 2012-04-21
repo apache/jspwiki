0a4381f60f3b8f804ec4eb6b16ee639275f3180f
Juan Pablo Santos Rodríguez <juanpablo@apache.org>
2012-04-21 17:09:19 +0000

    2.9.0-incubating-0, first ASF release candidate build.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@1328712 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/wiki/Release.java
+++ src/org/apache/wiki/Release.java
@@ -57,7 +57,7 @@ public final class Release
      *  <p>
      *  If the POSTFIX is empty, it is not added to the version string.
      */
-    private static final String    POSTFIX       = "svn";
+    private static final String    POSTFIX       = "incubating";
 
     /** The JSPWiki major version. */
     public static final int        VERSION       = 2;
@@ -77,7 +77,7 @@ public final class Release
      *  <p>
      *  If the build identifier is empty, it is not added.
      */
-    public static final String     BUILD         = "9";
+    public static final String     BUILD         = "0";
     
     /**
      *  This is the generic version string you should use
