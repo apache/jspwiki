49ff23023c70fe7fce0faf17c5e56cac504c0c31
Harry Metske <metskem@apache.org>
2012-11-13 17:50:09 +0000

    corrected unwanted change from FindPage to Search (JSPWIKI-664)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@1408858 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/wiki/PropertyReader.java
+++ src/org/apache/wiki/PropertyReader.java
@@ -75,7 +75,7 @@ public final class PropertyReader
       "jspwiki.specialPage.EditProfile",     "UserPreferences.jsp",
       "jspwiki.specialPage.Preferences",     "UserPreferences.jsp",
       "jspwiki.specialPage.Search",          "Search.jsp",
-      "jspwiki.specialPage.Search",        "Search.jsp"};
+      "jspwiki.specialPage.FindPage",        "FindPage.jsp"};
 
 
     /**
