409c18bd81ffa251581746f64cf45d261b3c0adc
Florian Holeczek <florianh@apache.org>
2012-11-22 00:04:28 +0000

    version switch to 2.9.1-svn-0
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/trunk@1412376 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/wiki/Release.java
+++ src/org/apache/wiki/Release.java
@@ -55,7 +55,7 @@ public final class Release
      *  <p>
      *  If the POSTFIX is empty, it is not added to the version string.
      */
-    private static final String    POSTFIX       = "incubating";
+    private static final String    POSTFIX       = "svn";
 
     /** The JSPWiki major version. */
     public static final int        VERSION       = 2;
@@ -64,7 +64,7 @@ public final class Release
     public static final int        REVISION      = 9;
 
     /** The minor revision.  */
-    public static final int        MINORREVISION = 0;
+    public static final int        MINORREVISION = 1;
 
     /** The build number/identifier.  This is a String as opposed to an integer, just
      *  so that people can add other identifiers to it.  The build number is incremented
@@ -75,7 +75,7 @@ public final class Release
      *  <p>
      *  If the build identifier is empty, it is not added.
      */
-    public static final String     BUILD         = "";
+    public static final String     BUILD         = "0";
     
     /**
      *  This is the generic version string you should use
