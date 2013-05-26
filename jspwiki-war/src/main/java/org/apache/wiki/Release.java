997d4b8f0b6cfe4d109f3d7367f046906b3d35c6
Juan Pablo Santos Rodríguez <juanpablo@apache.org>
2013-05-26 19:32:28 +0000

    o.a.w.Release pointing to 2.10.0-svn-0
    all pom.xml files pointing to 2.10.0-SNAPSHOT.
    
    fixed checkstyle & codestyle files location at maven-eclipse-plugin
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/jspwiki/branches/MVN3_BRANCH@1486453 13f79535-47bb-0310-9956-ffa450edef68

--- jspwiki-war/src/main/java/org/apache/wiki/Release.java
+++ jspwiki-war/src/main/java/org/apache/wiki/Release.java
@@ -55,16 +55,16 @@ public final class Release
      *  <p>
      *  If the POSTFIX is empty, it is not added to the version string.
      */
-    private static final String    POSTFIX       = "incubating";
+    private static final String    POSTFIX       = "svn";
 
     /** The JSPWiki major version. */
     public static final int        VERSION       = 2;
 
     /** The JSPWiki revision. */
-    public static final int        REVISION      = 9;
+    public static final int        REVISION      = 10;
 
     /** The minor revision.  */
-    public static final int        MINORREVISION = 2;
+    public static final int        MINORREVISION = 0;
 
     /** The build number/identifier.  This is a String as opposed to an integer, just
      *  so that people can add other identifiers to it.  The build number is incremented
@@ -75,7 +75,7 @@ public final class Release
      *  <p>
      *  If the build identifier is empty, it is not added.
      */
-    public static final String     BUILD         = "14";
+    public static final String     BUILD         = "0";
     
     /**
      *  This is the generic version string you should use
