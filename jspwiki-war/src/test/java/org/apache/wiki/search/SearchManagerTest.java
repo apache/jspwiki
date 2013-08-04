b88068dd1fd6e5f182b8857699b4956c4e3b0187
Harry Metske <metskem@apache.org>
2013-08-04 16:05:12 +0000

    more JSPWIKI-799, increase waittime
    git-svn-id: https://svn.apache.org/repos/asf/jspwiki/trunk@1510222 13f79535-47bb-0310-9956-ffa450edef68

--- jspwiki-war/src/test/java/org/apache/wiki/search/SearchManagerTest.java
+++ jspwiki-war/src/test/java/org/apache/wiki/search/SearchManagerTest.java
@@ -72,12 +72,12 @@ public class SearchManagerTest extends TestCase
     Collection waitForIndex( String text, String testName ) throws Exception
     {
         Collection res = null;
-        for( long l = 0; l < 20; l++ )
+        for( long l = 0; l < 500; l++ )
         {
             if( res == null || res.isEmpty() )
             {
                 Thread.sleep( SLEEP_TIME );
-                System.out.println( "SearchManagerTest.waitForIndex for " + testName + " sleeping (Thread " + Thread.currentThread() +") " + l + " (out of 20)" );
+                System.out.println( "SearchManagerTest.waitForIndex for " + testName + " sleeping " + l + " (out of 20)" );
             }
             else
             {
