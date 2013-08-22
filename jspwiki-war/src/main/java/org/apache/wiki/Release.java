c601ebde578d77d27d380ba1418b4da3737cb804
Glen Mazza <gmazza@apache.org>
2013-08-22 13:53:27 +0000

    Forgot to update the Release version too.
    git-svn-id: https://svn.apache.org/repos/asf/jspwiki/trunk@1516452 13f79535-47bb-0310-9956-ffa450edef68

--- jspwiki-war/src/main/java/org/apache/wiki/Release.java
+++ jspwiki-war/src/main/java/org/apache/wiki/Release.java
@@ -75,7 +75,7 @@ public final class Release
      *  <p>
      *  If the build identifier is empty, it is not added.
      */
-    public static final String     BUILD         = "42";
+    public static final String     BUILD         = "43";
     
     /**
      *  This is the generic version string you should use
