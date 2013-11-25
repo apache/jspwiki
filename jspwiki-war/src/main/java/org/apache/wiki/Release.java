5af027a5d10fce0de4cbe587eb43865f12ce9639
Harry Metske <metskem@apache.org>
2013-11-25 17:40:32 +0000

    2013-11-25  Harry Metske (metskem@apache.org)
           * 2.10.0-svn-47
    
           * fixed JSPWIKI-803 Too strict version requirement on plain editor, maxVersion set to 2.9
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jspwiki/trunk@1545348 13f79535-47bb-0310-9956-ffa450edef68

--- jspwiki-war/src/main/java/org/apache/wiki/Release.java
+++ jspwiki-war/src/main/java/org/apache/wiki/Release.java
@@ -75,7 +75,7 @@ public final class Release
      *  <p>
      *  If the build identifier is empty, it is not added.
      */
-    public static final String     BUILD         = "46";
+    public static final String     BUILD         = "47";
     
     /**
      *  This is the generic version string you should use
