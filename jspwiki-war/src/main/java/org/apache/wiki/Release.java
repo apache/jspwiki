5725033374e5ed2b80762b24e015293d8a1ed9b9
Harry Metske <metskem@apache.org>
2014-02-09 12:39:57 +0000

    updated Release and changelog to reflect 2.10.0-svn-70 version
    git-svn-id: https://svn.apache.org/repos/asf/jspwiki/trunk@1566287 13f79535-47bb-0310-9956-ffa450edef68

--- jspwiki-war/src/main/java/org/apache/wiki/Release.java
+++ jspwiki-war/src/main/java/org/apache/wiki/Release.java
@@ -52,7 +52,7 @@ public final class Release {
      *  <p>
      *  If the POSTFIX is empty, it is not added to the version string.
      */
-    private static final String    POSTFIX       = "";
+    private static final String    POSTFIX       = "svn";
 
     /** The JSPWiki major version. */
     public static final int        VERSION       = 2;
