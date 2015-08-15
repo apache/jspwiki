ed10e5c55597451d858adcf32ec1360d086d5b80
Dirk Frederickx <brushed@apache.org>
2015-08-15 21:30:53 +0000

    Fixing XmlUtilTest
    git-svn-id: https://svn.apache.org/repos/asf/jspwiki/trunk@1696101 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jspwiki-war/src/main/webapp/XHRHtml2Markup.jsp
@@ -0,0 +1,50 @@
+<%--
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
+--%>
+
+<%@ page language="java" pageEncoding="UTF-8"%>
+<%@ page import="org.apache.log4j.*" %>
+<%@ page import="org.apache.wiki.*" %>
+<%@ page import="org.apache.wiki.htmltowiki.HtmlStringToWikiTranslator" %>
+<%@ taglib uri="http://jspwiki.apache.org/tags" prefix="wiki" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%!
+  public void jspInit()
+  {
+    wiki = WikiEngine.getInstance( getServletConfig() );
+  }
+  //Logger log = Logger.getLogger("XHRHtml2Markup");
+  WikiEngine wiki;
+%>
+<%
+  WikiContext wikiContext = wiki.createContext( request, WikiContext.VIEW );
+
+  if( !wiki.getAuthorizationManager().hasAccess( wikiContext, response ) ) return;
+
+  response.setContentType("text/html; charset="+wiki.getContentEncoding() );
+  //response.setHeader( "Cache-control", "max-age=0" );
+  //response.setDateHeader( "Expires", new Date().getTime() );
+  //response.setDateHeader( "Last-Modified", new Date().getTime() );
+
+  String htmlText = request.getParameter( "htmlPageText" );
+
+  if( htmlText != null )
+  {
+%><%= new HtmlStringToWikiTranslator().translate(htmlText, wikiContext) %><%
+  }
+%>
--- jspwiki-war/src/test/java/org/apache/wiki/util/XmlUtilTest.java
+++ jspwiki-war/src/test/java/org/apache/wiki/util/XmlUtilTest.java
@@ -1,4 +1,4 @@
-/* 
+/*
     Licensed to the Apache Software Foundation (ASF) under one
     or more contributor license agreements.  See the NOTICE file
     distributed with this work for additional information
@@ -14,7 +14,7 @@
     "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
     KIND, either express or implied.  See the License for the
     specific language governing permissions and limitations
-    under the License.  
+    under the License.
  */
 package org.apache.wiki.util;
 
@@ -37,53 +37,53 @@ public class XmlUtilTest extends TestCase {
     public static Test suite() {
         return new TestSuite( XmlUtilTest.class );
     }
-    
+
     public void testParseFromClasspath() {
     	List< Element > elements = XmlUtil.parse( ModuleManager.PLUGIN_RESOURCE_LOCATION, "/modules/plugin" );
     	assertEquals( 4, elements.size() ); // 2 on src/main/resources, another 2 on src/test/resources
-    	
+
     	elements = XmlUtil.parse( ModuleManager.PLUGIN_RESOURCE_LOCATION, "/modules/filter" );
      	assertEquals( 1, elements.size() );
-     	
+
      	elements = XmlUtil.parse( ModuleManager.PLUGIN_RESOURCE_LOCATION, "/modules/editor" );
-     	assertEquals( 1, elements.size() );
-     	
+     	assertEquals( 2, elements.size() );
+
      	elements = XmlUtil.parse( ModuleManager.PLUGIN_RESOURCE_LOCATION, "/modules/heck" );
      	assertEquals( 0, elements.size() );
-     	
+
      	elements = XmlUtil.parse( "doesnt/exist.this", "/modules/editor" );
      	assertEquals( 0, elements.size() );
-     	
+
      	elements = XmlUtil.parse( ( String )null, "/modules/editor" );
      	assertEquals( 0, elements.size() );
-     	
+
      	elements = XmlUtil.parse( ModuleManager.PLUGIN_RESOURCE_LOCATION, null );
      	assertEquals( 0, elements.size() );
-     	
+
      	elements = XmlUtil.parse( ClassUtil.MAPPINGS, "/classmappings/mapping" );
      	assertEquals( 19, elements.size() );
     }
-    
+
     public void testParseFromStream() throws FileNotFoundException {
     	InputStream is = new FileInputStream( new File ("./src/test/resources/ini/jspwiki_module.xml" ) );
     	List< Element > elements = XmlUtil.parse( is, "/modules/plugin" );
-    	assertEquals( 2, elements.size() ); 
-    	
+    	assertEquals( 2, elements.size() );
+
     	elements = XmlUtil.parse( is, "/modules/filter" );
      	assertEquals( 0, elements.size() );
-     	
+
      	elements = XmlUtil.parse( is, "/modules/editor" );
      	assertEquals( 0, elements.size() );
-     	
+
      	elements = XmlUtil.parse( is, "/modules/heck" );
      	assertEquals( 0, elements.size() );
-     	
+
      	elements = XmlUtil.parse( ( InputStream )null, "/modules/editor" );
      	assertEquals( 0, elements.size() );
-     	
+
      	elements = XmlUtil.parse( is, null );
      	assertEquals( 0, elements.size() );
-     	
+
      	IOUtils.closeQuietly( is );
     }
 
