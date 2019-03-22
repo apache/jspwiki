791ccc1f4502a7cde258ef65ab54ab0bf6bbf005
juanpablo <juanpablo@apache.org>
2019-03-22 17:35:01 +0100

    prepare release for 2.11.0.M3

--- jspwiki-main/src/main/java/org/apache/wiki/Release.java
+++ jspwiki-main/src/main/java/org/apache/wiki/Release.java
@@ -52,7 +52,7 @@ public final class Release {
      *  <p>
      *  If the POSTFIX is empty, it is not added to the version string.
      */
-    private static final String    POSTFIX       = "M3-git";
+    private static final String    POSTFIX       = "M3";
 
     /** The JSPWiki major version. */
     public static final int        VERSION       = 2;
@@ -72,7 +72,7 @@ public final class Release {
      *  <p>
      *  If the build identifier is empty, it is not added.
      */
-    public static final String     BUILD         = "04";
+    public static final String     BUILD         = "";
 
     /**
      *  This is the generic version string you should use when printing out the version.  It is of
