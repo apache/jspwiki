0a0b1b803e63ff7beec35d535da9e942a9b1d616
Juan Pablo Santos Rodríguez <juanpablo.santos@gmail.com>
2022-03-22 11:51:17 +0100

    fix sonar issue

--- jspwiki-api/src/test/java/org/apache/wiki/api/engine/EngineLifecycleExtensionTest.java
+++ jspwiki-api/src/test/java/org/apache/wiki/api/engine/EngineLifecycleExtensionTest.java
@@ -30,7 +30,7 @@ import java.util.Properties;
 
 
 @ExtendWith( MockitoExtension.class )
-public class EngineLifecycleExtensionTest {
+class EngineLifecycleExtensionTest {
 
     @Mock Engine engine;
 
