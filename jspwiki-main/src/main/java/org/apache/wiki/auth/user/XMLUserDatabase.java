apache/jspwiki

commit 0013d81d626174ee8e72b851ab8c58091670e402

Use java Stream instead.

jspwiki-main/src/main/java/org/apache/wiki/auth/user/XMLUserDatabase.java
@@ -52,6 +52,8 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * <p>Manages {@link DefaultUserProfile} objects using XML files for persistence. Passwords are hashed using SHA1. User entries are simple
@@ -372,14 +374,7 @@ public class XMLUserDatabase extends AbstractUserDatabase {
         final DateFormat c_format = new SimpleDateFormat( DATE_FORMAT );
         final String index = profile.getLoginName();
         final NodeList users = c_dom.getElementsByTagName( USER_TAG );
-        Element user = null;
-        for( int i = 0; i < users.getLength(); i++ ) {
-            final Element currentUser = ( Element )users.item( i );
-            if( currentUser.getAttribute( LOGIN_NAME ).equals( index ) ) {
-                user = currentUser;
-                break;
-            }
-        }
+        Element user = IntStream.range(0, users.getLength()).mapToObj(i -> (Element) users.item(i)).filter(currentUser -> currentUser.getAttribute(LOGIN_NAME).equals(index)).findFirst().orElse(null);
 
         boolean isNew = false;
 
@@ -523,17 +518,12 @@ public class XMLUserDatabase extends AbstractUserDatabase {
      * @return the text nodes that are immediate children of the base element, concatenated together
      */
     private String extractText( final Element element ) {
-        final StringBuilder text = new StringBuilder();
+        final String text = "";
         if( element.getChildNodes().getLength() > 0 ) {
             final NodeList children = element.getChildNodes();
-            for( int k = 0; k < children.getLength(); k++ ) {
-                final Node child = children.item( k );
-                if( child.getNodeType() == Node.TEXT_NODE ) {
-                    text.append(((Text) child).getData());
-                }
-            }
+            text = IntStream.range(0, children.getLength()).mapToObj(children::item).filter(child -> child.getNodeType() == Node.TEXT_NODE).map(child -> ((Text) child).getData()).collect(Collectors.joining());
         }
-        return text.toString();
+        return text;
     }
 
     /**

jspwiki-main/src/main/java/org/apache/wiki/xmlrpc/AbstractRPCHandler.java
@@ -33,6 +33,7 @@ import java.util.Date;
 import java.util.Hashtable;
 import java.util.Set;
 import java.util.Vector;
+import java.util.stream.Collectors;
 
 /**
  *  Provides definitions for RPC handler routines.
@@ -72,7 +73,7 @@ public abstract class AbstractRPCHandler implements WikiRPCHandler {
     public Vector getRecentChanges( final Date since ) {
         checkPermission( PagePermission.VIEW );
         final Set< Page > pages = m_engine.getManager( PageManager.class ).getRecentChanges();
-        final Vector< Hashtable< ?, ? > > result = new Vector<>();
+        final Vector< Hashtable< ?, ? > > result;
 
         // Transform UTC into local time.
         final Calendar cal = Calendar.getInstance();
@@ -80,11 +81,7 @@ public abstract class AbstractRPCHandler implements WikiRPCHandler {
         cal.add( Calendar.MILLISECOND, cal.get( Calendar.ZONE_OFFSET ) +
                   (cal.getTimeZone().inDaylightTime( since ) ? cal.get( Calendar.DST_OFFSET ) : 0 ) );
 
-        for( final Page page : pages ) {
-            if( page.getLastModified().after( cal.getTime() ) ) {
-                result.add( encodeWikiPage( page ) );
-            }
-        }
+        result = pages.stream().filter(page -> page.getLastModified().after(cal.getTime())).map(this::encodeWikiPage).collect(Collectors.toCollection(Vector::new));
 
         return result;
     }

jspwiki-main/src/main/java/org/apache/wiki/WikiSession.java
@@ -91,12 +91,7 @@ public class WikiSession implements Session {
      * @return the result
      */
     protected boolean isInGroup( final Group group ) {
-        for( final Principal principal : getPrincipals() ) {
-            if( isAuthenticated() && group.isMember( principal ) ) {
-                return true;
-            }
-        }
-        return false;
+        return Arrays.stream(getPrincipals()).anyMatch(principal -> isAuthenticated() && group.isMember(principal));
     }
 
     /**
@@ -211,16 +206,10 @@ public class WikiSession implements Session {
     /** {@inheritDoc} */
     @Override
     public Principal[] getPrincipals() {
-        final ArrayList< Principal > principals = new ArrayList<>();
 
         // Take the first non Role as the main Principal
-        for( final Principal principal : m_subject.getPrincipals() ) {
-            if ( AuthenticationManager.isUserPrincipal( principal ) ) {
-                principals.add( principal );
-            }
-        }
 
-        return principals.toArray( new Principal[0] );
+        return m_subject.getPrincipals().stream().filter(AuthenticationManager::isUserPrincipal).toArray(Principal[]::new);
     }
 
     /** {@inheritDoc} */

jspwiki-main/src/main/java/org/apache/wiki/auth/AuthenticationManager.java
@@ -187,12 +187,7 @@ public interface AuthenticationManager extends Initializable {
      * @return the login principal
      */
     default Principal getLoginPrincipal( final Set< Principal > principals ) {
-        for( final Principal principal : principals ) {
-            if ( isUserPrincipal( principal ) ) {
-                return principal;
-            }
-        }
-        return null;
+        return principals.stream().filter(AuthenticationManager::isUserPrincipal).findFirst().orElse(null);
     }
 
     // events processing .......................................................

jspwiki-main/src/main/java/org/apache/wiki/auth/DefaultAuthorizationManager.java
@@ -189,11 +189,7 @@ public class DefaultAuthorizationManager implements AuthorizationManager {
         if( session.isAuthenticated() && AuthenticationManager.isUserPrincipal( principal ) ) {
             final String principalName = principal.getName();
             final Principal[] userPrincipals = session.getPrincipals();
-            for( final Principal userPrincipal : userPrincipals ) {
-                if( userPrincipal.getName().equals( principalName ) ) {
-                    return true;
-                }
-            }
+            return Arrays.stream(userPrincipals).anyMatch(userPrincipal -> userPrincipal.getName().equals(principalName));
         }
         return false;
     }

jspwiki-main/src/main/java/org/apache/wiki/auth/SessionMonitor.java
@@ -39,6 +39,7 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.WeakHashMap;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 /**
  *  <p>Manages Sessions for different Engines.</p>
@@ -220,11 +221,9 @@ public class SessionMonitor implements HttpSessionListener {
      * @return the array of user principals
      */
     public final Principal[] userPrincipals() {
-        final Collection<Principal> principals = new ArrayList<>();
+        final Collection<Principal> principals;
         synchronized ( m_sessions ) {
-            for ( final Session session : m_sessions.values()) {
-                principals.add( session.getUserPrincipal() );
-            }
+            principals = m_sessions.values().stream().map(Session::getUserPrincipal).collect(Collectors.toList());
         }
         final Principal[] p = principals.toArray( new Principal[0] );
         Arrays.sort( p, m_comparator );

jspwiki-main/src/main/java/org/apache/wiki/auth/acl/AclEntryImpl.java
@@ -25,6 +25,7 @@ import java.security.Permission;
 import java.security.Principal;
 import java.util.Enumeration;
 import java.util.Vector;
+import java.util.stream.Collectors;
 
 
 /**
@@ -135,19 +136,8 @@ public class AclEntryImpl implements AclEntry, Serializable {
      */
     public String toString() {
         final Principal p = getPrincipal();
-        final StringBuilder sb = new StringBuilder();
-        sb.append( "[AclEntry ALLOW " )
-          .append( p != null ? p.getName() : "null" )
-          .append( " " );
 
-        for( final Permission pp : m_permissions ) {
-            sb.append( pp.toString() );
-            sb.append( "," );
-        }
-
-        sb.append( "]" );
-
-        return sb.toString();
+        return m_permissions.stream().map(pp -> pp.toString() + ",").collect(Collectors.joining("", "[AclEntry ALLOW " + (p != null ? p.getName() : "null") + " ", "]"));
     }
 
     /**
@@ -155,12 +145,7 @@ public class AclEntryImpl implements AclEntry, Serializable {
      * permission.
      */
     private Permission findPermission( final Permission p ) {
-        for( final Permission pp : m_permissions ) {
-            if( pp.implies( p ) ) {
-                return pp;
-            }
-        }
-        return null;
+        return m_permissions.stream().filter(pp -> pp.implies(p)).findFirst().orElse(null);
     }
 
 }

jspwiki-main/src/main/java/org/apache/wiki/auth/acl/AclImpl.java
@@ -115,13 +115,8 @@ public class AclImpl implements Acl, Serializable {
     /** {@inheritDoc} */
     @Override
     public AclEntry getAclEntry( final Principal principal ) {
-        for( final AclEntry entry : m_entries ) {
-            if( entry.getPrincipal().getName().equals( principal.getName() ) ) {
-                return entry;
-            }
-        }
+        return m_entries.stream().filter(entry -> entry.getPrincipal().getName().equals(principal.getName())).findFirst().orElse(null);
 
-        return null;
     }
 
     /** {@inheritDoc} */

commit 7b1d9eaaacd747d78482a2bd7d94784cc7443cb0

fix compilation errors introduced by #240

jspwiki-main/src/main/java/org/apache/wiki/auth/user/XMLUserDatabase.java
@@ -518,7 +518,7 @@ public class XMLUserDatabase extends AbstractUserDatabase {
      * @return the text nodes that are immediate children of the base element, concatenated together
      */
     private String extractText( final Element element ) {
-        final String text = "";
+        String text = "";
         if( element.getChildNodes().getLength() > 0 ) {
             final NodeList children = element.getChildNodes();
             text = IntStream.range(0, children.getLength()).mapToObj(children::item).filter(child -> child.getNodeType() == Node.TEXT_NODE).map(child -> ((Text) child).getData()).collect(Collectors.joining());

jspwiki-main/src/main/java/org/apache/wiki/xmlrpc/AbstractRPCHandler.java
@@ -33,7 +33,6 @@ import java.util.Date;
 import java.util.Hashtable;
 import java.util.Set;
 import java.util.Vector;
-import java.util.stream.Collectors;
 
 /**
  *  Provides definitions for RPC handler routines.
@@ -73,7 +72,7 @@ public abstract class AbstractRPCHandler implements WikiRPCHandler {
     public Vector getRecentChanges( final Date since ) {
         checkPermission( PagePermission.VIEW );
         final Set< Page > pages = m_engine.getManager( PageManager.class ).getRecentChanges();
-        final Vector< Hashtable< ?, ? > > result;
+        final Vector< Hashtable< ?, ? > > result = new Vector<>();
 
         // Transform UTC into local time.
         final Calendar cal = Calendar.getInstance();
@@ -81,7 +80,11 @@ public abstract class AbstractRPCHandler implements WikiRPCHandler {
         cal.add( Calendar.MILLISECOND, cal.get( Calendar.ZONE_OFFSET ) +
                   (cal.getTimeZone().inDaylightTime( since ) ? cal.get( Calendar.DST_OFFSET ) : 0 ) );
 
-        result = pages.stream().filter(page -> page.getLastModified().after(cal.getTime())).map(this::encodeWikiPage).collect(Collectors.toCollection(Vector::new));
+        for( final Page page : pages ) {
+            if( page.getLastModified().after( cal.getTime() ) ) {
+                result.add( encodeWikiPage( page ) );
+            }
+        }
 
         return result;
     }