041cd532b56b78bcc080b2ef7404ef8eda246548
Arturo Bernal <abernal@apache.org>
2023-10-02 21:19:24 +0200

    JSPWIKI-1056: Enhance Absolute URL Utility Methods in HttpUtil
    - Implement getAbsoluteUrl methods in HttpUtil to construct absolute URLs, considering various headers like X-Forwarded-Host, X-Forwarded-Proto, and X-Forwarded-Server.
    - This enhancement allows emails to now include absolute URLs for the login page, replacing the previous relative URLs.

--- jspwiki-util/src/test/java/org/apache/wiki/util/HttpUtilTest.java
+++ jspwiki-util/src/test/java/org/apache/wiki/util/HttpUtilTest.java
@@ -23,6 +23,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 public class HttpUtilTest {
@@ -59,11 +64,123 @@ public class HttpUtilTest {
         final MockHttpServletRequest req = new MockHttpServletRequest( "/wiki", "/example" );
         req.setCookies( cookies );
 
-        Assertions.assertEquals( "value1", HttpUtil.retrieveCookieValue( req, "cookie1" ) );
-        Assertions.assertEquals( "value2", HttpUtil.retrieveCookieValue( req, "cookie2" ) );
+        assertEquals( "value1", HttpUtil.retrieveCookieValue( req, "cookie1" ) );
+        assertEquals( "value2", HttpUtil.retrieveCookieValue( req, "cookie2" ) );
         Assertions.assertNull( HttpUtil.retrieveCookieValue( req, "cookie3" ) );
         Assertions.assertNull( HttpUtil.retrieveCookieValue( req, "cookie4" ) );
         Assertions.assertNull( HttpUtil.retrieveCookieValue( req, "cookie5" ) );
     }
+
+    @Test
+    public void testGetAbsoluteUrlWithRelativeUrl() {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getScheme()).thenReturn("http");
+        when(request.getServerName()).thenReturn("localhost");
+        when(request.getServerPort()).thenReturn(8080);
+
+        String relativeUrl = "/login";
+        String expected = "http://localhost:8080/login";
+
+        String actual = HttpUtil.getAbsoluteUrl(request, relativeUrl);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetAbsoluteUrlWithoutRelativeUrl() {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getScheme()).thenReturn("https");
+        when(request.getServerName()).thenReturn("localhost");
+        when(request.getServerPort()).thenReturn(443);
+
+        String expected = "https://localhost";
+
+        String actual = HttpUtil.getAbsoluteUrl(request);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetAbsoluteUrlWithDefaultHttpPort() {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getScheme()).thenReturn("http");
+        when(request.getServerName()).thenReturn("localhost");
+        when(request.getServerPort()).thenReturn(80);
+
+        String relativeUrl = "/login";
+        String expected = "http://localhost/login";
+
+        String actual = HttpUtil.getAbsoluteUrl(request, relativeUrl);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetAbsoluteUrlWithDefaultHttpsPort() {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getScheme()).thenReturn("https");
+        when(request.getServerName()).thenReturn("localhost");
+        when(request.getServerPort()).thenReturn(443);
+
+        String relativeUrl = "/login";
+        String expected = "https://localhost/login";
+
+        String actual = HttpUtil.getAbsoluteUrl(request, relativeUrl);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetAbsoluteUrlWithForwardedHostAndProto() {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getHeader("X-Forwarded-Host")).thenReturn("proxyhost");
+        when(request.getHeader("X-Forwarded-Proto")).thenReturn("https");
+
+        String relativeUrl = "/login";
+        String expected = "https://proxyhost/login";
+
+        String actual = HttpUtil.getAbsoluteUrl(request, relativeUrl);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetAbsoluteUrlWithForwardedServerAndProto() {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getHeader("X-Forwarded-Server")).thenReturn("proxyserver");
+        when(request.getHeader("X-Forwarded-Proto")).thenReturn("https");
+
+        String relativeUrl = "/login";
+        String expected = "https://proxyserver/login";
+
+        String actual = HttpUtil.getAbsoluteUrl(request, relativeUrl);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetAbsoluteUrlWithNoForwardedHeaders() {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getScheme()).thenReturn("http");
+        when(request.getServerName()).thenReturn("localhost");
+        when(request.getServerPort()).thenReturn(8080);
+
+        String relativeUrl = "/login";
+        String expected = "http://localhost:8080/login";
+
+        String actual = HttpUtil.getAbsoluteUrl(request, relativeUrl);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetAbsoluteUrlWithAllHeaders() {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getHeader("X-Forwarded-Host")).thenReturn("forwardedHost");
+        when(request.getHeader("X-Forwarded-Proto")).thenReturn("forwardedProto");
+        when(request.getHeader("X-Forwarded-Server")).thenReturn("forwardedServer");
+        when(request.getScheme()).thenReturn("https");
+        when(request.getServerName()).thenReturn("localhost");
+        when(request.getServerPort()).thenReturn(443);
+
+        String expected = "forwardedProto://forwardedHost";
+
+        String actual = HttpUtil.getAbsoluteUrl(request);
+        assertEquals(expected, actual);
+    }
+
 
 }
