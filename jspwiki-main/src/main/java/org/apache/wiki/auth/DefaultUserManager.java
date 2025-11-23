0c8f3674c5e89d1f68a150395786c5f2d3f517c9
Alex O'Ree <alexoree@apache.org>
2025-11-23 08:03:04 -0500

    JSPWIKI-1248 adds a new password complexity check mechanism, measurement of the quantity of characters that have changed

--- jspwiki-main/src/main/java/org/apache/wiki/auth/DefaultUserManager.java
+++ jspwiki-main/src/main/java/org/apache/wiki/auth/DefaultUserManager.java
@@ -355,7 +355,7 @@ public class DefaultUserManager implements UserManager {
                 if( !profile.isNew() && !getUserDatabase().validatePassword( profile.getLoginName(), password0 ) ) {
                     session.addMessage( SESSION_MESSAGES, rb.getString( "security.error.passwordnomatch" ) );
                 }
-                List<String> msg = PasswordComplexityVeriffier.validate(password2, context);
+                List<String> msg = PasswordComplexityVeriffier.validate(password2, password0, context);
                 for (String s : msg) {
                     session.addMessage( SESSION_MESSAGES, s );
                 }
