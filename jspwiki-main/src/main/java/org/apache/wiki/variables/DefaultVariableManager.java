7066f558ee054f3c8122ac0e064882f6d79a0e10
Alex O'Ree <alexoree@apache.org>
2026-04-12 19:50:10 -0400

    resets to 3.0.0-SNAPSHOT syncs the last minute changes from 2.12.4

--- jspwiki-main/src/main/java/org/apache/wiki/variables/DefaultVariableManager.java
+++ jspwiki-main/src/main/java/org/apache/wiki/variables/DefaultVariableManager.java
@@ -156,8 +156,13 @@ public class DefaultVariableManager implements VariableManager {
         if (!"jspwiki.frontpage".equals(name) && 
             !"jspwiki.runfilters".equals(name) && 
             name.startsWith( "jspwiki" ) ) {
-            LOG.warn("variable manager is denying access to '" + name + "'");
+            String whitelist = context.getEngine().getWikiProperties().getProperty("jspwiki.variablemanager.whitelist");
+            if (whitelist!=null && !whitelist.contains(name)) {
+                 LOG.warn("variable manager is denying access to '" + name + "'. to override this behavior, "
+                         + "you can add this to jspwiki.variablemanager.whitelist in the properties file.");
             return "";
+            }
+           
         }
         for( final String value : THE_BIG_NO_NO_LIST ) {
             if( name.equals( value ) ) {
